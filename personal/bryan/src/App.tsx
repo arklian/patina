c1d0d1a5fce8e823c82093795058096a768dfb78
Bryan Zhang <bz5989@princeton.edu>
2024-06-18 14:20:10 -0400

    Bryan: Finished part 1.3
    Changed variable forms, then modified component calls to make sure
    everything still works

--- personal/bryan/src/App.tsx
+++ personal/bryan/src/App.tsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+    console.log(props)
     return (
         <div>
             <h1>{props.course}</h1>
@@ -7,6 +8,7 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+    console.log(props)
     return (
         <div>
             <Part part={props.part1} exercise={props.exercise1} />
@@ -17,29 +19,37 @@ const Content = (props) => {
 }
 
 const Part = (props) => {
+    console.log(props)
     return (
         <p>{props.part} {props.exercise}</p>
     )
 }
 
 const Total = (props) => {
+    console.log(props)
     return <p>Number of exercises {props.val}</p>
 }
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course={course}/>
-            <Content part1={part1} exercise1={exercises1} part2={part2} exercise2={exercises2} part3={part3} exercise3={exercises3}/>
-            <Total val={exercises1+exercises2+exercises3} />
+            <Content part1={part1.name} exercise1={part1.exercises} part2={part2.name} exercise2={part2.exercises} part3={part3.name} exercise3={part3.exercises}/>
+            <Total val={part1.exercises+part2.exercises+part3.exercises} />
         </div>
     )
 }
