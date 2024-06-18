b83bf30ddc101222f27a6aee99664f716f317de3
Haoking Luo <hao5k6032@gmail.com>
2024-06-18 14:22:29 -0400

    Haoking Luo: Intro to React
    Following along the lesson notes

--- personal/haoking/js/src/App.tsx
+++ personal/haoking/js/src/App.tsx
@@ -3,12 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Hello = (params) => {
+    console.log(params)
+    return (
+        <div>
+            <p>Hello {params.field1} you are {params.field2}</p>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <>
+            <div>
+                custom footer by me!
+            </div>
+        </>
+    )
+}
+
 function App() {
   const [count, setCount] = useState(0)
     const now = new Date()
 
     console.log("hello from console")
     console.log(now)
+
+    const name:string = 'johnathan'
+    const age:number = 26
 
   return (
     <>
@@ -40,6 +62,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+        <h1> Greetings </h1>
+        <Hello field1='george'field2={26+36} />
+        <Hello field1='john' field2={16*2}/>
+        <Hello field1={name} field2={age} />
+        <Footer/>
     </>
   )
 }
