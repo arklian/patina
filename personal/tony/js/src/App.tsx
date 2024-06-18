3bee2f62606a3f3f1814fc0096154ca2724bdda3
Tony Wang <wangtony4005@gmail.com>
2024-06-18 13:54:54 -0400

    tony : copy vite setup
    fullstack setup
    
    fixed website loading
    
    Checking out JSX

---
+++ personal/tony/js/src/App.tsx
@@ -0,0 +1,20 @@
+
+
+
+const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+    console.log(now, a+b)
+
+    return (
+        <div>
+            <p>Hello world, it is {now.toString()}</p>
+            <p>
+                {a} plus {b} is {a + b}
+            </p>
+        </div>
+    )
+}
+
+export default App
