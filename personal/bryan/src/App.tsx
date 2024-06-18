742466e99cb1a98b860326fc27f4e35a41c33f40
Bryan Zhang <bz5989@princeton.edu>
2024-06-18 13:57:19 -0400

    Bryan: Finished Exercise 1.1
    Finished exercise 1.1, which was refactoring the given code to
    have three extra components: Header, Content, and Total

--- personal/bryan/src/App.tsx
+++ personal/bryan/src/App.tsx
@@ -1,22 +1,39 @@
-const Hello = (props) => {
-    console.log(props)
+const Header = (props) => {
     return (
         <div>
-            <p>
-                Hello {props.name}, you are {props.age} years old
-            </p>
+            <h1>{props.course}</h1>
         </div>
     )
 }
 
+const Content = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercise}
+        </p>
+    )
+}
+
+const Total = (props) => {
+    return <p>Number of exercises props.val</p>
+}
+
 const App = () => {
-    const name = 'Bryan'
-    const age = 19
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name='Maya' age={16+10}/>
-            <Hello name={name} age={age}/>
+            <Header course={course}/>
+            <Content part={part1} exercise={exercises1} />
+            <Content part={part2} exercise={exercises2} />
+            <Content part={part3} exercise={exercises3} />
+            <Total val={exercises1+exercises2+exercises3} />
         </div>
     )
 }
