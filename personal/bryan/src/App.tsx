b37854ed1a78dff95936d695a7e597456bf3aa92
Bryan Zhang <bz5989@princeton.edu>
2024-06-18 13:46:03 -0400

    Bryan: Played around with props
    Added in props as shown in the tutorial

--- personal/bryan/src/App.tsx
+++ personal/bryan/src/App.tsx
@@ -1,15 +1,22 @@
-const App = () => {
-    const now = new Date()
-    const a = 10
-    const b = 20
-    console.log(now, a+b)
-
+const Hello = (props) => {
+    console.log(props)
     return (
         <div>
-            <p>Hello world, it is {now.toString()}</p>
             <p>
-                {a} plus {b} is {a + b}
+                Hello {props.name}, you are {props.age} years old
             </p>
+        </div>
+    )
+}
+
+const App = () => {
+    const name = 'Bryan'
+    const age = 19
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name='Maya' age={16+10}/>
+            <Hello name={name} age={age}/>
         </div>
     )
 }
