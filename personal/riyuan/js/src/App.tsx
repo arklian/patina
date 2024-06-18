f1995e4bdade721c164b60e22d3aaf74775be0df
Riyuan Liu <riyusnliu@gmail.com>
2024-06-18 15:43:02 -0400

    riyuan: Finish 1.5
    Finish 1.5

--- personal/riyuan/js/src/App.tsx
+++ personal/riyuan/js/src/App.tsx
@@ -1,8 +1,7 @@
-const Header = ({ course }: { course: any }) => {
-    return <h1>{course}</h1>;
+const Header = ({ prop }: { prop: any }) => {
+    return <h1>{prop}</h1>;
 };
 
-// Content component with props typed as any
 const Content = ({ parts }: { parts: any[] }) => {
     return (
         <div>
@@ -15,7 +14,6 @@ const Content = ({ parts }: { parts: any[] }) => {
     );
 };
 
-// Total component with props typed as any
 const Total = ({ parts }: { parts: any[] }) => {
     const total = parts.reduce((acc, part) => acc + part.exercises, 0);
     return (
@@ -25,20 +23,30 @@ const Total = ({ parts }: { parts: any[] }) => {
     );
 };
 
-// App component using any for component props
 const App = () => {
-    const course = 'Half Stack application development';
-    const parts = [
-        { name: 'Fundamentals of React', exercises: 10 },
-        { name: 'Using props to pass data', exercises: 7 },
-        { name: 'State of a component', exercises: 14 }
-    ];
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    };
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header prop={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     );
 }
