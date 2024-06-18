cbc3643691f2469992dec111805f16d73e6f318e
Jionghao Wu <jw6643@nyu.edu>
2024-06-18 14:15:58 -0400

    header working for 1.1

--- personal/Jionghao/js/src/App.tsx
+++ personal/Jionghao/js/src/App.tsx
@@ -1,40 +1,38 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 
-const Hello = () => {
-    return (
-        <div>
-            <p>Hello world</p>
-        </div>
+const Header = (props) => {
+    return(
+        <h1>{props.course}</h1>
     )
 }
 
 
-function App() {
-  const [count, setCount] = useState(0)
-    const now = new Date()
-    const a = 10
-    const b = 20
-    console.log(now, a+b)
-  return (
 
-    <>
-      <h1>Vite + React</h1>
-      <Hello />
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <Header course = {course}/>
+            <p>
+                {part1} {exercises1}
+            </p>
+            <p>
+                {part2} {exercises2}
+            </p>
+            <p>
+                {part3} {exercises3}
+            </p>
+            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        </div>
+    )
 }
 
 export default App
