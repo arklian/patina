38c1e35e5c45587acbdb48f320b8c55980220cb4
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-06-18 15:43:00 -0400

    Manny: exercise 1.3 complete

--- personal/manny/js/src/App.tsx
+++ personal/manny/js/src/App.tsx
@@ -40,12 +40,18 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
     const part = [
         {name: 'Fundamentals of React', exercises:10 },
         {name: 'Using props to pass data', exercises:7 },
@@ -57,7 +63,7 @@ const App = () => {
             <Header course={course} />
             <Content part = {part} />
 
-            <Total exercises1 = {exercises1} exercises2={exercises2} exercises3={exercises3}/>
+            <Total exercises1 = {part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
         </div>
     )
 }
