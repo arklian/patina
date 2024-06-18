ed00b0b7c960ce003e84b0ecc95d4a5c89b6ddb8
Jionghao Wu <jw6643@nyu.edu>
2024-06-18 13:59:03 -0400

    trying react

--- personal/Jionghao/js/src/App.tsx
+++ personal/Jionghao/js/src/App.tsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 
+const Hello = () => {
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+
 function App() {
   const [count, setCount] = useState(0)
+    const now = new Date()
+    const a = 10
+    const b = 20
+    console.log(now, a+b)
   return (
+
     <>
       <h1>Vite + React</h1>
+      <Hello />
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
