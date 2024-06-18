0bc698ad373924bc83f9ba8621fdc01361ad49a5
Tony Wang <wangtony4005@gmail.com>
2024-06-18 16:49:04 -0400

    Finished Exercise 1.4

--- personal/tony/js/src/App.tsx
+++ personal/tony/js/src/App.tsx
@@ -6,44 +6,49 @@ const CourseName = ({ course }) => {
     )
 }
 
-const Content = ({ part1, part2, part3 }) => {
+const Content = ({ parts }) => {
     return (
         <div>
-            <p>{part1.name} {part1.exercises}</p>
-            <p>{part2.name} {part2.exercises}</p>
-            <p>{part3.name} {part3.exercises}</p>
+            <p>{parts[0].name} {parts[0].exercises}</p>
+            <p>{parts[1].name} {parts[1].exercises}</p>
+            <p>{parts[2].name} {parts[2].exercises}</p>
         </div>
     )
 }
 
-const Total = ({ part1, part2, part3 }) => {
+const Total = ({ parts }) => {
     return (
-        <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+        <div>
+            <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+        </div>
     )
 }
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
 
     return (
         <div>
             <CourseName course={course} />
-            <Content part1={part1} part2={part2} part3={part3} />
-            <Total part1={part1} part2={part2} part3={part3} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
     )
 }
+
 
 export default App
