aa2ad67d46b80ea76164b0d6990190f1d6ade8c9
Jionghao Wu <jw6643@nyu.edu>
2024-06-18 15:11:33 -0400

    Jionghao: Finished 1.3

--- personal/Jionghao/js/src/App.tsx
+++ personal/Jionghao/js/src/App.tsx
@@ -37,23 +37,30 @@ const Total = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
             <Header course = {course}/>
-            <Content prt1 = {part1} exe1 = {exercises1}
-                     prt2 = {part2} exe2 = {exercises2}
-                     prt3 = {part3} exe3 = {exercises3}
+            <Content prt1 = {part1.name} exe1 = {part1.exercises}
+                     prt2 = {part2.name} exe2 = {part2.exercises}
+                     prt3 = {part3.name} exe3 = {part3.exercises}
             />
-            <Total tot = {exercises1 + exercises2 + exercises3} />
+            <Total tot = {part1.exercises + part2.exercises + part3.exercises} />
         </div>
     )
 }
+
 
 export default App
