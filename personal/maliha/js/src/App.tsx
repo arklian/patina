e3b02dab315810087a8ce0990c25257787caa98a
MalihaT111 <90735020+MalihaT111@users.noreply.github.com>
2024-06-18 16:21:25 -0400

    Finished 1.3

--- personal/maliha/js/src/App.tsx
+++ personal/maliha/js/src/App.tsx
@@ -1,4 +1,5 @@
 const Header = (props) => {
+    console.log(props)
     return (
         <>
             <h1>{props.course}</h1>
@@ -30,20 +31,25 @@ const Total = (props) => {
 }
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
-
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1} exercises1={exercises1}
-                     part2 = {part2} exercises2={exercises2}
-                     part3 = {part3} exercises3={exercises3}/>
-            <Total one={exercises1} two ={exercises2} three={exercises3}/>
+            <Content part1={part1.name} exercises1={part1.exercises}
+                     part2={part2.name} exercises2={part2.exercises}
+                     part3={part3.name} exercises3={part3.exercises}/>
+            <Total one={part1.exercises} two ={part2.exercises} three={part3.exercises}/>
         </div>
     )
 }
