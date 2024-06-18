5529b725c4b32a4d2f579ac5263e8f09896657f2
Kelly Lin <linkelly150@gmail.com>
2024-06-18 14:48:38 -0400

    Added console log
    Added console log

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,7 +1,15 @@
 const App = () => {
+    const now = new Date()
+    const a = 10
+    const b = 20
+    console.log(now, a+b)
+
     return (
         <div>
-            <p>Hello world</p>
+            <p>Hello world, it is {now.toString()}</p>
+            <p>
+                {a} plus {b} is {a + b}
+            </p>
         </div>
     )
 }
