c08608df8409deb928a1082dd84ed46ec60a105d
Henry Chen <hjc77@cornell.edu>
2024-06-18 14:51:24 -0400

    Prettier: Add JS/TS autoformatting
    Autoformatting is great, since it prevents people from needing to nit
    about various stylistic choices about the code in pull requests and
    also reduces the amount of diffs that are not related to the logic of
    the code and how it runs.
    
    Using prettier here as it's a popular autoformatter.
    
    If there are other options that people would prefer, we can edit the
    settings and reformat the world when needed.

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -9,11 +9,25 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a
+          href="https://vitejs.dev"
+          target="_blank"
+        >
+          <img
+            src={viteLogo}
+            className="logo"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://react.dev"
+          target="_blank"
+        >
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
