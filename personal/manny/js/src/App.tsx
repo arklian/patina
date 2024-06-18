689a1050f4139f368bd9b4a4a199bae1667e28c4
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-06-18 14:59:25 -0400

    Manny: exercise 1.1 complete

--- personal/manny/js/src/App.tsx
+++ personal/manny/js/src/App.tsx
@@ -1,25 +1,53 @@
+const Header = (props) => {
+    return(
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
 
-import { useState } from 'react'
-import './App.css'
+const Content = (props) =>{
+    return(
+        <div>
+            <p>
+                {props.part1} {props.exercises1}
+            </p>
+            <p>
+                {props.part2} {props.exercises2}
+            </p>
+            <p>
+                {props.part3} {props.exercises3}
+            </p>
+        </div>
+    )
+}
 
-function App() {
-    const [count, setCount] = useState(0)
+const Total = (props) => {
+    return (
+        <div>
+            <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        </div>
+    )
+}
+
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
     return (
-        <>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <div>
+            <Header course={course} />
+            <Content part1 = {part1} exercises1 = {exercises1} />
+            <Content part2 = {part2} exercises2={exercises2} />
+            <Content  part3={part3} exercises3={exercises3}/>
+            <Total exercises1 = {exercises1} exercises2={exercises2} exercises3={exercises3}/>
+        </div>
     )
 }
 
