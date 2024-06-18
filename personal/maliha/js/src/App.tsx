122b39e38ae4907d1008e63401c2b48b261dfb85
MalihaT111 <90735020+MalihaT111@users.noreply.github.com>
2024-06-18 16:24:17 -0400

    Finished 1.4

--- personal/maliha/js/src/App.tsx
+++ personal/maliha/js/src/App.tsx
@@ -31,25 +31,27 @@ const Total = (props) => {
 }
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ]
     return (
         <div>
             <Header course={course} />
-            <Content part1={part1.name} exercises1={part1.exercises}
-                     part2={part2.name} exercises2={part2.exercises}
-                     part3={part3.name} exercises3={part3.exercises}/>
-            <Total one={part1.exercises} two ={part2.exercises} three={part3.exercises}/>
+            <Content part1={parts[0].name} exercises1={parts[0].exercises}
+                     part2={parts[1].name} exercises2={parts[1].exercises}
+                     part3={parts[2].name} exercises3={parts[2].exercises}/>
+            <Total one={parts[0].exercises} two ={parts[1].exercises} three={parts[2].exercises}/>
         </div>
     )
 }
