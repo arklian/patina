f2ed680e597bc445a59420718706ae3e658b906e
Amanda Ruan <amandaruan262@gmail.com>
2024-06-18 15:17:48 -0400

    Amanda: Finished Exercise 1.4

--- personal/amanda/js/src/App.tsx
+++ personal/amanda/js/src/App.tsx
@@ -26,35 +26,37 @@ function Content(props: {parts: {name: string, exercises: number}[]}){
     )
 }
 
-function Total(props: {total: number}){
+function Total(props: {parts: {name: string, exercises: number}[]}){
+    const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
     return(
         <div>
-            <p>Number of exercises {props.total}</p>
+            <p>Number of exercises {total}</p>
         </div>
     )
 }
 
 function App() {
   const course = 'Half Stack application development';
-  const part1 = {
-      name: 'Fundamentals of React',
-      exercises: 10
-  };
-  const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-  };
-  const part3 = {
-      name: 'State of a component',
-      exercises: 14
-  };
-  const parts : {name: string, exercises: number}[] = [part1, part2, part3];
+  const parts: {name: string, exercises: number}[] = [
+      {
+          name: 'Fundamentals of React',
+          exercises: 10
+      },
+      {
+          name: 'Using props to pass data',
+          exercises: 7
+      },
+      {
+          name: 'State of a component',
+          exercises: 14
+      }
+      ];
 
   return (
       <div>
           <Header course={course}></Header>
           <Content parts={parts}></Content>
-          <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}></Total>
+          <Total parts={parts}></Total>
       </div>
   )
 }
