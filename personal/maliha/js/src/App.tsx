21a79050f920a07f51a4637fd47ac187a1b9b008
MalihaT111 <90735020+MalihaT111@users.noreply.github.com>
2024-06-18 19:57:39 -0400

    Finished 1.5
    fixed inconsistent spacing + extra tags
    
    trying to fix type error
    
    fixed 1.5
    
    started states

--- personal/maliha/js/src/App.tsx
+++ personal/maliha/js/src/App.tsx
@@ -1,58 +1,32 @@
-const Header = (props) => {
-    console.log(props)
-    return (
-        <>
-            <h1>{props.course}</h1>
-        </>
-    )
-}
-const Part = (props) =>{
-    return(
-        <>
-           <p>{props.parts} {props.exercises}</p>
-        </>
-    )
-}
-const Content = (props) => {
-    return (
-        <>
-            <Part parts={props.part1} exercises={props.exercises1}/>
-            <Part parts={props.part2} exercises={props.exercises2}/>
-            <Part parts={props.part3} exercises={props.exercises3}/>
-        </>
-    )
-}
-const Total = (props) => {
-    return(
-        <>
-            <p>Number of exercises {props.one + props.two + props.three}</p>
-        </>
-    )
-}
+// import { useState } from 'react'
+//
+// const App = () => {
+//     // save clicks of each button to its own state
+//     const [good, setGood] = useState(0)
+//     const [neutral, setNeutral] = useState(0)
+//     const [bad, setBad] = useState(0)
+//
+//     return (
+//         <div>
+//             <h1>give feedback</h1>
+//             <h1>statistics</h1>
+//         </div>
+//     )
+// }
+//
+// export default App
+import { useState } from 'react'
+
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const [ counter, setCounter ] = useState(0)
+
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    )
+
     return (
-        <div>
-            <Header course={course} />
-            <Content part1={parts[0].name} exercises1={parts[0].exercises}
-                     part2={parts[1].name} exercises2={parts[1].exercises}
-                     part3={parts[2].name} exercises3={parts[2].exercises}/>
-            <Total one={parts[0].exercises} two ={parts[1].exercises} three={parts[2].exercises}/>
-        </div>
+        <div>{counter}</div>
     )
 }
 
