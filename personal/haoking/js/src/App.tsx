bb666a11975ca89a566f51455608405b995b9341
Haoking Luo <hao5k6032@gmail.com>
2024-06-18 16:18:01 -0400

    Haoking Luo: Exercises 1.1-1.5!!

--- personal/haoking/js/src/App.tsx
+++ personal/haoking/js/src/App.tsx
@@ -1,74 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const Hello = (params) => {
-    console.log(params)
+const Hello = ( {name, age} ) => {
+    const bornYear = () => new Date().getFullYear()-age
     return (
         <div>
-            <p>Hello {params.field1} you are {params.field2}</p>
+            <p>
+                Hello {name}, you are {age} years old
+            </p>
+            <p>U are born in {bornYear()}</p>
         </div>
     )
 }
 
-const Footer = () => {
+const App = () => {
+    const name = 'Peter'
+    const age = 10
+
     return (
-        <>
-            <div>
-                custom footer by me!
-            </div>
-        </>
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="Maya" age={26 + 10} />
+            <Hello name={name} age={age} />
+        </div>
     )
-}
-
-function App() {
-  const [count, setCount] = useState(0)
-    const now = new Date()
-
-    console.log("hello from console")
-    console.log(now)
-
-    const name:string = 'johnathan'
-    const age:number = 26
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-          <div>
-              <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-              </button>
-          </div>
-            <div>
-                <button onClick={() => alert("saoifj")}> click </button>
-            </div>
-          <div>
-              <p>hello wolrd it is now {now.toString()}</p>
-          </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-        <h1> Greetings </h1>
-        <Hello field1='george'field2={26+36} />
-        <Hello field1='john' field2={16*2}/>
-        <Hello field1={name} field2={age} />
-        <Footer/>
-    </>
-  )
 }
 
 export default App
