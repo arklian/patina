8417d6b93af50fd93f191c5d6154fbe7413e553c
Henry Chen <hjc77@cornell.edu>
2024-06-18 18:41:06 -0400

    Henry: Add new page

--- js/src/Router.tsx
+++ js/src/Router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { HomePage } from './pages/Home.page'
+import { HenryPage } from './personal/_henry/Henry.page.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/henry',
+    element: <HenryPage />,
   },
 ])
 
