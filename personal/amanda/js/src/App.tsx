b5ff42542af0e6240dd25816eedcf3e75334c300
Amanda Ruan <amandaruan262@gmail.com>
2024-06-19 12:45:39 -0400

    Amanda: Finished Exercise 1.12

--- personal/amanda/js/src/App.tsx
+++ personal/amanda/js/src/App.tsx
@@ -1,80 +1,35 @@
 import {useState, MouseEventHandler} from 'react';
 import './App.css';
 
-function SectionTitle ({text}:{text:string}) {
-    return(
-        <h1>{text}</h1>
-    )
-}
-
 function Button ({text, onClick}: {text: string, onClick: MouseEventHandler}){
     return(
         <button onClick={onClick}>{text}</button>
     )
 }
 
-function StatisticLine ({text, value}: {text: string, value: number}){
-    return(
-        <tr>
-            <td>{text}</td>
-            <td>{value}</td>
-        </tr>
-    )
-}
-
-function Statistics ({good, neutral, bad}: {good: number, neutral: number, bad: number}){
-    const total:number = good + neutral + bad;
-    const average:number = (good - bad) / total;
-    const positivePercentage:number = good / total * 100;
-    if (total === 0){
-        return(
-            <p>No feedback given</p>
-        )
-    }
-    return(
-        <table>
-            <tbody>
-                <StatisticLine text="Good" value={good}></StatisticLine>
-                <StatisticLine text="Neutral" value={neutral}></StatisticLine>
-                <StatisticLine text="Total" value={total}></StatisticLine>
-                <StatisticLine text="Average" value={average}></StatisticLine>
-                <StatisticLine text="Positive (%)" value={positivePercentage}></StatisticLine>
-            </tbody>
-        </table>
-    )
-}
-
 function App() {
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
-
-    const handleClickGood = () => {
-        const newGood: number = good + 1;
-        console.log("current good: " + good);
-        return (() => setGood(newGood))
-    };
-
-    const handleClickNeutral = () => {
-        const newNeutral:number = neutral + 1;
-        console.log("current neutral: " + neutral);
-        return (() => setNeutral(newNeutral))
+    const anecdotes:string[] = [
+        'If it hurts, do it more often.',
+        'Adding manpower to a late software project makes it later!',
+        'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        'Premature optimization is the root of all evil.',
+        'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        'The only way to go fast, is to go well.'
+    ];
+    const [selected, setSelected] = useState(0);
+    const handleClick = () => {
+        let randNum:number = selected;
+        while (selected === randNum) {
+            randNum = Math.round(Math.random() * (anecdotes.length - 1));
+        }
+        return(() => setSelected(randNum))
     }
-
-    const handleClickBad = () => {
-        const newBad:number = bad + 1;
-        console.log("current bad: " + bad);
-        return (() => setBad(newBad))
-    }
-
     return (
       <div>
-        <SectionTitle text="Give Feedback"></SectionTitle>
-        <Button text="good" onClick={handleClickGood()}></Button>
-        <Button text="neutral" onClick={handleClickNeutral()}></Button>
-        <Button text="bad" onClick={handleClickBad()}></Button>
-        <SectionTitle text="Statistics"></SectionTitle>
-        <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+        <p>{anecdotes[selected]}</p>
+        <Button text="next anecdote" onClick={handleClick()}></Button>
       </div>
     )
 }
