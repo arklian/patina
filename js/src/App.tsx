3b08520e7bca55fa81e6a6195a700468bd35523a
Kelly Lin <linkelly150@gmail.com>
2024-06-20 09:43:35 -0400

    Exercise 1.1 End

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,27 +1,45 @@
 const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const course = 'Half Stack application development';
+    const parts = [
+        { name: 'Fundamentals of React', exercises: 10 },
+        { name: 'Using props to pass data', exercises: 7 },
+        { name: 'State of a component', exercises: 14 }
+    ];
+
+    // @ts-ignore
+    const Header = ({ course }) => {
+        return (
+            <h1>{course}</h1>
+        );
+    };
+
+    // @ts-ignore
+    const Content = ({ parts }) => {
+        return (
+            <div>
+                <p>{parts[0].name} {parts[0].exercises}</p>
+                <p>{parts[1].name} {parts[1].exercises}</p>
+                <p>{parts[2].name} {parts[2].exercises}</p>
+            </div>
+        );
+    };
+
+    // @ts-ignore
+    const Total = ({ parts }) => {
+        const totalExercises = parts.reduce((sum: any, part: { exercises: any; }) => sum + part.exercises, 0);
+        return (
+            <p>Number of exercises {totalExercises}</p>
+        );
+    };
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header course={course} />
+            <Content parts={parts} />
+            <Total parts={parts} />
         </div>
-    )
-}
+    );
+};
+
 
 export default App
