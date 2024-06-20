089809da180eda7c4172996173995ff37f6664a2
Jionghao Wu <jw6643@nyu.edu>
2024-06-20 17:15:57 -0400

    Jionghao: Finished 1.5
    Jionghao: trying stateful components
    
    Jionghao: interacting with events

--- personal/Jionghao/js/src/App.tsx
+++ personal/Jionghao/js/src/App.tsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 const Header = (props) => {
@@ -36,27 +36,41 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const [ counter, setCounter ] = useState(0)
+    // setTimeout(
+    //     () => setCounter(counter + 1),
+    //     1000
+    // )
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header course = {course}/>
-            <Content parts = {parts}/>
-            <Total parts = {parts} />
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+            <p>{counter}</p>
+            <button onClick={() => setCounter(counter + 1)}>
+                plus
+            </button>
+            <button onClick={() => setCounter(0)}>
+                zero
+            </button>
         </div>
     )
 }
