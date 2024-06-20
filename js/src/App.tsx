eebe143a4d2b9c471391cba3cdd2b848cb2387fa
Kelly Lin <linkelly150@gmail.com>
2024-06-20 09:25:37 -0400

    Change components to props

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,16 +1,25 @@
-const Hello = () => {
+
+
+const Hello = (props) => {
+    console.log(props)
     return (
         <div>
-            <p>Hello world</p>
+            <p>
+                Hello {props.name}, you are {props.age} years old
+            </p>
         </div>
     )
 }
 
 const App = () => {
+    const name = 'Peter'
+    const age = 10
+
     return (
         <div>
             <h1>Greetings</h1>
-            <Hello />
+            <Hello name='Maya' age={26 + 10} />
+            <Hello name={name} age={age} />
         </div>
     )
 }
