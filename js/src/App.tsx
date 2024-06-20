6b9516a964d2bcd740d17b537bcc4d1bcf6d63ed
Bryan Zhang <bz5989@princeton.edu>
2024-06-20 15:55:11 -0400

    Bryan: Earliest change to base file
    Just committing this to allow for a pull

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -4,31 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(2)
+  const [count2, setCount2] = useState(2)
+  const [count3, setCount3] = useState(2)
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div>
+              <a href="https://vitejs.dev" target="_blank">
+                  <img src={viteLogo} className="logo" alt="Vite logo"/>
+              </a>
+              <a href="https://react.dev" target="_blank">
+                  <img src={reactLogo} className="logo react" alt="React logo"/>
+              </a>
+          </div>
+          <h1>Mathematical Form</h1>
+          <div className="thing">
+              <button onClick={() => setCount2((count2: number) => count + count2)}>
+                  count2 is {count2} and {count2 / count}
+              </button>
+          </div>
+          <div className="thing2">
+              <button onClick={() => setCount3((count3: number) => count3 + count + count2)}>
+                  count3 is {count3} and {count3 / (count+count2)}
+              </button>
+          </div>
+          <div className="card">
+              <button onClick={() => setCount((count: number) => count2 + count)}>
+                  count is {count} and {count / count2}
+              </button>
+              <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+          </div>
+          <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+          </p>
+      </>
   )
 }
 
