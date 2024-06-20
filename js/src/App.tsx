1f3a9dcca0b3814d549c6f35ac4cb1f54ea0b83b
Kelly Lin <linkelly150@gmail.com>
2024-06-20 09:29:32 -0400

    Exercise 1.1 Begin

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,25 +1,25 @@
-
-
-const Hello = (props) => {
-    console.log(props)
-    return (
-        <div>
-            <p>
-                Hello {props.name}, you are {props.age} years old
-            </p>
-        </div>
-    )
-}
-
 const App = () => {
-    const name = 'Peter'
-    const age = 10
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
     return (
         <div>
-            <h1>Greetings</h1>
-            <Hello name='Maya' age={26 + 10} />
-            <Hello name={name} age={age} />
+            <h1>{course}</h1>
+            <p>
+                {part1} {exercises1}
+            </p>
+            <p>
+                {part2} {exercises2}
+            </p>
+            <p>
+                {part3} {exercises3}
+            </p>
+            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
         </div>
     )
 }
