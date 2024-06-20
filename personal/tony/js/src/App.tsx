4401a821bcee1c8b386144dcd4587c529c94e9ed
Tony Wang <wangtony4005@gmail.com>
2024-06-20 14:49:10 -0400

    Checking out page rerendering

--- personal/tony/js/src/App.tsx
+++ personal/tony/js/src/App.tsx
@@ -1,56 +1,10 @@
-const CourseName = ({ course }) => {
+
+
+const App = (props) => {
+    const {counter} = props
     return (
-        <div>
-            <h1>{course.name}</h1>
-        </div>
+        <div>{counter}</div>
     )
 }
-
-const Content = ({ course }) => {
-    return (
-        <div>
-            <p>{course.parts[0].name} {course.parts[0].exercises}</p>
-            <p>{course.parts[1].name} {course.parts[1].exercises}</p>
-            <p>{course.parts[2].name} {course.parts[2].exercises}</p>
-        </div>
-    )
-}
-
-const Total = ({ course }) => {
-    return (
-        <div>
-            <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-        </div>
-    )
-}
-
-const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
-
-    return (
-        <div>
-            <CourseName course = {course} />
-            <Content course = {course} />
-            <Total course = {course} />
-        </div>
-    )
-}
-
 
 export default App
--- personal/tony/js/src/main.tsx
+++ personal/tony/js/src/main.tsx
@@ -2,4 +2,16 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+let counter = 1
+
+const refresh = () => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+        <App counter={counter} />
+    )
+}
+
+refresh()
+counter += 1
+refresh()
+counter += 1
+refresh()
