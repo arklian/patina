bd484325bd5d3c97d054d188ef265f41db2d5e6f
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-06-20 01:00:41 -0400

    Manny: exercise 1.7 complete

--- personal/manny/js/src/App.tsx
+++ personal/manny/js/src/App.tsx
@@ -5,19 +5,41 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+
+    const handleGoodClick = () => {
+        const updatedGood = good + 1
+        setGood(updatedGood)
+        setAll(updatedGood+neutral+bad)
+    }
+
+    const handleNeutralClick = () => {
+        const updatedNeutral = neutral + 1
+        setNeutral(updatedNeutral)
+        setAll(good+updatedNeutral+bad)
+    }
+
+    const handleBadClick = () => {
+        const updatedBad = bad + 1
+        setBad(updatedBad)
+        setAll(good+neutral+updatedBad)
+    }
+
+    const average = all === 0?0 : (good-bad)/all
+    const posPer = all === 0?0 : (good/all)*100
 
     return (
         <div>
             <h1>give feedback</h1>
-            <button onClick={() => setGood(good + 1)}>
+            <button onClick={handleGoodClick}>
                 good
             </button>
 
-            <button onClick={() => setNeutral(neutral + 1)}>
+            <button onClick={handleNeutralClick}>
                 neutral
             </button>
 
-            <button onClick={() => setBad(bad + 1)}>
+            <button onClick={handleBadClick}>
                 bad
             </button>
 
@@ -26,6 +48,9 @@ const App = () => {
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average}</p>
+            <p>positive {posPer}%</p>
         </div>
     )
 }
