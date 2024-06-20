56f4445878c12c87e682d7f37f8bfbe3414d5a5f
Kelly Lin <linkelly150@gmail.com>
2024-06-20 10:47:23 -0400

    Exercise 1.1 End (modified)

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,42 +1,46 @@
+// @ts-ignore
+const Header = ({props}) => {
+    return (
+        <>
+            <h1>{props.course}</h1>
+        </>
+    )
+}
+
+// @ts-ignore
+const Content = (props) => {
+    return (
+        <>
+            <p>{props.parts} {props.exercises}</p>
+
+        </>
+    )
+}
+// @ts-ignore
+const Total = (props) => {
+    return(
+        <>
+            <p>Number of exercises {props.one + props.two + props.three}</p>
+        </>
+    )
+}
+
 const App = () => {
-    const course = 'Half Stack application development';
-    const parts = [
-        { name: 'Fundamentals of React', exercises: 10 },
-        { name: 'Using props to pass data', exercises: 7 },
-        { name: 'State of a component', exercises: 14 }
-    ];
-
-    // @ts-ignore
-    const Header = ({ course }) => {
-        return (
-            <h1>{course}</h1>
-        );
-    };
-
-    // @ts-ignore
-    const Content = ({ parts }) => {
-        return (
-            <div>
-                <p>{parts[0].name} {parts[0].exercises}</p>
-                <p>{parts[1].name} {parts[1].exercises}</p>
-                <p>{parts[2].name} {parts[2].exercises}</p>
-            </div>
-        );
-    };
-
-    // @ts-ignore
-    const Total = ({ parts }) => {
-        const totalExercises = parts.reduce((sum: any, part: { exercises: any; }) => sum + part.exercises, 0);
-        return (
-            <p>Number of exercises {totalExercises}</p>
-        );
-    };
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course = {course}/>
+            <Content part = {part1} exercises={exercises1}/>
+            <Content part = {part2} exercises={exercises2}/>
+            <Content part = {part3} exercises={exercises3}/>
+            <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
         </div>
     );
 };
