d132d9393fd0859b2140c4f2b5afc923f9efd893
Kelly Lin <linkelly150@gmail.com>
2024-06-20 09:17:46 -0400

    Used multiple components

--- js/src/App.tsx
+++ js/src/App.tsx
@@ -1,15 +1,16 @@
-const App = () => {
-    const now = new Date()
-    const a = 10
-    const b = 20
-    console.log(now, a+b)
-
+const Hello = () => {
     return (
         <div>
-            <p>Hello world, it is {now.toString()}</p>
-            <p>
-                {a} plus {b} is {a + b}
-            </p>
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+const App = () => {
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello />
         </div>
     )
 }
