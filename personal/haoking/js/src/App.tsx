b21b3fd31d278ac0658b10fa8f75d95fd53e952a
Haoking Luo <hao5k6032@gmail.com>
2024-06-19 23:40:11 -0400

    Haoking: conditional rendering

--- personal/haoking/js/src/App.tsx
+++ personal/haoking/js/src/App.tsx
@@ -1,25 +1,53 @@
 import { useState } from 'react'
 
-const Display = ({counter}) => <div>{counter}</div>
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        )
+    }
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
 
-
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Button = ({ handleClick, text } ) => (<button onClick={handleClick}>{text}</button>)
 
 const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
 
-    const [ counter, setCounter ] = useState(0)
+    const [total, setTotal] = useState(0)
 
-    const incrementCtr = () => setCounter(counter+1)
-    const resetCtr = () => setCounter(0)
-    const decrementCtr = () => setCounter(counter-1)
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        const updatedLeft = left+1
+        setLeft(updatedLeft)
+        setTotal(updatedLeft+right)
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        const updatedRight = right+1
+        setRight(updatedRight)
+        setTotal(left+updatedRight)
+    }
 
     return (
-        <>
-            <Display counter = {counter}/>
-            <Button text = "inc" onClick = {incrementCtr} />
-            <Button onClick = {resetCtr} text = "reset"/>
-            <Button onClick={decrementCtr} text = "dec"/>
-        </>
+        <div>
+            {left}
+            <Button handleClick={handleLeftClick} text="left"/>
+            <Button handleClick={handleRightClick} text="right"/>
+            {right}
+            <History allClicks={allClicks}/>
+
+            <p>total {total}</p>
+        </div>
     )
 }
 
