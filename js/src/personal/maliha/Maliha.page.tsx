c3598592e825cce9f47bcbcc8b92ca3e2bc1dd8c
Maliha Tasnim <ayesiddika@gmail.com>
2024-06-24 18:32:54 -0400

    Maliha: Imports gsap library, fixed .body issue, added a menu
    Also fixed linting issues
    
    L=ENG-70
    TEST=manual
    
    Ran locally and menu icon changes

--- js/src/personal/maliha/Maliha.page.tsx
+++ js/src/personal/maliha/Maliha.page.tsx
@@ -1,22 +1,24 @@
 import { Title, Text } from '@mantine/core'
 import styles from './Maliha.module.css'
+import { Menu } from './Menu.tsx'
 
 export function MalihaPage() {
   return (
-    <>
+    <div className={styles.body}>
+      <Menu />
       <Title
         className={styles.title}
         ta="center"
         mt={100}
       >
-        Welcome to{' '}
+        {'Welcome to'}
         <Text
           inherit
           variant="gradient"
           component="span"
-          gradient={{ from: 'green', to: 'blue' }}
+          gradient={{ from: 'red', to: 'blue' }}
         >
-          Maliha's Page
+          {"Maliha's Page"}
         </Text>
       </Title>
       <Text
@@ -27,8 +29,8 @@ export function MalihaPage() {
         mx="auto"
         mt="xl"
       >
-        Hey! I'm Maliha.
+        {"Hey, I'm Maliha"}
       </Text>
-    </>
+    </div>
   )
 }
