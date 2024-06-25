69b31f00795912afe88cbc8515865cc0739cb2b9
Jionghao Wu <jw6643@nyu.edu>
2024-06-25 15:02:24 -0400

    Jionghao: Added button to switch light/dark modes
    Implemented button for light/dark modes
    and started a footer for social medias
    
    L=ENG-71

---
+++ js/src/personal/jionghao/FooterSocial.tsx
@@ -0,0 +1,53 @@
+import { Container, Group, ActionIcon, rem } from '@mantine/core'
+import {
+  IconBrandGithub,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+} from '@tabler/icons-react'
+import classes from './FooterSocial.module.css'
+
+export function FooterSocial() {
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Group
+          gap={0}
+          className={classes.links}
+          justify="space-between"
+          wrap="nowrap"
+        >
+          <ActionIcon
+            size="lg"
+            color="gray"
+            variant="subtle"
+          >
+            <IconBrandLinkedin
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon
+            size="lg"
+            color="gray"
+            variant="subtle"
+          >
+            <IconBrandGithub
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon
+            size="lg"
+            color="gray"
+            variant="subtle"
+          >
+            <IconBrandInstagram
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </div>
+  )
+}
--- js/src/personal/jionghao/Jionghao.page.tsx
+++ js/src/personal/jionghao/Jionghao.page.tsx
@@ -1,34 +1,61 @@
-import { Title, Text } from '@mantine/core'
+import {
+  Title,
+  Text,
+  Button,
+  useMantineColorScheme,
+  MantineColorScheme,
+} from '@mantine/core'
+import { useToggle } from '@mantine/hooks'
+import { IconSunHigh } from '@tabler/icons-react'
 import styles from './Jionghao.module.css'
+import { FooterSocial } from './FooterSocial.tsx'
 
 export function JionghaoPage() {
+  const { setColorScheme } = useMantineColorScheme()
+  const [modeValue, toggle] = useToggle(['light', 'dark'])
+  const changeMode = () => {
+    toggle()
+    setColorScheme(modeValue as MantineColorScheme)
+  }
   return (
-    <>
-      <Title
-        className={styles.title}
-        ta="center"
-        mt={100}
-      >
-        Welcome to{' '}
-        <Text
-          inherit
-          variant="gradient"
-          component="span"
-          gradient={{ from: 'green', to: 'blue' }}
+    <div>
+      <div className={styles.pageContainer}>
+        <Title
+          className={styles.title}
+          ta="center"
+          mt={100}
         >
-          Jionghao's Page
+          {'Welcome to '}
+          <Text
+            inherit
+            variant="gradient"
+            gradient={{ from: 'green', to: 'blue' }}
+          >
+            {"Jionghao's Page"}
+          </Text>
+        </Title>
+
+        <Text
+          c="dimmed"
+          ta="center"
+          size="lg"
+          maw={580}
+          mx="auto"
+          mt="xl"
+        >
+          {"Hey! I'm Jionghao."}
         </Text>
-      </Title>
-      <Text
-        c="dimmed"
-        ta="center"
-        size="lg"
-        maw={580}
-        mx="auto"
-        mt="xl"
-      >
-        Hey! I'm Jionghao.
-      </Text>
-    </>
+        <div className={styles.buttonWrapper}>
+          <Button
+            size={'sm'}
+            onClick={changeMode}
+          >
+            <IconSunHigh size={25} />{' '}
+          </Button>
+        </div>
+      </div>
+
+      <FooterSocial />
+    </div>
   )
 }
