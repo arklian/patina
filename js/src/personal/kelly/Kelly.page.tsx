c432b8647af8d236b2bbd1d9525b7228d7ca6630
Kelly Lin <linkelly150@gmail.com>
2024-06-25 13:04:40 -0400

    Kelly: Updated title of webpage and fonts

--- js/src/personal/kelly/Kelly.page.tsx
+++ js/src/personal/kelly/Kelly.page.tsx
@@ -1,34 +1,12 @@
-import { Title, Text } from '@mantine/core'
+import { Title } from '@mantine/core'
 import styles from './Kelly.module.css'
 
 export function KellyPage() {
   return (
     <>
-      <Title
-        className={styles.title}
-        ta="center"
-        mt={100}
-      >
-        Welcome to{' '}
-        <Text
-          inherit
-          variant="gradient"
-          component="span"
-          gradient={{ from: 'green', to: 'blue' }}
-        >
-          Kelly's Page
-        </Text>
-      </Title>
-      <Text
-        c="dimmed"
-        ta="center"
-        size="lg"
-        maw={580}
-        mx="auto"
-        mt="xl"
-      >
-        Hey! I'm Kelly.
-      </Text>
+      <div className="title_container">
+        <Title className={styles.title}>{'KELLYLIN'}</Title>
+      </div>
     </>
   )
 }
