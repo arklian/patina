85f2ed88355d36502ab8c7281b270ceb7f7eddb2
Tony Wang <wangtony4005@gmail.com>
2024-06-25 15:56:20 -0400

    Tony: Added Page layout and image
    Added react icons library to make social buttons that link
    to socials.
    
    Split up page into 2 grids, left side with name and image
    right side with temporary paragraph for about me.
    
    Expanded grid to fit entire viewport.
    
    L=ENG-24
    L=ENG-73
    L=ENG-86

--- js/src/personal/tony/Tony.page.tsx
+++ js/src/personal/tony/Tony.page.tsx
@@ -1,35 +1,67 @@
-import { Title, Text } from '@mantine/core'
+import { Title, Text, Grid, Image, ActionIcon } from '@mantine/core'
+import { FaGithub, FaEnvelope, FaLinkedin } from 'react-icons/fa'
 import styles from './Tony.module.css'
 
+//TonyPage Component
 export function TonyPage() {
   return (
     <>
-      <div className={styles.background}>
-        <Title
-          className={styles.title}
-          ta="left"
-          mt={50}
-        >
-          <Text
-            inherit
-            variant="gradient"
-            component="span"
-            gradient={{ from: 'blue', to: 'green' }}
+      <Grid
+        gutter="md"
+        className={styles.grid}
+      >
+        <Grid.Col span={6}>
+          <Title
+            className={styles.title}
+            ta="left"
           >
-            Tony Wang
+            <Text
+              inherit
+              component="span"
+            >
+              {'Tony Wang'}
+            </Text>
+          </Title>
+          <Text className={styles.text}>{'Software Engineer Intern'}</Text>
+          <Image
+            className={styles.pfp}
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+            alt="Profile"
+          />
+          <div className={styles.socialIcons}>
+            <ActionIcon
+              component="a"
+              href="https://github.com/wangtony4005"
+              target="_blank"
+              size="lg"
+            >
+              <FaGithub size={24} />
+            </ActionIcon>
+            <ActionIcon
+              component="a"
+              href="mailto:wangtony4005@gmail.com"
+              size="lg"
+            >
+              <FaEnvelope size={24} />
+            </ActionIcon>
+            <ActionIcon
+              component="a"
+              href="https://linkedin.com/in/wangtony4005"
+              target="_blank"
+              size="lg"
+            >
+              <FaLinkedin size={24} />
+            </ActionIcon>
+          </div>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <Text className={styles.about}>
+            {
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
           </Text>
-        </Title>
-        <Text
-          c="dimmed"
-          ta="center"
-          size="lg"
-          maw={580}
-          mx="auto"
-          mt="xl"
-        >
-          Hey! I'm Tony.
-        </Text>
-      </div>
+        </Grid.Col>
+      </Grid>
     </>
   )
 }
