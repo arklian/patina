a38a3a897d895f115878529091249f3c6238513b
Henry Chen <hjc77@cornell.edu>
2024-06-26 19:19:07 -0400

    Mantine: Updated theme colors
    Just picked some green color that sort of matches the ones in the
    kickoff slide deck.

--- js/src/theme.ts
+++ js/src/theme.ts
@@ -1,5 +1,23 @@
 import { createTheme } from '@mantine/core'
 
+/** Custom mantine theme override here
+ * {@link https://mantine.dev/theming/theme-object/}
+ */
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    patina: [
+      '#effbf4',
+      '#def5e6',
+      '#b7eaca',
+      '#8cdfad',
+      '#6ad693',
+      '#55d184',
+      '#4acf7b',
+      '#3cb668',
+      '#31a25c',
+      '#218c4c',
+    ],
+  },
+
+  primaryColor: 'patina',
 })
