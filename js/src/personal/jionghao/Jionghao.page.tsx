2ce717e8ac0ea67b950571c8b5618fea506678e4
Jionghao Wu <jw6643@nyu.edu>
2024-06-27 17:40:25 -0400

    Jionghao: Add about me section
    L=ENG-97

--- js/src/personal/jionghao/Jionghao.page.tsx
+++ js/src/personal/jionghao/Jionghao.page.tsx
@@ -1,9 +1,10 @@
-import { Title, Text, MantineProvider } from '@mantine/core'
+import { MantineProvider } from '@mantine/core'
 import styles from './Jionghao.module.css'
 import { theme } from './theme.ts'
 import { FooterSocial } from './components/FooterSocial.tsx'
 import { Header } from './components/Header.tsx'
 import { Hero } from './components/Hero.tsx'
+import { About } from './components/About.tsx'
 
 export function JionghaoPage() {
   return (
@@ -11,31 +12,7 @@ export function JionghaoPage() {
       <div className={styles.pageContainer}>
         <Header />
         <Hero />
-        <Title
-          className={styles.title}
-          ta="center"
-          mt={100}
-        >
-          {'Welcome to '}
-          <Text
-            inherit
-            variant="gradient"
-            gradient={{ from: 'green', to: 'blue' }}
-          >
-            {"Jionghao's Page"}
-          </Text>
-        </Title>
-
-        <Text
-          c="dimmed"
-          ta="center"
-          size="lg"
-          maw={580}
-          mx="auto"
-          mt="xl"
-        >
-          {"Hey! I'm Jionghao."}
-        </Text>
+        <About />
       </div>
       <FooterSocial />
     </MantineProvider>
