7d600b2b430fe49bde915c1789c248761fdd91ed
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-06-27 16:55:20 -0400

    Manny: I re-added the image to the page and made it a object that when hovered over gives a menu

--- js/src/personal/manny/Manny.page.tsx
+++ js/src/personal/manny/Manny.page.tsx
@@ -3,6 +3,7 @@ import {Title, Text, MantineProvider} from '@mantine/core'
 import styles from './Manny.module.css'
 import { theme } from './theme.ts'
 import {FooterSocial} from "@/personal/manny/components/FooterSocial.tsx";
+import {ClickableImage} from "@/personal/manny/components/ClickableImage.tsx";
 
 
 export function MannyPage() {
@@ -23,6 +24,7 @@ export function MannyPage() {
           {"Manny's Page"}
         </Text>
       </Title>
+      <ClickableImage />
       <FooterSocial />
 
     </MantineProvider>
