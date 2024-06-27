5d6d2e81e4ac11c659c55597e6fe6139507fc51d
Amanda Ruan <amandaruan262@gmail.com>
2024-06-27 14:27:02 -0400

    Amanda: Restructured code based on comments and to declutter main page
    I turned each page into its own component and modified
    the code to read from a json file. The color on the pages were also changed to not be the default Mantine color.
    
    L=ENG-87
    L=ENG-74

--- js/src/personal/amanda/Amanda.page.tsx
+++ js/src/personal/amanda/Amanda.page.tsx
@@ -1,43 +1,25 @@
-import { Button, Center, MantineProvider, Title } from '@mantine/core'
-import { useViewportSize } from '@mantine/hooks'
+import { MantineProvider } from '@mantine/core'
 import { useState } from 'react'
-import styles from './Amanda.module.css'
-import { AmandaMainPage } from './Amanda.main.page.tsx'
-import { theme } from './theme.ts'
+import { MainPage } from './Main.page.tsx'
+import { LandingPage } from './Landing.page.tsx'
+import { cssVariableResolver, theme } from './theme.ts'
+
+enum Page {
+  Landing,
+  Main,
+}
 
 export function AmandaPage() {
-  const { height, width } = useViewportSize()
-  const [landingPage, setLandingPage] = useState(1)
-
+  const [currentPage, setCurrentPage] = useState(Page.Landing)
   return (
-    <MantineProvider theme={theme}>
-      {landingPage === 1 && ( // render loading page
-        <div className={styles.landingPage}>
-          <Center
-            w={width}
-            h={height / 2}
-          >
-            <Title
-              className={styles.title}
-              ta="center"
-              mt={100}
-            >
-              {"Hi! I'm Amanda!"}
-            </Title>
-          </Center>
-          <Center>
-            <Button
-              bg="none"
-              variant="outline"
-              color="white"
-              onClick={() => setLandingPage(0)}
-            >
-              {'start'}
-            </Button>
-          </Center>
-        </div>
+    <MantineProvider
+      theme={theme}
+      cssVariablesResolver={cssVariableResolver}
+    >
+      {currentPage === Page.Landing && (
+        <LandingPage onClick={() => setCurrentPage(Page.Main)} />
       )}
-      {landingPage === 0 && <AmandaMainPage />}
+      {currentPage === Page.Main && <MainPage />}
     </MantineProvider>
   )
 }
