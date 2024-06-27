55546d5185bd02019b446941bd34ee94666f5604
Jionghao Wu <jw6643@nyu.edu>
2024-06-27 00:32:33 -0400

    Jionghao: continued working on header
    L=ENG-34

--- js/src/personal/jionghao/components/Header.tsx
+++ js/src/personal/jionghao/components/Header.tsx
@@ -1,20 +1,42 @@
-import { Container, Group, Button, Flex } from '@mantine/core'
+import {
+  Container,
+  Group,
+  Button,
+  Flex,
+  useMantineColorScheme,
+  MantineColorScheme,
+} from '@mantine/core'
+import { IconSunHigh, IconMoon } from '@tabler/icons-react'
+import { useToggle } from '@mantine/hooks'
 import styles from './Header.module.css'
 
 export function Header() {
+  const { setColorScheme } = useMantineColorScheme()
+  const [modeValue, toggle] = useToggle(['light', 'dark'])
+  const changeMode = () => {
+    toggle()
+    setColorScheme(modeValue as MantineColorScheme)
+  }
+  const isDark = modeValue !== 'dark'
   return (
-    <header className={styles.header}>
-      <Container>
+    <div className={styles.header}>
+      <Container className={styles.mainContainer}>
         <Flex
-          justify="space-between"
+          justify="right"
           align="center"
         >
           <div className={styles.logo}>{'PlaceHolderLogo'}</div>
+        </Flex>
+        <Flex
+          justify="right"
+          align="center"
+        >
           <Group>
             <Button
               variant="subtle"
               component="a"
               href="#about"
+              color={'cyan'}
             >
               {'About'}
             </Button>
@@ -22,6 +44,7 @@ export function Header() {
               variant="subtle"
               component="a"
               href="#experience"
+              color={'cyan'}
             >
               {'Experience'}
             </Button>
@@ -29,19 +52,33 @@ export function Header() {
               variant="subtle"
               component="a"
               href="#work"
+              color={'cyan'}
             >
               {'Work'}
             </Button>
             <Button
-              variant="subtle"
+              variant="transparent"
               component="a"
               href="#contact"
+              color={'cyan'}
             >
               {'Contact'}
+            </Button>
+            <Button
+              className={styles.modeButton}
+              size={'compact-md'}
+              color={'gray'}
+              variant={'outline'}
+              onClick={changeMode}
+              radius={'xl'}
+            >
+              {isDark ?
+                <IconSunHigh size={24} />
+              : <IconMoon size={24} />}
             </Button>
           </Group>
         </Flex>
       </Container>
-    </header>
+    </div>
   )
 }
