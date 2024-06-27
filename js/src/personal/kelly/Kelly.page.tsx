e4a6b1a3a6eb435e100c630b40363f60c6987396
Kelly Lin <linkelly150@gmail.com>
2024-06-27 14:31:26 -0400

    Kelly: Updated website hero and added image
    L=ENG-25
    
    fixed code
    
    updates

--- js/src/personal/kelly/Kelly.page.tsx
+++ js/src/personal/kelly/Kelly.page.tsx
@@ -1,12 +1,34 @@
-import { MantineProvider, Title } from '@mantine/core'
+import { Box, MantineProvider, Title } from '@mantine/core'
 import styles from './Kelly.module.css'
 import { theme } from './theme.ts'
 
 export function KellyPage() {
   return (
     <MantineProvider theme={theme}>
-      <div className="title_container">
-        <Title className={styles.title}>{'KELLYLIN'}</Title>
+      <div className={styles.body_load}></div>
+      <div className={styles.nav}>
+        <a>
+          <p className={styles.nav_item}>{'About'}</p>
+        </a>
+        <a>
+          <p className={styles.nav_item}>{'Projects'}</p>
+        </a>
+        <a>
+          <p className={styles.nav_item}>{'Contact'}</p>
+        </a>
+      </div>
+      <div className={styles.body_container}>
+        <Title className={styles.title}>{'KELLY LIN'}</Title>
+        <div className={styles.window_container}>
+          <Box className={styles.window}></Box>
+        </div>
+      </div>
+      <div className={styles.about}>
+        <p>
+          {
+            'dummytext dummytext dummytext dummytext dummytext dummytext dummytext dummytext'
+          }
+        </p>
       </div>
     </MantineProvider>
   )
