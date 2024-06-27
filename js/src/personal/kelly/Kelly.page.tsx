4775f2ba1f9cc318f127edefe89076f96c1ce54f
Kelly Lin <linkelly150@gmail.com>
2024-06-27 14:01:02 -0400

    fixed code

--- js/src/personal/kelly/Kelly.page.tsx
+++ js/src/personal/kelly/Kelly.page.tsx
@@ -1,13 +1,27 @@
-import { MantineProvider, Title } from '@mantine/core'
+import { Box, Title } from '@mantine/core'
 import styles from './Kelly.module.css'
-import { theme } from './theme.ts'
 
 export function KellyPage() {
   return (
-    <MantineProvider theme={theme}>
-      <div className="title_container">
-        <Title className={styles.title}>{'KELLYLIN'}</Title>
+    <>
+      <div className={styles.body_load}></div>
+      <div className={styles.nav}>
+        <a>
+          <p className={styles.nav_item}>{'Home'}</p>
+        </a>
+        <a>
+          <p className={styles.nav_item}>{'Projects'}</p>
+        </a>
+        <a>
+          <p className={styles.nav_item}>{'About'}</p>
+        </a>
       </div>
-    </MantineProvider>
+      <div className={styles.body_container}>
+        <Title className={styles.title}>{'KELLY LIN'}</Title>
+        <div className={styles.window_container}>
+          <Box className={styles.window}></Box>
+        </div>
+      </div>
+    </>
   )
 }
