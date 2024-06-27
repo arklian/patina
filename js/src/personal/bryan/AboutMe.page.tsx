80c408b7b8408c1f98feba69fbaa35a7c96ee316
Bryan Zhang <bz5989@princeton.edu>
2024-06-27 17:48:23 -0400

    Bryan: Added routing system to allow for subpages
    Using children parameter of routers, now can create subpages. Currently
    still have main page as primary path (/bryan) Also have about me, fun,
    and project page built from small parts of main page
    
    L=ENG-100

---
+++ js/src/personal/bryan/AboutMe.page.tsx
@@ -0,0 +1,20 @@
+import { Text } from '@mantine/core'
+import { Socials } from './Socials.tsx'
+
+export function AboutMePage() {
+  return (
+    <>
+      <Text
+        c="blue"
+        ta="center"
+        size="md"
+        maw={580}
+        mx="auto"
+        mt="md"
+      >
+        {"Hey! I'm Bryan."}
+      </Text>
+      <Socials />
+    </>
+  )
+}
