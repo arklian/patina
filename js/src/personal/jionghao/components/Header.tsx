12be4d516e947df9152ec12bee4d34b7d130fe8f
Jionghao Wu <jw6643@nyu.edu>
2024-06-27 15:03:48 -0400

    Jionghao: Finished structure for Hero section
    added spacing and font sizes for the hero section,
    also updated the navbar font sizes to match the hero section
    
    L=ENG-93,ENG-34

--- js/src/personal/jionghao/components/Header.tsx
+++ js/src/personal/jionghao/components/Header.tsx
@@ -1,58 +1,61 @@
-import { Container, Group, Button, Flex } from '@mantine/core'
+import { Group, Button, Flex } from '@mantine/core'
 import styles from './Header.module.css'
 import { DarkModeToggle } from './DarkModeToggle.tsx'
 
 export function Header() {
   return (
-    <div className={styles.header}>
-      <Container className={styles.mainContainer}>
-        <Flex
-          justify="right"
-          align="center"
-        >
-          <div className={styles.logo}>{'PlaceHolderLogo'}</div>
-        </Flex>
-        <Flex
-          justify="right"
-          align="center"
-        >
-          <Group>
-            <Button
-              variant="subtle"
-              component="a"
-              href="#about"
-              color={'cyan'}
-            >
-              {'About'}
-            </Button>
-            <Button
-              variant="subtle"
-              component="a"
-              href="#experience"
-              color={'cyan'}
-            >
-              {'Experience'}
-            </Button>
-            <Button
-              variant="subtle"
-              component="a"
-              href="#work"
-              color={'cyan'}
-            >
-              {'Work'}
-            </Button>
-            <Button
-              variant="subtle"
-              component="a"
-              href="#contact"
-              color={'cyan'}
-            >
-              {'Contact'}
-            </Button>
-            <DarkModeToggle />
-          </Group>
-        </Flex>
-      </Container>
+    <div className={styles.mainContainer}>
+      <Flex
+        justify="right"
+        align="center"
+      >
+        <div className={styles.logo}>{'PlaceHolderLogo'}</div>
+      </Flex>
+      <Flex justify="space-between">
+        <Group>
+          <Button
+            className={styles.button}
+            variant="subtle"
+            component="a"
+            href="#about"
+            color={'cyan'}
+            size={'md'}
+          >
+            {'About'}
+          </Button>
+          <Button
+            className={styles.button}
+            variant="subtle"
+            component="a"
+            href="#experience"
+            color={'cyan'}
+            size={'md'}
+          >
+            {'Experience'}
+          </Button>
+          <Button
+            className={styles.button}
+            variant="subtle"
+            component="a"
+            href="#work"
+            color={'cyan'}
+            size={'md'}
+          >
+            {'Work'}
+          </Button>
+          <Button
+            className={styles.button}
+            variant="subtle"
+            component="a"
+            href="#contact"
+            color={'cyan'}
+            size={'md'}
+          >
+            {'Contact'}
+          </Button>
+          <DarkModeToggle />
+        </Group>
+      </Flex>
     </div>
   )
 }
