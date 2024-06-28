c90b944d0258feb511bd8821a596199f15a227d9
Maliha Tasnim <ayesiddika@gmail.com>
2024-06-28 02:41:14 -0400

    Maliha: Added social media icons, changed fonts
    Icons change color when hovered over
    ENG-104, ENG-105

--- js/src/personal/maliha/Maliha.page.tsx
+++ js/src/personal/maliha/Maliha.page.tsx
@@ -1,12 +1,52 @@
 import { Text, MantineProvider } from '@mantine/core'
-import { Menu } from './ModalMenu.tsx'
 import { theme } from './theme.ts'
+import { Menu } from './ModalMenu.tsx'
+import { Social } from './Social.tsx'
 import styles from './Maliha.module.css'
 
 export function MalihaPage() {
   return (
     <MantineProvider theme={theme}>
       <div className={styles.body}>
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caudex:ital,wght@0,400;0,700;1,400;1,700&family=Handlee&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap"
+          rel="stylesheet"
+        />
+        <Social />
         <Menu />
         <div className={styles.border} />
         <Text
