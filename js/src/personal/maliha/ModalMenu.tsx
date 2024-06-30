30432b0f98dbe8191a57e8727dfb9d165dac2dcd
Maliha Tasnim <ayesiddika@gmail.com>
2024-06-30 18:40:16 -0400

    Maliha: Created about me page
    Added images, changed fonts/aesthetics around, and home button leads you to beginning of site. In addition, made id names more specific and increased visibility of social media icons
    
    [ENG]-69
    [ENG]-20
    [ENG]-121

--- js/src/personal/maliha/ModalMenu.tsx
+++ js/src/personal/maliha/ModalMenu.tsx
@@ -1,26 +1,30 @@
-import { useState } from 'react'
 import { useDisclosure } from '@mantine/hooks'
 import { Modal, UnstyledButton, Stack } from '@mantine/core'
 import { Sandwich } from './Sandwich.tsx'
 import styles from './Maliha.module.css'
 
-const labels = [{ label: 'about' }, { label: 'projects' }, { label: 'gallery' }]
+const labels = [
+  { label: 'About' },
+  { label: 'Projects' },
+  { label: 'Gallery' },
+  { label: 'Notes' },
+  { label: 'Resume' },
+]
 
 export function Menu() {
   const [opened, { open, close }] = useDisclosure(false)
-  const [burgerOpened, setBurgerOpened] = useState(false)
 
   const items = labels.map((label) => (
-    <UnstyledButton className={styles.menuItems}>{label.label}</UnstyledButton>
+    <UnstyledButton
+      key={label.label}
+      className={styles.menuItems}
+    >
+      {label.label}
+    </UnstyledButton>
   ))
 
   const handleToggleMenu = () => {
-    if (opened) {
-      close()
-    } else {
-      open()
-    }
-    setBurgerOpened(!burgerOpened)
+    opened ? close() : open()
   }
 
   return (
@@ -33,24 +37,20 @@ export function Menu() {
         opened={opened}
         onClose={handleToggleMenu}
         title="Menu"
-        size={400}
+        size={450}
         zIndex={2}
+        pl={20}
         overlayProps={{
           backgroundOpacity: 0.1,
           blur: 0,
         }}
       >
-        <Stack
-          w={100}
-          miw={100}
-        >
-          {items}
-        </Stack>
+        <Stack>{items}</Stack>
       </Modal>
 
       <Sandwich
         onToggleMenu={handleToggleMenu}
-        opened={burgerOpened}
+        opened={opened}
       />
     </>
   )
