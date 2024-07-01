7898255222b50ed088bab7604b64f8d208741aab
Tony Wang <wangtony4005@gmail.com>
2024-07-01 17:24:27 -0400

    Tony: Added project section with links
    Created project section and added link to project.
    Added links for experience section as well.
    Changed some spacing and css.
    Fixed flashlight not going down when scrolling.
    Changed nav section location.
    Fixed inconsistent naming scheme.
    Removed safari anti-aliasing css style.
    
    L=ENG-111
    
    tmp
    
    Tony: Removed negative margin
    
    switched to using padding:0 instead of margin-left: -14
    
    temp

--- js/src/personal/tony/Tony.page.tsx
+++ js/src/personal/tony/Tony.page.tsx
@@ -19,8 +19,12 @@ export function TonyPage() {
   })
   const [showFlashlight, setShowFlashlight] = useState(false)
   const handleMouseMove = (e: React.MouseEvent) => {
-    setFlashlightStyle({ left: `${e.clientX}px`, top: `${e.clientY}px` })
+    setFlashlightStyle({
+      left: `${e.clientX + window.scrollX}px`,
+      top: `${e.clientY + window.scrollY}px`,
+    })
   }
+
   const handleMouseEnter = () => setShowFlashlight(true)
   const handleMouseLeave = () => setShowFlashlight(false)
 
@@ -42,7 +46,7 @@ export function TonyPage() {
         <Grid gutter="md">
           <Grid.Col span={6}>
             <Title
-              className={styles.title}
+              className={styles.name}
               ta="left"
             >
               <Text
@@ -52,9 +56,9 @@ export function TonyPage() {
                 {'Tony Wang'}
               </Text>
             </Title>
-            <Container className={styles.flex}>
-              <Text className={styles.text}>{'Software Engineer'}</Text>
-              <Container className={styles.socialIcons}>
+            <div className={styles.flex}>
+              <Text className={styles.textSWE}>{'Software Engineer'}</Text>
+              <div className={styles.socialIcons}>
                 <ActionIcon
                   color={'#DADDD8'}
                   component="a"
@@ -81,27 +85,27 @@ export function TonyPage() {
                 >
                   <FaLinkedin size={24} />
                 </ActionIcon>
-              </Container>
+              </div>
+            </div>
+            <Container className={styles.menu}>
+              <div className={styles.flexMenu}>
+                <div className={styles.navLine}></div>
+                <Text className={styles.menu1}>{' About'}</Text>
+              </div>
+              <div className={styles.flexMenu}>
+                <div className={styles.navLine}></div>
+                <Text className={styles.menu1}>{' Experiences'}</Text>
+              </div>
+              <div className={styles.flexMenu}>
+                <div className={styles.navLine}></div>
+                <Text className={styles.menu1}>{' Projects'}</Text>
+              </div>
             </Container>
             <Image
               className={styles.pfp}
               src="https://placehold.co/500x500?text=Placeholder"
               alt="Profile"
             />
-            <Container className={styles.menu}>
-              <div className={styles.flex1}>
-                <div className={styles.titleLine1}></div>
-                <Text className={styles.menu1}>{' About'}</Text>
-              </div>
-              <div className={styles.flex1}>
-                <div className={styles.titleLine1}></div>
-                <Text className={styles.menu1}>{' Experiences'}</Text>
-              </div>
-              <div className={styles.flex1}>
-                <div className={styles.titleLine1}></div>
-                <Text className={styles.menu1}>{' Projects'}</Text>
-              </div>
-            </Container>
           </Grid.Col>
           <Grid.Col span={6}>
             <Text className={styles.about}>
@@ -110,48 +114,78 @@ export function TonyPage() {
               }
             </Text>
             <Container>
-              <Text className={styles.subTitle}>
-                {
-                  'Undergraduate Teaching Assistant * Hunter College | Jan 2023 - Present'
-                }
-              </Text>
-              <Text className={styles.text1}>
-                {
-                  'Provided tutoring for 400+ students in topics related to OOP in C++ and Python. Created and graded projects/assignments in C++. Conducted weekly code reviews.'
-                }
-              </Text>
+              <a
+                href="https://maryash.github.io/135/index.html"
+                rel="noopener noreferrer"
+                target="_blank"
+                className={styles.link}
+              >
+                <Text className={styles.experienceTitle}>
+                  {
+                    'CSCI-135 Teaching Assistant @ Hunter College | Jan 2023 - Present ↗'
+                  }
+                </Text>
+                <Text className={styles.experienceText}>
+                  {
+                    'Provided tutoring for 400+ students in topics related to OOP in C++ and Python. Created and graded projects/assignments in C++. Conducted weekly code reviews.'
+                  }
+                </Text>
+              </a>
             </Container>
             <Container>
-              <Text className={styles.subTitle}>
-                {
-                  'Software Developer Intern * The Difference App | Jul 2023 - Aug 2023'
-                }
-              </Text>
-              <Text className={styles.text1}>
-                {
-                  'Implemented push notifications options using Flutter for a weight management calorie tracking app to allow both Android and iOS users to receive periodic reminder notifications to log their caloric intake and exercise'
-                }
-              </Text>
+              <a
+                href="https://thedifferenceapp.com/"
+                rel="noopener noreferrer"
+                target="_blank"
+                className={styles.link}
+              >
+                <Text className={styles.experienceTitle}>
+                  {
+                    'Software Developer Intern @ The Difference App | Jul 2023 - Aug 2023 ↗'
+                  }
+                </Text>
+                <Text className={styles.experienceText}>
+                  {
+                    'Implemented push notifications options using Flutter for a weight management calorie tracking app to allow both Android and iOS users to receive periodic reminder notifications to log their caloric intake and exercise'
+                  }
+                </Text>
+              </a>
             </Container>
             <Container>
-              <Text className={styles.subTitle}>
-                {'Software Developer Intern * Placeholder | Placeholder '}
+              <Text className={styles.experienceTitle}>
+                {'Software Developer Intern @ Patina Network | Placeholder ↗'}
               </Text>
-              <Text className={styles.text1}>
+              <Text className={styles.experienceText}>
                 {
                   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum nunc aliquet bibendum enim facilisis.'
                 }
               </Text>
             </Container>
-            <Container className={styles.flex}>
-              <Image
-                className={styles.projectPic}
-                src="https://placehold.co/500x500?text=Placeholder"
-                alt="A-List"
-              />
-              <Text className={styles.subTitle}>
-                {'A-List Manga Updates App'}
-              </Text>
+            <Container className={styles.flexProject}>
+              <a
+                href="https://devpost.com/software/a-ping"
+                rel="noopener noreferrer"
+                target="_blank"
+                className={styles.link}
+              >
+                <div className={styles.projectItem}>
+                  <Image
+                    className={styles.projectPic}
+                    src="https://placehold.co/500x500?text=Placeholder"
+                    alt="A-List"
+                  />
+                  <div className={styles.projectText}>
+                    <Text className={styles.experienceTitle}>
+                      {'A-List Manga Updates App'}
+                    </Text>
+                    <Text className={styles.experienceText}>
+                      {
+                        'Flutter app that allows you to create a list of your favorite webtoon/manga series, and track most recently updated chapters. Save 5+ minutes a day from manually checking and refreshing.'
+                      }
+                    </Text>
+                  </div>
+                </div>
+              </a>
             </Container>
           </Grid.Col>
         </Grid>
