69f508f72ea9ad1362168431aab01a6aa298c6f5
Bryan Zhang <bz5989@princeton.edu>
2024-07-01 17:41:45 -0400

    Bryan: Started work on Projects page
    Created new component for a general project, which allows for adding
    in a photo, an extra link, and text
    
    L=ENG-17
    L=ENG-130

--- js/src/personal/bryan/Projects/Projects.page.tsx
+++ js/src/personal/bryan/Projects/Projects.page.tsx
@@ -1,7 +1,72 @@
-import { Flex, Text, Title, Grid, Anchor } from '@mantine/core'
+import { Flex, Text, Title, Grid, Anchor, Center, Image } from '@mantine/core'
 import classes from './Projects.module.css'
+import image from '@/personal/bryan/images/Personal_Site.png'
+import document from '@/personal/bryan/Files/COS_429_Report.pdf'
 
 export function ProjectsPage() {
+  const Project = (props: {
+    title: string
+    link: string
+    text: string
+    read?: boolean
+    file?: string
+  }) => {
+    let Starter = (
+      <Anchor
+        href={props.link}
+        target="_blank"
+      >
+        {'Github'}
+      </Anchor>
+    )
+    if (props.read) {
+      Starter = (
+        <>
+          <Anchor
+            href={props.link}
+            target="_blank"
+          >
+            {'Github'}
+          </Anchor>
+          <Anchor
+            href={props.file}
+            target="_blank"
+          >
+            {'Report'}
+          </Anchor>
+        </>
+      )
+    }
+    return (
+      <Grid.Col
+        span={{ base: 12, md: 12, lg: 6 }}
+        bd="1px solid gray"
+      >
+        <Grid component="span">
+          <Grid.Col span={{ base: 12, md: 5, lg: 5 }}>
+            <Center>
+              <Title
+                mt="1rem"
+                component="span"
+                className={classes.title2}
+              >
+                {props.title}
+              </Title>
+            </Center>
+            <Image
+              w={250}
+              h={150}
+              src={image}
+            />
+          </Grid.Col>
+          <Grid.Col span={{ base: 12, md: 7, lg: 7 }}>
+            {Starter}
+            <Text>{props.text}</Text>
+          </Grid.Col>
+        </Grid>
+      </Grid.Col>
+    )
+  }
   return (
     <Flex
       mr="2rem"
@@ -22,72 +87,31 @@ export function ProjectsPage() {
         <Grid.Col span={12}>
           <Text>{'Currently done projects'}</Text>
         </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 4, lg: 2 }}>
-          <Title
-            component="span"
-            className={classes.title2}
-          >
-            {'This site'}
-          </Title>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 8, lg: 4 }}>
-          <Anchor href="https://github.com/arklian/patina/tree/main/js/src/personal/bryan">
-            {'Github'}
-          </Anchor>
-          <Text>
-            {
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure doloremque quas dolorum. Quo amet earum alias consequuntur quam accusamus a quae beatae, odio, quod provident consectetur non repudiandae enim adipisci?'
-            }
-          </Text>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 4, lg: 2 }}>
-          <Title
-            component="span"
-            className={classes.title2}
-          >
-            {'COS 429'}
-          </Title>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 8, lg: 4 }}>
-          <Text>
-            {
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure doloremque quas dolorum. Quo amet earum alias consequuntur quam accusamus a quae beatae, odio, quod provident consectetur non repudiandae enim adipisci?'
-            }
-          </Text>
-        </Grid.Col>
+        <Project
+          title="This site"
+          link="https://github.com/arklian/patina/tree/main/js/src/personal/bryan"
+          text="Hello World. This is placeholder text that I'm writing here for the sake of having a long text thing. This is so I have a good sense of how much I would normally write for a description of one of my pieces. That way, I know how large of an image I can put on the side, as well as how much space it'll take up on my page."
+        />
+        <Project
+          title="COS 429"
+          link="https://github.com/bz5989/429_Final"
+          read
+          file={document}
+          text="Hello World. This is placeholder text that I'm writing here for the sake of having a long text thing. This is so I have a good sense of how much I would normally write for a description of one of my pieces. That way, I know how large of an image I can put on the side, as well as how much space it'll take up on my page."
+        />
         <Grid.Col span={12}>
           <Text>{'Future projects'}</Text>
         </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 4, lg: 2 }}>
-          <Title
-            component="span"
-            className={classes.title2}
-          >
-            {'COS 333'}
-          </Title>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 8, lg: 4 }}>
-          <Text>
-            {
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure doloremque quas dolorum. Quo amet earum alias consequuntur quam accusamus a quae beatae, odio, quod provident consectetur non repudiandae enim adipisci?'
-            }
-          </Text>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 4, lg: 2 }}>
-          <Title
-            component="span"
-            className={classes.title2}
-          >
-            {'COS 397'}
-          </Title>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 8, lg: 4 }}>
-          <Text>
-            {
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure doloremque quas dolorum. Quo amet earum alias consequuntur quam accusamus a quae beatae, odio, quod provident consectetur non repudiandae enim adipisci?'
-            }
-          </Text>
-        </Grid.Col>
+        <Project
+          title="COS 333"
+          link="https://github.com/arklian/patina/tree/main/js/src/personal/bryan"
+          text="Hello World. This is placeholder text that I'm writing here for the sake of having a long text thing. This is so I have a good sense of how much I would normally write for a description of one of my pieces. That way, I know how large of an image I can put on the side, as well as how much space it'll take up on my page."
+        />
+        <Project
+          title="COS 397"
+          link="https://github.com/arklian/patina/tree/main/js/src/personal/bryan"
+          text="Hello World. This is placeholder text that I'm writing here for the sake of having a long text thing. This is so I have a good sense of how much I would normally write for a description of one of my pieces. That way, I know how large of an image I can put on the side, as well as how much space it'll take up on my page."
+        />
       </Grid>
     </Flex>
   )
