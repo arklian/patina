70cfd2c3656ab5127a27524c6c82d3ed21cb02af
Riyuan Liu <riyusnliu@gmail.com>
2024-07-02 20:22:29 -0400

    Riyuan: Fixed style on Container
    Add a Container Item on each section of the
    website to ensure everything is center.
    Remove Timeline Component from experience section

--- js/src/personal/riyuan/components/home/Home.tsx
+++ js/src/personal/riyuan/components/home/Home.tsx
@@ -1,5 +1,6 @@
-import { Text, Title } from '@mantine/core'
+import { Text, Title, Container } from '@mantine/core'
 import styles from '@/personal/riyuan/Riyuan.module.css'
+import hstyles from '@/personal/riyuan/components/home/Home.module.css'
 
 export function Home() {
   return (
@@ -7,25 +8,27 @@ export function Home() {
       id={'Home'}
       className={styles.container}
     >
-      <Title
-        className={styles.title}
-        ta="left"
-        mt={100}
-      >
-        {'Home Page'}
-      </Title>
-      <Text
-        c="dimmed"
-        ta="center"
-        size="lg"
-        maw={580}
-        mx="auto"
-        mt="xl"
-      >
-        {
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        }
-      </Text>
+      <Container className={hstyles.inner}>
+        <Title
+          className={styles.title}
+          ta="left"
+          mt={100}
+        >
+          {'Home Page'}
+        </Title>
+        <Text
+          c="dimmed"
+          ta="center"
+          size="lg"
+          maw={580}
+          mx="auto"
+          mt="xl"
+        >
+          {
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          }
+        </Text>
+      </Container>
     </div>
   )
 }
