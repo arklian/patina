d015daa626f487013ecb671d3a83b991aca04677
Kelly Lin <linkelly150@gmail.com>
2024-07-08 20:55:09 -0400

    Kelly: Temp update
    Change-Id: I1a16a0553574e40407befcc5beee657c56961ade

--- js/src/personal/kelly/Kelly.page.tsx
+++ js/src/personal/kelly/Kelly.page.tsx
@@ -62,7 +62,11 @@ export function KellyPage() {
         ></div>
         <p className={styles.about_header}>{'✦ ABOUT ME '}</p>
         <div className={styles.about}>
-          <div className={styles.about_item}></div>
+          <p className={styles.about_item}>
+            {
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+          </p>
           <div>
             <img
               className={styles.pfp_img}
