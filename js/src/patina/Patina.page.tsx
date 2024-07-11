ea3416b1216beedd4e77d100248877bb90a24dcb
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-11 15:27:20 -0400

    Maliha: Added home to patina page
    Change-Id: I9908174a6752fe0423d328dc51adc2bafeb11ae8
    
    Maliha: Setting up css file imports and some flex boxes
    
    Change-Id: Id299db3c8398363858bdafde6d53d609ed6d456b

--- js/src/patina/Patina.page.tsx
+++ js/src/patina/Patina.page.tsx
@@ -1,11 +1,12 @@
 import { MantineProvider } from '@mantine/core'
-import { PatinaWelcome } from '@/patina/PatinaWelcome.tsx'
+// import { PatinaWelcome } from '@/patina/PatinaWelcome.tsx'
+import { Home } from '@/patina/home/Home.tsx'
 import { theme } from './theme.ts'
 
 export function PatinaPage() {
   return (
     <MantineProvider theme={theme}>
-      <PatinaWelcome />
+      <Home />
     </MantineProvider>
   )
 }
