7a56c8d7b8485413b7bb0d766d47f64bdde73882
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-11 15:32:54 -0400

    Revert "Maliha: Added home to patina page"
    This reverts commit 353b56be7abef6ccb6a42321064b90d2c71e09c0.
    
    Change-Id: I5d1174d13100845305a015f668f8ae53f06ed0df

--- js/src/patina/Patina.page.tsx
+++ js/src/patina/Patina.page.tsx
@@ -1,12 +1,11 @@
 import { MantineProvider } from '@mantine/core'
-// import { PatinaWelcome } from '@/patina/PatinaWelcome.tsx'
-import { Home } from '@/patina/home/Home.tsx'
+import { PatinaWelcome } from '@/patina/PatinaWelcome.tsx'
 import { theme } from './theme.ts'
 
 export function PatinaPage() {
   return (
     <MantineProvider theme={theme}>
-      <Home />
+      <PatinaWelcome />
     </MantineProvider>
   )
 }
