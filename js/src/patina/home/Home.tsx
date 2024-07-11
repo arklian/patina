e30cb62614ac6e05eda762aa009470a19134cbc0
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-11 17:13:47 -0400

    Manny: Adds a skeleton to the Home page of patina
    Change-Id: I1bbec04be101c025a645416fc3738961a0c78d27

---
+++ js/src/patina/home/Home.tsx
@@ -0,0 +1,44 @@
+import { Skeleton } from '@mantine/core'
+import styles from './Home.module.css'
+
+export function Home() {
+  return (
+    <>
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={70} width="100%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={600} width="100%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={50} width="40%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={900} width="80%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={50} width="40%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={500} width="80%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={50} width="40%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={400} width="60%" mb="xl" />
+      </div>
+
+      <div className={styles.skeletonStyle}>
+        <Skeleton animate={false} height={70} width="100%" mt="xl" />
+      </div>
+    </>
+  )
+}
