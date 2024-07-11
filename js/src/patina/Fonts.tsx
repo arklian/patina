5fdc2d9119aee9e1b14fc0537172071fe2fbefe4
Jionghao Wu <jhwu03@gmail.com>
2024-07-11 17:29:05 -0400

    Jionghao: Create about me text and button
    L=ENG-190
    
    Change-Id: I727698ab5cf90b793f3173179d8b6bde45107c05

---
+++ js/src/patina/Fonts.tsx
@@ -0,0 +1,16 @@
+export function Fonts() {
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin={''}
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  )
+}
