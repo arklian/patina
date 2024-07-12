78ff34446d33474ea3375b93236950e88cb57e40
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-12 17:11:23 -0400

    Manny: fixes the theme, to reflect the desired colors for the patina scheme
    Change-Id: I91ac8982cbb8e68e16ccf80e51d5e11adef45318

--- js/src/patina/Patina.page.tsx
+++ js/src/patina/Patina.page.tsx
@@ -1,10 +1,10 @@
 import { MantineProvider } from '@mantine/core'
 import { Home } from '@/patina/home/Home.tsx'
-import { theme } from './theme.ts'
+import { theme, resolver } from './theme.ts'
 
 export function PatinaPage() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider theme={theme} cssVariablesResolver={resolver}>
       <Home />
     </MantineProvider>
   )
