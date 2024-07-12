25eb16d20b8d2f9446281b29cf947bc0fba3f195
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-12 15:25:55 -0400

    Maliha: Make image object to hold src for all images
    Delete big image files and replaced with urls
    
    ENG-210
    
    Change-Id: I77acfd2fc5ab0b0bdaeeeb4661a4336fec5032c1

---
+++ js/src/patina/assets/images.ts
@@ -0,0 +1,18 @@
+/**
+ * Images for patina website, hosted using google photos
+ * https://webapps.stackexchange.com/a/98573
+ */
+export const imageUrls = {
+  mentorship: {
+    drive: 'https://drive.google.com/drive/u/0/home',
+    src: 'https://lh3.googleusercontent.com/pw/AP1GczNC28oh6XrEApaU9KvFx3LcHtqZI3h_SYsE-upbv-xfCuviZOWn1Xk-zHqP2WEM5NkVx-CY_OHZcRfjJdisQQuAkC9IWf6xNOKXe_67atsXUcc0gEOlxFNxTouvac0p0xsAC3PbHkhSWv86bZjFuMlt=w2078-h1558-s-no-gm',
+  },
+  scholarship: {
+    drive: 'https://drive.google.com/drive/u/0/home',
+    src: 'https://lh3.googleusercontent.com/pw/AP1GczPwoTf53J69OBpDfRL4ADnwLagApAbL58iIAauisxPCURVInkFulkIHDGj5832Ow_s6-b-hKqTScuNjQ3fAo8iaa708gnV09J52YI5jCgaFMw2As5EjXnuaLkC12mcWQQ4flmmL9q2QXVqLL-AAN1ZG=w800-h1200-s-no-gm',
+  },
+  community: {
+    drive: 'https://drive.google.com/drive/u/0/home',
+    src: 'https://lh3.googleusercontent.com/pw/AP1GczNrORpkdmN6ylW3u1eAHZH6Voqp4sLdZ81byejoOTS1h9M9GgfLXQ78_R5sIkOBSPTed2KgaJRnKCMDCuE_a4fnuvG5MNlDHhvakg7DlT7JUlknqHIpg0Dmpx99wrCgeYcjnydyPXhDt12WTHFsNTOh=w800-h533-s-no-gm',
+  },
+}
