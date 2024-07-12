8778d8873bf960b53fddc4713cddf93211df83be
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-12 14:21:34 -0400

    Maliha: Make image object to hold src for all images
    Delete big image files and replaced with urls
    
    Change-Id: I77acfd2fc5ab0b0bdaeeeb4661a4336fec5032c1

--- js/src/patina/home/Initiatives.tsx
+++ js/src/patina/home/Initiatives.tsx
@@ -1,9 +1,7 @@
 import { Text } from '@mantine/core'
 import styles from './Initiatives.module.css'
-import community from './images/community.jpeg'
-import scholarship from './images/scholarship.jpeg'
-import mentorship from './images/mentorship.jpeg'
 import { Cards } from './Cards.tsx'
+import { imageUrls } from '@/patina/assets/images'
 
 /** Initiatives Section Component */
 const detailsMap = [
@@ -11,20 +9,20 @@ const detailsMap = [
     title: 'Mentorship',
     details:
       'Mentor matching and internships for students and young professionals.',
-    img: mentorship,
+    img: imageUrls.mentorship.src,
     link: 'https://github.com/arklian/patina/blob/main/tools/git/gerrit/setup.md',
   },
   {
     title: 'Scholarship',
     details: 'Annual scholarship fund for under resourced students.',
-    img: scholarship,
+    img: imageUrls.scholarship.src,
     link: 'https://github.com/arklian/patina/blob/main/tools/git/gerrit/setup.md',
   },
   {
     title: 'Community',
     details:
       'Spaces to connect with community and learn about diverse cultures.',
-    img: community,
+    img: imageUrls.community.src,
     link: 'https://github.com/arklian/patina/blob/main/tools/git/gerrit/setup.md',
   },
 ]
