0d83a1f0d150ee67364d874044180b22388f4e19
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-12 15:18:00 -0400

    Maliha: Create the initiatives section
    Change-Id: I3eca06abc98b1373da14b1bc2573edefce6177b9

--- js/src/patina/home/Initiatives.tsx
+++ js/src/patina/home/Initiatives.tsx
@@ -11,12 +11,14 @@ const detailsMap = [
       'Mentor matching and internships for students and young professionals.',
     img: imageUrls.mentorship.src,
     link: 'placeholder link',
+    alt: 'Group picture of all interns.',
   },
   {
     title: 'Scholarship',
     details: 'Annual scholarship fund for under resourced students.',
     img: imageUrls.scholarship.src,
     link: 'placeholder link',
+    alt: 'Woman posing in graduation gown and holding a degree.',
   },
   {
     title: 'Community',
@@ -24,6 +26,7 @@ const detailsMap = [
       'Spaces to connect with community and learn about diverse cultures.',
     img: imageUrls.community.src,
     link: 'placeholder link',
+    alt: 'Group of women sitting on chairs. They are talking to each other.',
   },
 ]
 
@@ -32,13 +35,14 @@ export function Initiatives() {
     <>
       <div className={styles.container}>
         <Text>{'Our initiatives'}</Text>
-        {detailsMap.map(({ title, details, img, link }, index) => (
+        {detailsMap.map(({ title, details, img, link, alt }, index) => (
           <Cards
             key={index}
             title={title}
             details={details}
             img={img}
             link={link}
+            alt={alt}
           />
         ))}
       </div>
