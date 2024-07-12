db213d61add52af5852c1e1884d83fd278ea617a
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-12 17:11:23 -0400

    Patina: Add Patina Colors to Mantine Theme
    To keep consistency of colors through the website and allow for
    easier refactors, defining our colors in the Mantine theme.
    
    L=ENG-9999
    TEST=manual
    Tested the page still loads and changed a style to use the new css
    variable.
    
    Change-Id: I91ac8982cbb8e68e16ccf80e51d5e11adef45318

--- js/src/patina/theme.ts
+++ js/src/patina/theme.ts
@@ -1,4 +1,9 @@
-import { createTheme, DEFAULT_THEME, mergeMantineTheme } from '@mantine/core'
+import {
+  createTheme,
+  CSSVariablesResolver,
+  DEFAULT_THEME,
+  mergeMantineTheme,
+} from '@mantine/core'
 
 /**
  * Custom mantine theme override
@@ -8,22 +13,32 @@ import { createTheme, DEFAULT_THEME, mergeMantineTheme } from '@mantine/core'
  * {@link https://github.com/mantinedev/mantine/blob/master/packages/%40mantine/core/src/core/MantineProvider/default-colors.ts Default Colors}
  */
 export const themeOverride = createTheme({
-  colors: {
-    patina: [
-      '#effbf4',
-      '#def5e6',
-      '#b7eaca',
-      '#8cdfad',
-      '#6ad693',
-      '#55d184',
-      '#4acf7b',
-      '#3cb668',
-      '#31a25c',
-      '#218c4c',
-    ],
+  other: {
+    patinaGreenDark: '#03664D',
+    patinaGreenLight: '#4DFFB0',
+    patinaBlueDark: '#1550C4',
+    patinaBlueLight: '#47FFFF',
+    patinaRedDark: '#BB352C',
+    patinaRedLight: '#FF3D3D',
   },
-
-  primaryColor: 'patina',
 })
 
 export const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride)
+
+/**
+ * Add custom CSS variables to keep consistency across the codebase.
+ * Generally these should be used instead of raw values.
+ * Usage: color: var(--mantine-color-patina-green-light)
+ */
+export const resolver: CSSVariablesResolver = (computedTheme) => ({
+  variables: {
+    '--mantine-color-patina-green-light': computedTheme.other.patinaGreenLight,
+    '--mantine-color-patina-blue-light': computedTheme.other.patinaBlueLight,
+    '--mantine-color-patina-red-light': computedTheme.other.patinaRedLight,
+    '--mantine-color-patina-green-dark': computedTheme.other.patinaGreenDark,
+    '--mantine-color-patina-blue-dark': computedTheme.other.patinaBlueDark,
+    '--mantine-color-patina-red-dark': computedTheme.other.patinaRedDark,
+  },
+  light: {},
+  dark: {},
+})
