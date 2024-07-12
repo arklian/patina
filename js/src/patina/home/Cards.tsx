a112b5e40b46bf799b40a13226011bde1a2ea1c8
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-12 14:40:42 -0400

    Maliha: Create card component
    Change-Id: If6f4f3189561a8c1b0e88dba033f47cc2e80e863

---
+++ js/src/patina/home/Cards.tsx
@@ -0,0 +1,29 @@
+import { Text, Title, Card, Image, Anchor, AspectRatio } from '@mantine/core'
+import styles from './Cards.module.css'
+
+/** Cards For Initiatives Component */
+type CardsProps = {
+  title: string
+  details: string
+  img: string
+  link: string
+}
+
+export function Cards({ title, details, img, link }: CardsProps) {
+  return (
+    <>
+      <Card className={styles.card}>
+        <AspectRatio>
+          <Image className={styles.image} src={img}></Image>
+        </AspectRatio>
+        <div className={styles.text_container}>
+          <Title className={styles.title}>{title}</Title>
+          <Text className={styles.text}>{details}</Text>
+          <Anchor className={styles.anchor} href={link}>
+            <Text>{'Learn more\u2192'}</Text>
+          </Anchor>
+        </div>
+      </Card>
+    </>
+  )
+}
