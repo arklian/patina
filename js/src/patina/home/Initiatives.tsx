95b9efea4bc96753a98610ad66f49ce744db80df
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-12 15:29:44 -0400

    Maliha: Create the initiatives section
    ENG-188
    
    Change-Id: I3eca06abc98b1373da14b1bc2573edefce6177b9

--- js/src/patina/home/Initiatives.tsx
+++ js/src/patina/home/Initiatives.tsx
@@ -3,7 +3,6 @@ import styles from './Initiatives.module.css'
 import { Cards } from './Cards.tsx'
 import { imageUrls } from '@/patina/assets/images'
 
-/** Initiatives Section Component */
 const detailsMap = [
   {
     title: 'Mentorship',
@@ -11,12 +10,14 @@ const detailsMap = [
       'Mentor matching and internships for students and young professionals.',
     img: imageUrls.mentorship.src,
     link: 'placeholder link',
+    alt: 'Group picture of all interns.',
   },
   {
     title: 'Scholarship',
     details: 'Annual scholarship fund for under resourced students.',
     img: imageUrls.scholarship.src,
     link: 'placeholder link',
+    alt: 'Woman posing in graduation gown and holding a degree.',
   },
   {
     title: 'Community',
@@ -24,21 +25,24 @@ const detailsMap = [
       'Spaces to connect with community and learn about diverse cultures.',
     img: imageUrls.community.src,
     link: 'placeholder link',
+    alt: 'Group of women sitting on chairs. They are talking to each other.',
   },
 ]
 
+/** Displays custom cards and images of the various initiatives of Patina */
 export function Initiatives() {
   return (
     <>
       <div className={styles.container}>
         <Text>{'Our initiatives'}</Text>
-        {detailsMap.map(({ title, details, img, link }, index) => (
+        {detailsMap.map(({ title, details, img, link, alt }, index) => (
           <Cards
             key={index}
             title={title}
             details={details}
             img={img}
             link={link}
+            alt={alt}
           />
         ))}
       </div>
