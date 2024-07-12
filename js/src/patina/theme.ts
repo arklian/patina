acac15ff640b00880c2d9db9f625482bc8ae6677
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-12 17:11:23 -0400

    Manny: fixes the theme, to reflect the desired colors for the patina scheme
    Change-Id: I91ac8982cbb8e68e16ccf80e51d5e11adef45318

--- js/src/patina/theme.ts
+++ js/src/patina/theme.ts
@@ -1,29 +1,32 @@
-import { createTheme, DEFAULT_THEME, mergeMantineTheme } from '@mantine/core'
+import {
+  createTheme,
+  CSSVariablesResolver,
+  DEFAULT_THEME,
+  mergeMantineTheme,
+} from '@mantine/core'
 
-/**
- * Custom mantine theme override
- *
- * {@link https://mantine.dev/theming/theme-object/ Theme Object Docs}
- * {@link https://github.com/mantinedev/mantine/blob/master/packages/%40mantine/core/src/core/MantineProvider/default-theme.ts Default Theme}
- * {@link https://github.com/mantinedev/mantine/blob/master/packages/%40mantine/core/src/core/MantineProvider/default-colors.ts Default Colors}
- */
 export const themeOverride = createTheme({
-  colors: {
-    patina: [
-      '#effbf4',
-      '#def5e6',
-      '#b7eaca',
-      '#8cdfad',
-      '#6ad693',
-      '#55d184',
-      '#4acf7b',
-      '#3cb668',
-      '#31a25c',
-      '#218c4c',
-    ],
+  other: {
+    patinaGreenDark: '#03664D',
+    patinaGreenLight: '#4DFFB0',
+    patinaBlueDark: '#1550C4',
+    patinaBlueLight: '#47FFFF',
+    patinaRedDark: '#BB352C',
+    patinaRedLight: '#FF3D3D',
   },
-
-  primaryColor: 'patina',
 })
 
 export const theme = mergeMantineTheme(DEFAULT_THEME, themeOverride)
+
+export const resolver: CSSVariablesResolver = (computedTheme) => ({
+  variables: {
+    '--mantine-color-patina-green-light': computedTheme.other.patinaGreenLight,
+    '--mantine-color-patina-blue-light': computedTheme.other.patinaBlueLight,
+    '--mantine-color-patina-red-light': computedTheme.other.patinaRedLight,
+    '--mantine-color-patina-green-dark': computedTheme.other.patinaGreenDark,
+    '--mantine-color-patina-blue-dark': computedTheme.other.patinaBlueDark,
+    '--mantine-color-patina-red-dark': computedTheme.other.patinaRedDark,
+  },
+  light: {},
+  dark: {},
+})
