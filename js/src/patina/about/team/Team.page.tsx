e685b83959a01c2cb9728fd636ac5c18c500cf68
Henry Chen <hjc77@cornell.edu>
2024-07-14 19:23:47 -0400

    Patina: Create example page skeletons
    TEST=manual
    New pages can be navigated to.
    
    Change-Id: Icc908caca2405516c393f7180ec982b879228e36

---
+++ js/src/patina/about/team/Team.page.tsx
@@ -0,0 +1,19 @@
+import { Container, Skeleton, Stack } from '@mantine/core'
+
+/**
+ * Todo: Fill in
+ */
+export function TeamPage() {
+  return (
+    <Container>
+      <Stack>
+        {/* Section 1 */}
+        <Skeleton animate={false} height={70} width="100%" mb="xl" />
+        {/* Section 2 */}
+        <Skeleton animate={false} height={70} width="100%" mb="xl" />
+        {/* Section 3 */}
+        <Skeleton animate={false} height={70} width="100%" mb="xl" />
+      </Stack>
+    </Container>
+  )
+}
