68fcac6421f9fb9c8bae70a04c06f5aab63f79b7
Henry Chen <hjc77@cornell.edu>
2024-07-14 19:10:34 -0400

    JS-Testing: Move theme into test folder
    This theme is only used for the testing framework, so moving it in.
    As we clean up the test infrastructure, this will likely change.
    
    TEST=none
    
    Change-Id: Iafc58ff5055957da8939542b134f0d40574e8794

--- js/test-utils/render.tsx
+++ js/test-utils/render.tsx
@@ -1,6 +1,6 @@
 import { render as testingLibraryRender } from '@testing-library/react'
 import { MantineProvider } from '@mantine/core'
-import { theme } from '../src/theme'
+import { theme } from './theme.ts'
 
 export function render(ui: React.ReactNode) {
   return testingLibraryRender(<>{ui}</>, {
