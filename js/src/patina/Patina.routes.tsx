04f11990f45a996dfc0c6a38f7b2211bce4c9e32
Henry Chen <hjc77@cornell.edu>
2024-07-14 19:23:47 -0400

    Patina: Create example page skeletons
    TEST=manual
    New pages can be navigated to.
    
    Change-Id: Icc908caca2405516c393f7180ec982b879228e36

--- js/src/patina/Patina.routes.tsx
+++ js/src/patina/Patina.routes.tsx
@@ -1,4 +1,5 @@
 import { HomePage } from '@/patina/home/Home.page.tsx'
+import { MissionPage } from '@/patina/about/mission/Mission.page.tsx'
 
 /**
  * Routes for the Patina Website
@@ -8,5 +9,15 @@ export const PatinaRoutes = [
     index: true,
     description: 'Home',
     element: <HomePage />,
+  },
+  {
+    path: 'mission',
+    description: 'Mission',
+    element: <MissionPage />,
+  },
+  {
+    path: 'team',
+    description: 'Team',
+    element: <MissionPage />,
   },
 ]
