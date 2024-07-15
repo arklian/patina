f55c43de0bfdb062e31f63a4e50179d568fad22a
Kelly Lin <linkelly150@gmail.com>
2024-07-15 12:27:03 -0400

    Kelly: Finished home page hero section
    L=ENG-189
    
    Change-Id: I660c1cad8421289261f7a75e100165ec57d9e63e

--- js/src/patina/home/Home.page.tsx
+++ js/src/patina/home/Home.page.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from '@mantine/core'
+import { Skeleton, Title } from '@mantine/core'
 import styles from './Home.module.css'
 
 export function HomePage() {
@@ -8,8 +8,26 @@ export function HomePage() {
         <Skeleton animate={false} height={70} width="100%" mb="xl" />
       </div>
 
-      <div className={styles.skeletonStyle}>
-        <Skeleton animate={false} height={600} width="100%" mb="xl" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet"
+      />
+      <div className={styles.hero}>
+        <div>
+          <img
+            className={styles.hero_image}
+            alt={'hero'}
+            src={
+              'https://cdn.discordapp.com/attachments/1261068702171009085/1262442743255269487/hero.png?ex=66969ced&is=66954b6d&hm=ac8fedf35fd1f662e9051863056f2cc1879d4962453600d7455d874b5d23516e&'
+            }
+          />
+        </div>
+        <Title className={styles.hero_title}>
+          {'Empowering AANHPI leaders to '}
+          <span className={styles.title_teal}>{'make their mark.'}</span>
+        </Title>
       </div>
 
       <div className={styles.skeletonStyle}>
