1c86d1561e06926704c415c16825e19a8c26cc8a
Kelly Lin <linkelly150@gmail.com>
2024-07-15 15:28:55 -0400

    Kelly: Scholarship Hero Section w/o button
    L=ENG-225
    
    Change-Id: Ia2dae5f60e53ceac68a0071f3bae68f605f637da

---
+++ js/src/patina/scholarship/Scholarship.page.tsx
@@ -0,0 +1,30 @@
+import styles from './Scholarship.module.css'
+
+export function ScholarshipPage() {
+  return (
+    <>
+      <div className={styles.hero}>
+        <div className={styles.hero_container}>
+          <div className={styles.hero_title}>
+            {'Patina Network Scholarship Fund'}
+            <div className={styles.hero_description}>
+              {
+                'The Patina Network Scholarship fund is... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+              }
+            </div>
+          </div>
+          <div>
+            <img
+              className={styles.hero_image}
+              alt={'scholarship hero'}
+              src={
+                'https://cdn.discordapp.com/attachments/1261068702171009085/1262480237615124571/Group_297559.png?ex=6696bfd8&is=66956e58&hm=2506faf9caa9b3d56c06b7f228d89b3c1fe6137c605e1732b3f2bf80ae694531&'
+              }
+            />
+          </div>
+        </div>
+      </div>
+      <div className={styles.selection}></div>
+    </>
+  )
+}
