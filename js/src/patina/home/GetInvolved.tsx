33b222cd4d06de47b0272839b2a2c1f1a26a7395
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-15 17:13:54 -0400

    Manny: Add the "Want to get involved" component to the home page
    L= ENG-187
    
    Change-Id: Ibbe792dd891a534f2e2ae7a1df86e0def6c518d1

---
+++ js/src/patina/home/GetInvolved.tsx
@@ -0,0 +1,40 @@
+import styles from '@/patina/home/GetInvolved.module.css'
+import { StaticCards } from '@/patina/home/StaticCards.tsx'
+import { imageUrls } from '@/patina/assets/images.ts'
+
+export function GetInvolved() {
+  return (
+    <div className={styles.involvedContainer}>
+      <div className={styles.sectionHeader}>
+        <h1>{'Want to get involved?'}</h1>
+      </div>
+
+      <div className={styles.cardsContainer}>
+        <StaticCards
+          title={'For Students'}
+          imageUrl={imageUrls.learn.src}
+          imageAlt={'Learn'}
+          description={
+            'Need career fair prep, help choosing a major, interview practice, or scholarship info?'
+          }
+        />
+        <StaticCards
+          title={'For Volunteers'}
+          imageUrl={imageUrls.hands.src}
+          imageAlt={'Hands'}
+          description={
+            'Want to mentor students, organize events, or learn about other ways to get involved?'
+          }
+        />
+        <StaticCards
+          title={'For the Community'}
+          imageUrl={imageUrls.heart.src}
+          imageAlt={'Heart'}
+          description={
+            'Join us for events to form new connections, and maybe learn something new!'
+          }
+        />
+      </div>
+    </div>
+  )
+}
