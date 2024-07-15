bee809cafbe0a22ba81430d5bdcb3954f3895f04
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-15 15:18:53 -0400

    Maliha: Replaces google links with figma links.
    Change-Id: Idfb6edf37fe05d5c3063a9da5691234875c385ad

--- js/src/patina/assets/images.ts
+++ js/src/patina/assets/images.ts
@@ -1,26 +1,26 @@
 /**
- * Images for patina website, hosted using google photos
+ * Images for patina website, hosted using figma
  * https://webapps.stackexchange.com/a/98573
  */
 export const imageUrls = {
   mentorship: {
     drive:
       'https://drive.google.com/file/d/1jGA8KWh0NVbS1s-zIDzTVRY27A_Y8o86/view',
-    src: 'https://lh3.googleusercontent.com/pw/AP1GczNC28oh6XrEApaU9KvFx3LcHtqZI3h_SYsE-upbv-xfCuviZOWn1Xk-zHqP2WEM5NkVx-CY_OHZcRfjJdisQQuAkC9IWf6xNOKXe_67atsXUcc0gEOlxFNxTouvac0p0xsAC3PbHkhSWv86bZjFuMlt=w2078-h1558-s-no-gm',
+    src: 'https://s3-alpha-sig.figma.com/img/ab55/bc46/a5b565bbe44b5dc709c2b2dab294b429?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kUFLOORJwb~HPXo8mLO10mjlVwlHEwR4CcMdPTmrviVJc60RsnwZ~hnEthqvSHnYRUsW82iMWmoMxQUfR4NSOdZELtMq0TqMf4ucEJJH~MGnS7g~W9FAZ1KLHt9xJxVTIdySbDJ-ZI1cdL9~qbotGvFikWdlp3hMWNdhmZLq3~pGlMJV-pCESmkMIXEqWriywPTc5iUtSRACl3lrha3Vt4hirPJP3pYr64gVD-Qzpjwpj31IDS-kCodU6ozBJmdJMS80lXV8P5P07wcGwYvhjLB1dpSwp0gmfENLJm2t2QCP0LNsHvRuc5-HNR2y-aPgRH-ruc9jWh3243AlAtd27A__',
   },
   scholarship: {
     drive:
       'https://drive.google.com/file/d/1aDgXnl75WwSaoNFwzPiO5fVvZ78sBHPZ/view',
-    src: 'https://lh3.googleusercontent.com/pw/AP1GczPwoTf53J69OBpDfRL4ADnwLagApAbL58iIAauisxPCURVInkFulkIHDGj5832Ow_s6-b-hKqTScuNjQ3fAo8iaa708gnV09J52YI5jCgaFMw2As5EjXnuaLkC12mcWQQ4flmmL9q2QXVqLL-AAN1ZG=w800-h1200-s-no-gm',
+    src: 'https://s3-alpha-sig.figma.com/img/81bb/6576/46a9742de3b14b9fb0a861818826d53c?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=X0~nv8EK9Dkzx3v1S7fbmaoA2ymD3V-5NmvWELKTegCv~2HFUPWgvvS1R2Z72m7irGzwhaeRJOraaWxNComwPxnai706WPAZDcdOJViOfrBXcKhtF~lZ50JuS9kN5g7Dt4UKM2RgxUTBbP6FuZdEEob7Dt3b3X4A-V~nZAS0jAiEklBPRK1rO0nQ8O2b72JW-WZUYFyv~z28bfYtush9z1hQuXYVL1YuzJlGqJoPgp-bPu3VDtJAT2OK78FSCQamZxwagWczOYJ~X2nqMMm8jAtXuRjrkAQCd7-AOAqlMBGE7kABzAj68vwDGgCPLiPKTwVV8C64rAg4XwSUf1qjqg__',
   },
   community: {
     drive:
       'https://drive.google.com/file/d/16fqE7xIAEA_4mH32dLcN5lm804aBCozd/view',
-    src: 'https://lh3.googleusercontent.com/pw/AP1GczNrORpkdmN6ylW3u1eAHZH6Voqp4sLdZ81byejoOTS1h9M9GgfLXQ78_R5sIkOBSPTed2KgaJRnKCMDCuE_a4fnuvG5MNlDHhvakg7DlT7JUlknqHIpg0Dmpx99wrCgeYcjnydyPXhDt12WTHFsNTOh=w800-h533-s-no-gm',
+    src: 'https://s3-alpha-sig.figma.com/img/ebbf/e20b/737934d80aa70ba4db2d8a0667135dda?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UgsXIcTI3JAH-9aWRFbDbY0fNDEOKI4W7oD6KxmaTBNTm4BKjei9ePwnQbXiFVd32-MABrE4gVCuFfk1YS8snC2YYsyeYc3j9aHU8BjUbf7uRRUTA4m30DTEc7A~2RyWl-R~OXQ2QF7fg49kYBbCzzysA966aSoZJ~wwELC7MMcQJ2qPEvkDPVUtZtbzIgnEXrNIHu-vWIF5ZmkPUDhKKrEs6ZQsg~QEqMgKs8LlaUxpIQnvMRgaTnveLwzE4F3IBF~keRVS-6dd-SX~l7-S45s6N5UgJLryhzPfFMnZCFSU9pti1nVMW~krLCNNXUSKNni0RU2WsX0vqyP0zzI9Vg__',
   },
   splash: {
     drive:
       'https://drive.google.com/file/d/1wQR-vC-UXMqiJaBsskk6uA4Ehcpast87/view',
-    src: 'https://lh3.googleusercontent.com/pw/AP1GczPFM4RfxjygGOWyF5oUZ3ninAUAXAW_I6i--UW4lAPpSHBgz07KRGbOVKWpBWBWw8nZKBimYjLR47TMDprCglT6NuLFcnGMHRjx8NZNUKgDAz9wCLc4LE8YzPNh-ge46xIUerGiibbF5Z6gJVuBws0w=w986-h948-s-no-gm',
+    src: 'https://s3-alpha-sig.figma.com/img/bae1/524f/d1a3acc270ae541e29094cc729945808?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YZE0OS-izjxqoA2Jp1Lu9RlMncY7apaTh81o-Y7cRuEWMr6zp8rWLefIQnS96wcXRGbiPwQ0kyG75vkTqEaN4~F6bZNmxgXrQzeBrmiEql9zXS0M9MKCY1bMh9AXumHtn~DdhPrKmyVGfRPfcHaAOGLSqcWe~Hvsp4tnMADjEwAaUFWsZZY-Bl3cQIgmzAVsVgsXJBITY02qaRfN~viUUuXFMMfzYjWZyAvVbZsclQTdji~6cpj3OluT8P1uw22aVzv2ODutRV1UzryGO24FJj2vYFHQg7xOHgIN0j1sMjnog5bgax-kf88cCBZimeuclGr-8TQFOuNf6wYFby0y4A__',
   },
 }
