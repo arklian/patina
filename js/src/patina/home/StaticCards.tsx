3becf668681679c2c505f0a8a08fede930b0d02e
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-15 17:13:54 -0400

    Home: Add the "Want to get involved" component to the home page
    L=ENG-187
    
    Change-Id: Ibbe792dd891a534f2e2ae7a1df86e0def6c518d1

---
+++ js/src/patina/home/StaticCards.tsx
@@ -0,0 +1,29 @@
+import { Button, Card, Image } from '@mantine/core'
+import styles from '@/patina/home/StaticCards.module.css'
+
+type StaticCardsProps = {
+  title: string
+  imageUrl: string
+  imageAlt: string
+  description: string
+}
+
+export function StaticCards({
+  title,
+  imageUrl,
+  imageAlt,
+  description,
+}: StaticCardsProps) {
+  return (
+    <Card className={styles.card}>
+      <h3>{title}</h3>
+      <div className={styles.cardImageContainer}>
+        <Image src={imageUrl} alt={imageAlt} className={styles.cardImage} />
+      </div>
+      <p className={styles.cardText}>{description}</p>
+      <Button variant={'outline'} className={styles.cardButton}>
+        {'Check it out'}
+      </Button>
+    </Card>
+  )
+}
