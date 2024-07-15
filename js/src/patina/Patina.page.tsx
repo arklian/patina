07b451ab15004204694add3b0c9ad53466918493
Henry Chen <hjc77@cornell.edu>
2024-07-15 00:13:52 -0400

    Patina: Add navigation header
    Copied this mostly from the Mantine examples:
    https://ui.mantine.dev/category/headers/
    
    TEST=manual
    
    Change-Id: I98b25f842cf10703b8ca804f1c3be3049346ad71

--- js/src/patina/Patina.page.tsx
+++ js/src/patina/Patina.page.tsx
@@ -1,11 +1,15 @@
-import { MantineProvider } from '@mantine/core'
+import { MantineProvider, Stack } from '@mantine/core'
 import { Outlet } from 'react-router-dom'
+import { NavigationHeader } from '@patina/NavigationHeader.tsx'
 import { theme, resolver } from './theme.ts'
 
 export function PatinaPage() {
   return (
     <MantineProvider theme={theme} cssVariablesResolver={resolver}>
-      <Outlet />
+      <Stack>
+        <NavigationHeader />
+        <Outlet />
+      </Stack>
     </MantineProvider>
   )
 }
