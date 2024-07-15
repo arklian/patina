1eff5958422e7efc0d39b87e42a7a99419fab91d
Jionghao Wu <jhwu03@gmail.com>
2024-07-15 12:27:42 -0400

    Jionghao: Create about me text and button
    L=ENG-190
    
    Change-Id: I727698ab5cf90b793f3173179d8b6bde45107c05

---
+++ js/src/patina/home/About.tsx
@@ -0,0 +1,23 @@
+import { Text, Button } from '@mantine/core'
+import styles from './About.module.css'
+
+export function About() {
+  return (
+    <div className={styles.temp}>
+      <div className={styles.container}>
+        <Text className={styles.aboutText}>
+          {'We’re on a mission to address the unique struggles of AANHPI women and foster the diverse, ' +
+            'inclusionary leaders of tomorrow.'}
+        </Text>
+        <Text className={styles.aboutText}>
+          {
+            'Wherever you are on your career and allyship journey, we’re here to boldly celebrate what makes you unique—your patina.'
+          }
+        </Text>
+        <Button variant={'white'} c={'black'} h={'43px'} w={'95px'}>
+          {'More about us'}
+        </Button>
+      </div>
+    </div>
+  )
+}
