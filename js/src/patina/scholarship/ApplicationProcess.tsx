9666f81a1d1da45118d0024347513fef113e55a9
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-16 16:26:13 -0400

    Maliha: Make stack component that is reusable throughout the pages
    L=ENG-243
    TEST=manual
    Ran manual test, passed except for personal website pages.
    
    Change-Id: I3b827db546f0673327d2e74da981648be5a0f803

--- js/src/patina/scholarship/ApplicationProcess.tsx
+++ js/src/patina/scholarship/ApplicationProcess.tsx
@@ -1,25 +1,18 @@
 import { Text } from '@mantine/core'
+import { Stacks } from './Stacks'
 import styles from './ApplicationProcess.module.css'
 
+const processStack = [
+  { title: 'OCT 10', description: 'Applications Open' },
+  { title: 'OCT 10', description: 'Application Deadline' },
+  { title: 'OCT 10', description: 'Results Announced' },
+]
 /** Displays information about the application deadlines */
 export function ApplicationProcess() {
   return (
     <div className={styles.container}>
       <Text className={styles.title}>{'Application Process'}</Text>
-      <div className={styles.itemsMain}>
-        <div className={styles.itemContainer}>
-          <Text className={styles.date}>{'OCT 10'}</Text>
-          <Text className={styles.description}>{'Applications Open'}</Text>
-        </div>
-        <div className={styles.itemContainer}>
-          <Text className={styles.date}>{'OCT 10'}</Text>
-          <Text className={styles.description}>{'Application Deadline'}</Text>
-        </div>
-        <div className={styles.itemContainer}>
-          <Text className={styles.date}>{'OCT 10'}</Text>
-          <Text className={styles.description}>{'Recipients Announced'}</Text>
-        </div>
-      </div>
+      <Stacks items={processStack} />
     </div>
   )
 }
