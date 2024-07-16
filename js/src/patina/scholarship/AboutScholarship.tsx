1680db7a28ee7a696c4c04a4dca1ea9294bd6f23
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-16 18:06:18 -0400

    Maliha: Separate scholarship about section into two sections
    Felt like the whole div could be divided into two separate components
    because they both have very different layouts. Also fixed minor issues
    like rem(0px) and prettier tag issues.
    
    L=ENG-241
    L=ENG-226
    TEST=manual
    Ran it and it passed (except for issues in our personal pages).
    
    Change-Id: Ifd710a1786f90e0e47ae343ae21ff1ecf23af9f2

---
+++ js/src/patina/scholarship/AboutScholarship.tsx
@@ -0,0 +1,18 @@
+import { Text } from '@mantine/core'
+import styles from './AboutScholarship.module.css'
+
+/** Displays information about the scholarship */
+export function AboutScholarship() {
+  return (
+    <div className={styles.about}>
+      <div className={styles.text}>
+        <Text className={styles.title}>{'About the Scholarship'}</Text>
+        <Text className={styles.body}>
+          {
+            'The APIA Scholarship is our largest scholarship program, open to AANHPI undergraduate students attending any U.S. accredited university or college. Scholarship amounts range from $2,500 one-year awards to $20,000 multi-year awards. APIA Scholars has a special focus on supporting AANHPI students who live at or below the poverty line; are in the first generation of their family to attend college; are representative of the APIA community’s diversity, (geographically and ethnically}, especially those ethnicities that have been underrepresented on college campuses due to limited access and opportunity. Strong applicants would also have an emphasis on community service and leadership.'
+          }
+        </Text>
+      </div>
+    </div>
+  )
+}
