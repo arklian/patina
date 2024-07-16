d3f9ea8c68b857cfa84503fd366e58af3b970d24
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-16 18:15:03 -0400

    Scholarship: Separate section into two
    Felt like the whole div could be divided into two separate components
    because they both have very different layouts. Also fixed minor issues
    like rem(0px) and prettier tag issues.
    
    L=ENG-241
    L=ENG-226
    TEST=manual
    Ran it and it passed (except for issues in our personal pages).
    
    Change-Id: Ifd710a1786f90e0e47ae343ae21ff1ecf23af9f2

---
+++ js/src/patina/scholarship/ApplicationProcess.tsx
@@ -0,0 +1,18 @@
+import { Text } from '@mantine/core'
+import { Stacks } from '../components/Stacks'
+import styles from './ApplicationProcess.module.css'
+
+const processStack = [
+  { title: 'OCT 10', description: 'Applications Open' },
+  { title: 'OCT 10', description: 'Application Deadline' },
+  { title: 'OCT 10', description: 'Results Announced' },
+]
+/** Displays information about the application deadlines */
+export function ApplicationProcess() {
+  return (
+    <div className={styles.container}>
+      <Text className={styles.title}>{'Application Process'}</Text>
+      <Stacks items={processStack} />
+    </div>
+  )
+}
