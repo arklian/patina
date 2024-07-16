8f7401191015311ad62239c5640b3e3931320dd0
Tony Wang <wangtony4005@gmail.com>
2024-07-16 14:14:00 -0400

    Mentorship: Finished section 2 (about section)
    Wrapped section in a div and then separated the div
    into 2 sections to work on left and right side.
    Used the font, color and font-size in the figma.
    
    L=ENG-231
    TEST=manual
    
    Change-Id: Ia02e95bb3001c742380e2a74f250ff6ac73bfb09

--- js/src/patina/mentorship/Mentorship.page.tsx
+++ js/src/patina/mentorship/Mentorship.page.tsx
@@ -1,9 +1,10 @@
-import { Container, Skeleton, Stack } from '@mantine/core'
-
+import { Container, Skeleton, Stack, Text } from '@mantine/core'
+import styles from './Mentorship.module.css'
 /**
  * MentorshipPage component renders a mentorship page layout with multiple sections.
  * Each section is represented by a Skeleton component, which can be replaced with actual content.
- */
+ * */
+
 export function MentorshipPage() {
   return (
     <Container>
@@ -12,7 +13,29 @@ export function MentorshipPage() {
         {/* Section 1 */}
         <Skeleton animate={false} height={70} width="100%" mb="xl" />
         {/* Section 2 */}
-        <Skeleton animate={false} height={70} width="100%" mb="xl" />
+        <div className={styles.section2}>
+          <div className={styles.right}>
+            <h2 className={styles.aboutTitle}>About the Program</h2>
+            <Text className={styles.aboutText}>
+              If you ever felt that you were alone in the journey of educational
+              or career navigation, this is a safe space for you to meet someone
+              who will be like the older sibling, relative, or elder who can
+              mentor and coach you to meet your workplace readiness goals. Our
+              mentors will guide you to be better ready for your desired career
+              track.
+            </Text>
+            <h2 className={styles.timeCommitmentTitle}>Time Commitment</h2>
+            <Text className={styles.timeCommitmentText}>
+              Students are expected to commit to a 12 week program where they
+              meet their Mentor for 1 hour on a biweekly basis.
+            </Text>
+          </div>
+          <div className={styles.left}>
+            <Text className={styles.quote}>
+              " Quote from a student about how mentorship has helped them "
+            </Text>
+          </div>
+        </div>
         {/* Section 3 */}
         <Skeleton animate={false} height={70} width="100%" mb="xl" />
       </Stack>
