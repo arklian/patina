f2f5358f5643a0dc382f654513fd5ba41c684d86
Riyuan Liu <riyusnliu@gmail.com>
2024-07-16 12:35:07 -0400

    PatNet: Mentorship page layout
    Separate different part into it's own component
    
    L=ENG-235
    
    Change-Id: I70744ae11e331c70b9ad0a8e5358156c875520d5

---
+++ js/src/patina/mentorship/about/About.tsx
@@ -0,0 +1,6 @@
+import { Skeleton } from '@mantine/core'
+
+// Defining the About component
+export function About() {
+  return <Skeleton animate={false} height={70} width="100%" mb="xl" />
+}
---
+++ js/src/patina/mentorship/gain/Gain.tsx
@@ -0,0 +1,6 @@
+import { Skeleton } from '@mantine/core'
+
+// Defining the Gain component
+export function Gain() {
+  return <Skeleton animate={false} height={70} width="100%" mb="xl" />
+}
