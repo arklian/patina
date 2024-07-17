288157461a76cc71304303621e585a8e5e5f6fc3
Martin M <m2msquared47@gmail.com>
2024-07-17 16:00:09 -0700

    Martin: Add Navigation Menu
    Used AppShell to contain the header, navigation menu, and the page
    currently active. The Burger component is used to collapse the nav
    menu to increase screen space.
    
    Change-Id: I47dd2ac6994f15596d05732866f30c50a8528b54

--- js/src/personal/martin/Martin.page.tsx
+++ js/src/personal/martin/Martin.page.tsx
@@ -1,23 +1,38 @@
-import {
-  Container,
-  Flex,
-  Image,
-  Title,
-  MantineProvider
-} from '@mantine/core'
+import {AppShell, Burger, Flex, Group, Image, MantineProvider, Title} from '@mantine/core'
 import { Outlet } from 'react-router-dom'
 import { theme } from './theme.ts'
+import Logo from "@/personal/martin/assets/MartinLogo.ico";
+import {useDisclosure} from "@mantine/hooks";
 
 export function MartinPage() {
+  const [openedNav, { toggle: toggleNav }] = useDisclosure(false);
   return (
     <MantineProvider theme={theme}>
-      <Container size="responsive" bg="cobalt">
-        <Flex gap="xl" justify="flex-start">
-          <Image w={32} h={32} radius="sm" src="https://m-squared47.github.io/content/logo.ico"/>
-          <Title order={1}>{"Martin's Page"}</Title>
-        </Flex>
-      </Container>
-      <Outlet />
+      <AppShell
+        header={{ height: '5em' }}
+        navbar={{
+          width: '10em',
+          breakpoint:'sm',
+          collapsed: { mobile: !openedNav, desktop: !openedNav },
+        }}
+        padding='md'
+      >
+        <AppShell.Header>
+          <Group h="100%" px="md">
+            <Flex gap="xl" justify="flex-start">
+              <Burger opened={openedNav} onClick={toggleNav} />
+              <Image w={48} h={48} radius="md" src={ Logo } alt='Martin Logo'/>
+              <Title order={1}>{"Martin's Page"}</Title>
+            </Flex>
+          </Group>
+        </AppShell.Header>
+        <AppShell.Navbar>
+          <Title order={2}>NavBar</Title>
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <Outlet />
+        </AppShell.Main>
+      </AppShell>
     </MantineProvider>
   )
 }
