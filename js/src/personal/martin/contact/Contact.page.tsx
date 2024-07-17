d9476b025c7047b8d023b75b01818bb81beba8ad
Martin M <m2msquared47@gmail.com>
2024-07-16 20:10:26 -0700

    Martin: Remove MantineProvider
    Did some research on react-router. Removed MantineProvider from about,
    contact, main, and project and added Outlet for child routing.
    
    Change-Id: I4c8c580ee9fdc84f6b4155d8b1017696f10e82ef

--- js/src/personal/martin/contact/Contact.page.tsx
+++ js/src/personal/martin/contact/Contact.page.tsx
@@ -1,11 +1,10 @@
-import { Title, Text, MantineProvider } from '@mantine/core'
-import { theme } from '@/personal/martin/theme.ts'
+import { Title, Text } from '@mantine/core'
 
 export function MartinContact() {
   return (
-    <MantineProvider theme={theme}>
+    <>
       <Title order={1}>{'Contact'}</Title>
       <Text fw={500}>{'Contact Page'}</Text>
-    </MantineProvider>
+    </>
   )
 }
