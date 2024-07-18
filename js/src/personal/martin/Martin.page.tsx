97cf3fe8ed6bb2297a74ebf183e089911fb1bd70
Martin M <m2msquared47@gmail.com>
2024-07-17 18:07:43 -0700

    Martin: Add Navigation Menu
    Used AppShell to contain the header, navigation menu, and the page
    currently active. The Burger component is used to collapse the nav
    menu to increase screen space. Cleaned up Martin.page.tsx and moved the
    AppShell to its own .tsx file. You can now navigate all pages from the
    navigation menu.
    
    Change-Id: I72d8f14894cbc18614e59864db9faf0c5d424006

--- js/src/personal/martin/Martin.page.tsx
+++ js/src/personal/martin/Martin.page.tsx
@@ -1,24 +1,11 @@
-import {
-  Container,
-  Flex,
-  Image,
-  Title,
-  MantineProvider
-} from '@mantine/core'
-import { Outlet } from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
+import { HeaderNav } from './components/HeaderNav.tsx'
 import { theme } from './theme.ts'
-import {Outlet} from "react-router-dom";
 
 export function MartinPage() {
   return (
     <MantineProvider theme={theme}>
-      <Container size="responsive" bg="cobalt">
-        <Flex gap="xl" justify="flex-start">
-          <Image w={32} h={32} radius="sm" src="https://m-squared47.github.io/content/logo.ico"/>
-          <Title order={1}>{"Martin's Page"}</Title>
-        </Flex>
-      </Container>
-      <Outlet/>
+      <HeaderNav />
     </MantineProvider>
   )
 }
