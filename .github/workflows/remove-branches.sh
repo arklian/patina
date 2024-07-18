fe7778362d285e6789040351ed3e5338ce6086ea
Amanda Ruan <amandaruan262@gmail.com>
2024-07-18 20:32:45 +0000

    CI/CD: Remove CI branch if inactive for a week
    Modified workflow to delete branches if CR has not been
    updated for over a week.
    
    L=ENG-247
    
    TEST=manual
    - Created branch for CR that hasn't been updated for over a week
    - Modified condition in workflow to make sure branch will delete based on update time
    - Branch was deleted
    
    Test run:
    https://github.com/arklian/patina/actions/runs/9997135078
    
    Change-Id: Ia53b0a85d91ec2a6760efba7a67c2f761cd7b36f

--- .github/workflows/remove-branches.sh
+++ .github/workflows/remove-branches.sh
@@ -19,9 +19,12 @@ for branch in "${branches[@]}";
 do
   json=$(curl -u bot-patina:$BOT_PWD https://gerrithub.io/a/changes/$branch)
   status=($(echo $json | grep -o '"status":"[^"]*' | grep -o '[^"]*$'))
+  updated=($(echo $json | grep -o '"updated":"[^"]*' | grep -o '[^"]*$' | cut -c 1-10))
+  daysPassed=$(( $(( $(date +%s) - $(date -d "$updated" +%s))) / 86400 ))
 
   echo "Branch ci/$branch has the status $status"
-  if [[ $status == "MERGED" || $status == "ABANDONED" ]]; then
+  echo "Branch ci/$branch was last updated $daysPassed day(s) ago"
+  if [[ $status == "MERGED" || $status == "ABANDONED" || $daysPassed -gt 7 ]]; then
     git push origin --delete ci/$branch
     deletedBranches+=("$branch")
     echo -e "Branch ci/$branch was deleted\n"
@@ -35,5 +38,5 @@ if [[ ${#deletedBranches[@]} == 0 ]]; then
   echo "No CI branches deleted"
 else
   echo "Deleted branches:"
-  printf '%s\n' "${deletedBranches[@]}"
+  printf 'ci/%s\n' "${deletedBranches[@]}"
 fi
