bb7e46936fb592d7e8ae61b2768f38c32c59c263
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-18 16:39:06 -0400

    Mentorship: Makes details section of page
    Also got rid of criteria component because Kelly worked on it
    
    L=ENG-233
    TEST=manual
    Ran test and it passed.
    
    Change-Id: I2105cced6b3de713a0fd50faf41f5dd8e9db71b6

---
+++ js/src/patina/mentorship/details/Details.tsx
@@ -0,0 +1,46 @@
+import { Text } from '@mantine/core'
+import styles from './Details.module.css'
+
+/** Displays program details */
+export function Details() {
+  return (
+    <div className={styles.container}>
+      <Text className={styles.title}>{'Program Details'}</Text>
+      <Text className={styles.about}>
+        {'There are two types of Mentors. Your'}
+        <div className={styles.colored}>&nbsp;{'Track Mentor'}&nbsp;</div>
+        {
+          'will be your 1:1 Mentor who works in a field closest to the field you are interested in pursuing.'
+        }
+        <div className={styles.colored}>&nbsp;{'Group Mentor'}&nbsp;</div>
+        {
+          'is a Mentor who oversees all Mentees and provides DEI and Workplace readiness mentorship.'
+        }
+      </Text>
+      <div className={styles.list}>
+        <div className={styles.list1}>
+          <Text className={styles.item}>
+            {'1. Match with your Track Mentor[Track]'}
+          </Text>
+          <Text className={styles.item}>{'2. Career Exploration [Track]'}</Text>
+          <Text className={styles.item}>{'3. DEI Workshop [Group]'}</Text>
+          <Text className={styles.item}>
+            {'4. Networking Fundamentals [Group]'}
+          </Text>
+        </div>
+        <div className={styles.list2}>
+          <Text className={styles.item}>
+            {'5. Workplace Readiness [Track/Group]'}
+          </Text>
+          <Text className={styles.item}>
+            {'6. Progress Report Presentation [Group]'}
+          </Text>
+          <Text className={styles.item}>
+            {'7. Execute Goals [Track/Group]'}
+          </Text>
+          <Text className={styles.item}>{'8. Final Evaluation [All]'}</Text>
+        </div>
+      </div>
+    </div>
+  )
+}
