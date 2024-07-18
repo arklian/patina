089e208971f204f5a6868a3477bdbe7f2b8b38d7
Amanda Ruan <amandaruan262@gmail.com>
2024-07-17 22:33:34 -0400

    CI/CD: Remove CI branches if CR is merged/abandoned
    Created new Github Actions workflow that runs at the
    beginning of each day to delete CI branches for merged
    or abandoned commits.
    
    L=ENG-236
    L=ENG-238
    L=ENG-239
    
    TEST=manual
    - Created CI branches for merged and abandoned commits
    - Manually ran the workflow to see if branches were deleted
    - Branches were deleted
    - Unable to test if schedule works because workflow needs
    to be on the default/main branch
    https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#schedule
    
    Most recent test run:
    https://github.com/arklian/patina/actions/runs/9984560032
    
    Change-Id: I3f89b1e500e277128d455135be826f66e8d4c0eb

---
+++ .github/workflows/remove-branches.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Retrieve CI branches
+echo "Retrieving CI branches..."
+branches=($(git branch -r --list origin/ci/\* | sed 's/.*ci\///'))
+if [[ ${#branches[@]} == 0 ]]; then
+  echo "No CI branches found"
+  exit 0
+fi
+
+# Print CI branches
+printf 'ci/%s\n' "${branches[@]}"
+echo ""
+
+# Check each branch
+deletedBranches=()
+
+for branch in "${branches[@]}";
+do
+  json=$(curl -u bot-patina:$BOT_PWD https://gerrithub.io/a/changes/$branch)
+  status=($(echo $json | grep -o '"status":"[^"]*' | grep -o '[^"]*$'))
+
+  echo "Branch ci/$branch has the status $status"
+  if [[ $status == "MERGED" || $status == "ABANDONED" ]]; then
+    git push origin --delete ci/$branch
+    deletedBranches+=("$branch")
+    echo -e "Branch ci/$branch was deleted\n"
+  else
+    echo -e "Branch ci/$branch was not deleted\n"
+  fi
+done
+
+# Print deleted branches
+if [[ ${#deletedBranches[@]} == 0 ]]; then
+  echo "No CI branches deleted"
+else
+  echo "Deleted branches:"
+  printf '%s\n' "${deletedBranches[@]}"
+fi
