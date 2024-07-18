d00d4f5c1a6c47eb1b46340a9896efd6dec34513
Riyuan Liu <riyusnliu@gmail.com>
2024-07-18 12:28:14 -0400

    Riyuan: Finished Hero/Home Section
    Added Introduction sentence for Home section
    
    L=ENG-248
    TEST=manual
    -Page loads locally
    
    Change-Id: I6a9906c1b39c0f02760ba1c0300426894336a9ed

--- js/src/personal/riyuan/components/home/Home.tsx
+++ js/src/personal/riyuan/components/home/Home.tsx
@@ -1,24 +1,72 @@
-import { Text, Title, Container } from '@mantine/core'
-import styles from '@/personal/riyuan/Riyuan.module.css'
+import {
+  Text,
+  Title,
+  Container,
+  Group,
+  Button,
+  Image,
+  SimpleGrid,
+} from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
 import hstyles from '@/personal/riyuan/components/home/Home.module.css'
+import typing from '@/personal/riyuan/assets/gif/typing.gif'
 
 /**
  *  Function to render the Home section of the MainPage ->
  *  Provides a welcoming introduction to visitors
  */
 export function Home() {
+  const largeScreen = useMediaQuery('(min-width: 60em)')//960px
   return (
-    <div id={'Home'} className={styles.container}>
-      <Container className={hstyles.inner}>
-        <Title className={styles.title} ta="left" mt={100}>
-          {'Hello!'}
-        </Title>
-        <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
-          {
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-          }
-        </Text>
-      </Container>
-    </div>
+    <Container className={hstyles.wrapper}>
+      <SimpleGrid cols={largeScreen ? 2 : 1} spacing="xl">
+        <div>
+          <Title size={40} className={hstyles.title1} ta={'left'}>
+            {'Hi there I'}&apos;{'m'}{' '}
+          </Title>
+          <Title className={hstyles.title2} ta={'left'}>
+            <Text component="span" className={hstyles.highlight} inherit>
+              {'Riyuan'}
+            </Text>
+          </Title>
+          <Title className={hstyles.title3}>{'a Software Engineer!'}</Title>
+          <Text
+            size="lg"
+            color="dimmed"
+            className={hstyles.description}
+            ta={'left'}
+          >
+            {
+              'This website is a space where I share my experiences, interests, and'
+            }
+            {'personal projects. If you'}&apos;
+            {'re interested in learning more about'}
+            {
+              'me and my work, keep scrolling to explore my skills and projects.'
+            }
+          </Text>
+          <Group justify={'left'} pt={'md'}>
+            <Button
+              className={hstyles.control}
+              ta={'center'}
+              size="lg"
+              variant="default"
+              color="gray"
+              mr="md"
+              component="a"
+              target="_blank"
+              href=""
+            >
+              <Text component="span" className={hstyles.highlight} inherit>
+                {'Check my resume'}
+              </Text>
+            </Button>
+          </Group>
+        </div>
+        <Group className={hstyles.Typing} justify={'center'} pt={'lg'}>
+          <Image src={typing} alt="My GIF" height={400} radius="md" />
+        </Group>
+      </SimpleGrid>
+    </Container>
   )
 }
