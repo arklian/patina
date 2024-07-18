ebb7765fa025b491c85fb0c53e1b702769d9e34c
Tony Wang <wangtony4005@gmail.com>
2024-07-18 12:28:04 -0400

    Mentorship: Finished section 2 (about section)
    Wrapped section in a div and then separated the div
    into 2 sections to work on left and right side.
    Used the font, color and font-size in the figma.
    
    L=ENG-231
    TEST=manual
    
    Change-Id: Ia02e95bb3001c742380e2a74f250ff6ac73bfb09

--- js/src/patina/mentorship/about/About.tsx
+++ js/src/patina/mentorship/about/About.tsx
@@ -1,6 +1,30 @@
-import { Skeleton } from '@mantine/core'
+import { Text } from '@mantine/core'
+import styles from './About.module.css'
 
 // Defining the About component
 export function About() {
-  return <Skeleton animate={false} height={70} width="100%" mb="xl" />
+  return (
+    <div className={styles.section2}>
+      <div className={styles.left}>
+        <h2 className={styles.aboutTitle}>About the Program</h2>
+        <Text className={styles.aboutText}>
+          If you ever felt that you were alone in the journey of educational or
+          career navigation, this is a safe space for you to meet someone who
+          will be like the older sibling, relative, or elder who can mentor and
+          coach you to meet your workplace readiness goals. Our mentors will
+          guide you to be better ready for your desired career track.
+        </Text>
+        <h2 className={styles.timeCommitmentTitle}>Time Commitment</h2>
+        <Text className={styles.timeCommitmentText}>
+          Students are expected to commit to a 12 week program where they meet
+          their Mentor for 1 hour on a biweekly basis.
+        </Text>
+      </div>
+      <div className={styles.right}>
+        <Text className={styles.quote}>
+          " Quote from a student about how mentorship has helped them "
+        </Text>
+      </div>
+    </div>
+  )
 }
