c7c24f5cab86ce9e2593189450dbba6560e91488
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-18 17:33:01 -0400

    Mentorship: Makes details section of page
    Also got rid of criteria component because Kelly worked on it
    
    L=ENG-233
    TEST=manual
    Ran test and it passed.
    
    Change-Id: I2105cced6b3de713a0fd50faf41f5dd8e9db71b6

--- js/src/patina/mentorship/Mentorship.page.tsx
+++ js/src/patina/mentorship/Mentorship.page.tsx
@@ -4,13 +4,14 @@ import { Apply } from '@/patina/mentorship/apply/Apply.tsx'
 import { Gain } from '@/patina/mentorship/gain/Gain.tsx'
 import { Hero } from '@/patina/mentorship/hero/Hero.tsx'
 import { Impact } from '@/patina/mentorship/impact/Impact.tsx'
+import { Details } from '@/patina/mentorship/details/Details.tsx'
 /**
  * MentorshipPage component renders a mentorship page layout with multiple sections.
  * Each section is represented by a Skeleton component, which can be replaced with actual content.
  * */
 export function MentorshipPage() {
   return (
-    <Container>
+    <Container size="80rem">
       <Stack>
         {'Mentorship Page'}
         {/* Section 1 */}
@@ -20,6 +21,7 @@ export function MentorshipPage() {
         {/* Section 3 */}
         <Gain />
         {/* Section 4 */}
+        <Details />
         <Apply />
         {/* Section 5 */}
         <Impact />
