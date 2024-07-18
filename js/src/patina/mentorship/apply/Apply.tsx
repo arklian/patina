e3b88e7d9c9e2b8aa675d8e0d0d00294f1dd7d1b
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-18 16:39:06 -0400

    Mentorship: Makes details section of page
    Also got rid of criteria component because Kelly worked on it
    
    L=ENG-233
    TEST=manual
    Ran test and it passed.
    
    Change-Id: I2105cced6b3de713a0fd50faf41f5dd8e9db71b6

--- js/src/patina/mentorship/apply/Apply.tsx
+++ js/src/patina/mentorship/apply/Apply.tsx
@@ -1,6 +1,5 @@
 import { Button, Text } from '@mantine/core'
 import { Stacks } from '../../components/Stacks'
-import { Criteria } from '@/patina/components/Criteria'
 import styles from './Apply.module.css'
 
 const applyStack = [
@@ -9,35 +8,23 @@ const applyStack = [
   { title: 'Requirement 3', description: 'Explain' },
 ]
 
-const criteria = [
-  { item: 'Must be currently enrolled in an undergraduate program' },
-  { item: 'Must be currently enrolled in an undergraduate program' },
-  { item: 'Must be currently enrolled in an undergraduate program' },
-  { item: 'Must be currently enrolled in an undergraduate program' },
-  { item: 'Must be currently enrolled in an undergraduate program' },
-]
-/** Displays info on applying to the mentorship program */
+/** Displays requirements on applying to the mentorship program */
 export function Apply() {
   return (
     <div className={styles.itemContainer}>
-      <Criteria items={criteria} />
-      <div className={styles.itemContainer2}>
-        <Text className={styles.title}>{'How to Apply'}</Text>
-        <Stacks items={applyStack}></Stacks>
-        <div className={styles.buttonContainer}>
-          {/*TODO: Replace link for the button when it's ready*/}
-          <Button
-            component="a"
-            className={styles.appButton}
-            variant="filled"
-            color="white"
-            href="templink"
-          >
-            <Text className={styles.appButtonText} size="20" fw={600}>
-              {'Apply Here'}
-            </Text>
-          </Button>
-        </div>
+      <Text className={styles.title}>{'How to Apply'}</Text>
+      <Stacks items={applyStack}></Stacks>
+      <div className={styles.buttonContainer}>
+        {/*TODO: Replace link for the button when it's ready*/}
+        <Button
+          component="a"
+          className={styles.appButton}
+          variant="filled"
+          color="white"
+          href="templink"
+        >
+          <Text className={styles.appButtonText}>{'Apply Here'}</Text>
+        </Button>
       </div>
     </div>
   )
