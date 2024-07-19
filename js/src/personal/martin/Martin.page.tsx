71a0c475e946ecd5da9652a33d77422f5edfd01a
Martin M <m2msquared47@gmail.com>
2024-07-18 19:43:46 -0700

    Martin: Added a Basic Header
    Blocking out and creating elements for the header before exporting to
    a .css/.tsx files.
    
    Change-Id: Ice5ee53eee1e4f8e29819f116e7c0c283a8a508a

--- js/src/personal/martin/Martin.page.tsx
+++ js/src/personal/martin/Martin.page.tsx
@@ -1,12 +1,23 @@
-import { Title, Text, MantineProvider } from '@mantine/core'
+import {
+  Container,
+  Flex,
+  Image,
+  Title,
+  MantineProvider
+} from '@mantine/core'
+import { Outlet } from 'react-router-dom'
 import { theme } from './theme.ts'
 import {Outlet} from "react-router-dom";
 
 export function MartinPage() {
   return (
     <MantineProvider theme={theme}>
-      <Title order={1}>{"Martin's Page"}</Title>
-      <Text fw={500}>{"Welcome to Martin's Page"}</Text>
+      <Container size="responsive" bg="cobalt">
+        <Flex gap="xl" justify="flex-start">
+          <Image w={32} h={32} radius="sm" src="https://m-squared47.github.io/content/logo.ico"/>
+          <Title order={1}>{"Martin's Page"}</Title>
+        </Flex>
+      </Container>
       <Outlet/>
     </MantineProvider>
   )
