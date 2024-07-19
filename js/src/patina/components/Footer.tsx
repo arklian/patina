2ea171ae63a7465d7fc93d2de3fa0171293e77dd
Tony Wang <wangtony4005@gmail.com>
2024-07-19 14:58:49 -0400

    Footer: Added Logo and Link Icons
    Added logo as Img and used react icons for link icons.
    Not sure what to use as color for the icons so I left them
    as the default blue.
    
    L=ENG-184
    TEST=manual
    
    Change-Id: Ibf96a6afe054acc33f20696f343e1277dea046d1

--- js/src/patina/components/Footer.tsx
+++ js/src/patina/components/Footer.tsx
@@ -1,5 +1,7 @@
-import { Text, Skeleton } from '@mantine/core'
+import { ActionIcon, Text } from '@mantine/core'
+import { FaEnvelope, FaLinkedin } from 'react-icons/fa'
 import styles from './Footer.module.css'
+import patinaLogo from '../../patinasquare.svg'
 
 /**
  * Footer component to display patina network logo, social media links
@@ -7,10 +9,29 @@ import styles from './Footer.module.css'
 export function Footer() {
   return (
     <div className={styles.footer}>
-      <Skeleton animate={false} w={48} />
+      <div className={styles.logoTextContainer}>
+        <img
+          src={patinaLogo}
+          alt="Patina Network Logo"
+          className={styles.logo}
+        />
+        <div className={styles.textContainer}>
+          <Text className={styles.patina}>{'Patina'}</Text>
+          <Text className={styles.patina}>{'Network'}</Text>
+        </div>
+      </div>
       <div className={styles.footerLinks}>
-        <Text>{'link1'}</Text>
-        <Text>{'link2'}</Text>
+        <ActionIcon component="a" href="mailto:hi@patinanetwork.org" size="lg">
+          <FaEnvelope size={24} />
+        </ActionIcon>
+        <ActionIcon
+          component="a"
+          href="https://www.linkedin.com/company/patinanetwork/"
+          target="_blank"
+          size="lg"
+        >
+          <FaLinkedin size={24} />
+        </ActionIcon>
       </div>
     </div>
   )
