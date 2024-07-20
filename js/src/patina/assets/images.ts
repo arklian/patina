595b58f5b3825c344ef249d41ed9ad472efc91d7
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-20 14:44:59 +0000

    Patnet: Change the image hosting service
    - this change changes the hosting service for the images
    from figma to digital ocean
    
    - Add another image to image.ts
    
    L=ENG-255
    
    Change-Id: Ie804e55ac3951aa363a6d90ec4fd7c63471e0384

--- js/src/patina/assets/images.ts
+++ js/src/patina/assets/images.ts
@@ -3,44 +3,49 @@
  * https://webapps.stackexchange.com/a/98573
  */
 export const imageUrls = {
+  patinaLogo: {
+    drive:
+      'https://drive.google.com/file/d/1DwXQtbDrDBiRiS96KEbJj84FIj_3ARuI/view',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/logo%202.png',
+  },
   mentorship: {
     drive:
       'https://drive.google.com/file/d/1jGA8KWh0NVbS1s-zIDzTVRY27A_Y8o86/view',
-    src: 'https://s3-alpha-sig.figma.com/img/ab55/bc46/a5b565bbe44b5dc709c2b2dab294b429?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kUFLOORJwb~HPXo8mLO10mjlVwlHEwR4CcMdPTmrviVJc60RsnwZ~hnEthqvSHnYRUsW82iMWmoMxQUfR4NSOdZELtMq0TqMf4ucEJJH~MGnS7g~W9FAZ1KLHt9xJxVTIdySbDJ-ZI1cdL9~qbotGvFikWdlp3hMWNdhmZLq3~pGlMJV-pCESmkMIXEqWriywPTc5iUtSRACl3lrha3Vt4hirPJP3pYr64gVD-Qzpjwpj31IDS-kCodU6ozBJmdJMS80lXV8P5P07wcGwYvhjLB1dpSwp0gmfENLJm2t2QCP0LNsHvRuc5-HNR2y-aPgRH-ruc9jWh3243AlAtd27A__',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/mentorship.jpeg',
   },
   scholarship: {
     drive:
       'https://drive.google.com/file/d/1aDgXnl75WwSaoNFwzPiO5fVvZ78sBHPZ/view',
-    src: 'https://s3-alpha-sig.figma.com/img/81bb/6576/46a9742de3b14b9fb0a861818826d53c?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=X0~nv8EK9Dkzx3v1S7fbmaoA2ymD3V-5NmvWELKTegCv~2HFUPWgvvS1R2Z72m7irGzwhaeRJOraaWxNComwPxnai706WPAZDcdOJViOfrBXcKhtF~lZ50JuS9kN5g7Dt4UKM2RgxUTBbP6FuZdEEob7Dt3b3X4A-V~nZAS0jAiEklBPRK1rO0nQ8O2b72JW-WZUYFyv~z28bfYtush9z1hQuXYVL1YuzJlGqJoPgp-bPu3VDtJAT2OK78FSCQamZxwagWczOYJ~X2nqMMm8jAtXuRjrkAQCd7-AOAqlMBGE7kABzAj68vwDGgCPLiPKTwVV8C64rAg4XwSUf1qjqg__',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/scholarship.jpeg',
   },
   community: {
     drive:
       'https://drive.google.com/file/d/16fqE7xIAEA_4mH32dLcN5lm804aBCozd/view',
-    src: 'https://s3-alpha-sig.figma.com/img/ebbf/e20b/737934d80aa70ba4db2d8a0667135dda?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UgsXIcTI3JAH-9aWRFbDbY0fNDEOKI4W7oD6KxmaTBNTm4BKjei9ePwnQbXiFVd32-MABrE4gVCuFfk1YS8snC2YYsyeYc3j9aHU8BjUbf7uRRUTA4m30DTEc7A~2RyWl-R~OXQ2QF7fg49kYBbCzzysA966aSoZJ~wwELC7MMcQJ2qPEvkDPVUtZtbzIgnEXrNIHu-vWIF5ZmkPUDhKKrEs6ZQsg~QEqMgKs8LlaUxpIQnvMRgaTnveLwzE4F3IBF~keRVS-6dd-SX~l7-S45s6N5UgJLryhzPfFMnZCFSU9pti1nVMW~krLCNNXUSKNni0RU2WsX0vqyP0zzI9Vg__',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/community.jpeg',
   },
-  splash: {
+  homeSplash: {
     drive:
       'https://drive.google.com/file/d/1wQR-vC-UXMqiJaBsskk6uA4Ehcpast87/view',
-    src: 'https://s3-alpha-sig.figma.com/img/bae1/524f/d1a3acc270ae541e29094cc729945808?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YZE0OS-izjxqoA2Jp1Lu9RlMncY7apaTh81o-Y7cRuEWMr6zp8rWLefIQnS96wcXRGbiPwQ0kyG75vkTqEaN4~F6bZNmxgXrQzeBrmiEql9zXS0M9MKCY1bMh9AXumHtn~DdhPrKmyVGfRPfcHaAOGLSqcWe~Hvsp4tnMADjEwAaUFWsZZY-Bl3cQIgmzAVsVgsXJBITY02qaRfN~viUUuXFMMfzYjWZyAvVbZsclQTdji~6cpj3OluT8P1uw22aVzv2ODutRV1UzryGO24FJj2vYFHQg7xOHgIN0j1sMjnog5bgax-kf88cCBZimeuclGr-8TQFOuNf6wYFby0y4A__',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/patina%20home.png',
+  },
+  scholarSplash: {
+    drive:
+      'https://drive.google.com/file/d/1gVKHEX9NJExD83VqCsNo4IosDDF1h5r-/view',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/scholarship%20home.png',
   },
   learn: {
     drive:
       'https://drive.google.com/file/d/1m_rLYBHiNmBc1BQPIZcGRF7py6Gkr1t9/view',
-    src: 'https://s3-alpha-sig.figma.com/img/b891/d8a1/8da960803d5d6746feafff1b9ffec5ed?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qLYUQuGgXpYIl-b3Gc7rJ6LzkDyoQNcbvLhByiku-SDhEp92c-tO-KTOqGQOS1iVdSe97tz-mRa6AX9aIuY8lCOLGOMJRhg8rcAQbRJWr6Zck3p1M3OwIH64k2kKt8eUsRn7JKLHBxkx1M5Ksk8Z6tbHrOFa0XabF1DeaOEv7uie~eFOZB1o3RW4Y2X0FMSlQaRfUSWMnQVOqEc7f5OCh7zKJ8GTX4uIhd1nt6wzfeuEoI8fS8ZiZE-VGtp5DTZIAQ2TBfC7NV~npxMjI2NGd0hyHXe6ydtBkj72Md5RCJT6pay7Ppu5B-oEcqVZJDo8Rz~0eF~lI1nQKe1cYbWP~A__',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/Learn.png',
   },
   hands: {
     drive:
       'https://drive.google.com/file/d/1oS2Un2VuVm_xwG6nG_fA2wg9eGCJw-jn/view',
-    src: 'https://s3-alpha-sig.figma.com/img/fc29/f89f/d4247a60dc8a58fad8ba84e142dcf4bb?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=m2XFVhp0IDJDdL0P~GiLNX36JS5tLSvdRC9oKmmwNDui4DqAoIDQ02oqNSI-AqxXsvFhfyY0wu7nXvV7ZUs6vSPsPnmPmTb3FCORmU-2S0WloSu17VB7DUkd2hNGy9rMJEu7pCDE6QmoM-2F4~NS5Z~3Pa42wpAxiFVvQP2Fx3Wol9PVg-HASsrFc50dqvo5A4aPonYakEU~GoWApxq87pHw868UVKsrRZuNJIwh-U9YDjBCpTCTOBY4YEPqpTb~YddriRcRavTXmLu6Ss6nZzPA57hp4OFRu4Lkg2aiXD-VVXsdwd8bHUZlwqYTcqhkoYBjAVEBQwhRPjxgsghHAw__',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/Hands.png',
   },
   heart: {
     drive:
       'https://drive.google.com/file/d/1htDYhOf4XefDcHicVvctZGGuIPx9gutB/view',
-    src: 'https://s3-alpha-sig.figma.com/img/b9a6/81f3/db51fc60ff547aab2368cbb1589d86fe?Expires=1722211200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IRcjAkKvXjy~nHhNJ4sbJSpGPzWYZnK9FOnCfTcf0gckRm9602xVrN0KgCy88Zvay8bbHLywsmWj6oMFIyfdAaUsHQn~qQhbBQMPj4wYM0kCOYsvV9BroiU0W3apZCSryYvs-CPEGd6DrGL9jqzVYIKEI1zjbV3ZbpgWOiJpMBSjS3SBV7o2QG4tBFNuSasYpnzW4ZV73L~jihvx9vGN78hFeExqRpt1qvKTZhEljnLiYVp8ytF4V8MW-y40NetBbGbGLsU1XM5p87aiqdZFvOTd-csXW3KrKxwiN2up~Q1ExoBHtqtKGwRXjc6a-f73t79ty3TUtBYKhvF102FdPw__',
-  },
-  scholarHero: {
-    drive:
-      'https://drive.google.com/file/d/1gVKHEX9NJExD83VqCsNo4IosDDF1h5r-/view?usp=sharing',
-    src: 'https://cdn.discordapp.com/attachments/1261068702171009085/1262480237615124571/Group_297559.png?ex=6696bfd8&is=66956e58&hm=2506faf9caa9b3d56c06b7f228d89b3c1fe6137c605e1732b3f2bf80ae694531&',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/Heart.png',
   },
 }
