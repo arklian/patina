de0e85a331f63ea61a463876ec9ddaf71bc83397
Martin M <m2msquared47@gmail.com>
2024-07-22 15:28:22 -0700

    Martin: Add Project Page Content
    Started a basic layout for presenting my projects.
    
    TEST=manual
    Resolved praf conflicts. prt ran with no errors
    
    Change-Id: Ibf40b0befdc78547ff88e33b36c212413eac69f5

--- js/src/personal/martin/project/Projects.page.tsx
+++ js/src/personal/martin/project/Projects.page.tsx
@@ -1,15 +1,52 @@
+import { Card, Text, SimpleGrid } from '@mantine/core'
 import MartinFront from '@/personal/martin/assets/MartinFront.jpg'
 import { Jumbotron } from '@/personal/martin/components/Jumbotron.tsx'
 
+const projectData = [
+  {
+    title: 'Patina Network',
+    desc: 'Description',
+  },
+  {
+    title: 'Voxel Game Engine',
+    desc:
+      'Using c++ and OpenGl to make a game engine library for voxel based games. Voxels allow' +
+      ' a player to have the freedom to manipulate and change their environment and have a more' +
+      ' interactive experience.',
+  },
+  {
+    title: '2D Cloth Physics Simulation',
+    desc: 'Description',
+  },
+  {
+    title: 'JS Game Engine API',
+    desc: 'Description',
+  },
+]
+
 export function MartinProject() {
+  const projects = projectData.map((project) => (
+    <Card key={project.title} shadow="md" radius="md" padding="xl">
+      <Text fz="lg" fw={500} mt="md">
+        {project.title}
+      </Text>
+      <Text fz="sm" c="dimmed" mt="sm">
+        {project.desc}
+      </Text>
+    </Card>
+  ))
   return (
     <>
       <Jumbotron
         title="Projects"
-        text={'Short Paragraph into to page. '}
+        text={'Showcase of my best and favorite projects.'}
         imgSrc={MartinFront}
         imgSize="35%"
       />
+
+      <SimpleGrid cols={{ base: 1, md: 2 }} spacing="xl" mt={50}>
+        {projects}
+      </SimpleGrid>
     </>
   )
 }
