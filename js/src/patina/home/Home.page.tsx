822b6939e6f6444b9f02c4b675e7eb1501daaea2
Kelly Lin <linkelly150@gmail.com>
2024-07-22 15:35:05 -0400

    Home: Fixed home page hero section
    Image is now correctly behind text and is
    responsive in smaller window sizes.
    
    L=ENG-189
    
    Change-Id: I9764b11e572b0f8b27274b4aa68bc426e1ce7e3b

--- js/src/patina/home/Home.page.tsx
+++ js/src/patina/home/Home.page.tsx
@@ -1,10 +1,10 @@
-import { Container, Skeleton, Title } from '@mantine/core'
+import { BackgroundImage, Container, Skeleton, Title } from '@mantine/core'
 import { Initiatives } from './Initiatives'
 import { About } from './About.tsx'
 import { ImportantSection } from '@/patina/home/ImportantSection.tsx'
 import styles from './Home.module.css'
 import { GetInvolved } from '@/patina/home/GetInvolved.tsx'
-/*import { imageUrls } from '@/patina/assets/images.ts'*/
+import { imageUrls } from '@/patina/assets/images.ts'
 
 export function HomePage() {
   return (
@@ -12,21 +12,22 @@ export function HomePage() {
       <div className={styles.skeletonStyle}>
         <Skeleton animate={false} height={70} width="100%" mb="xl" />
       </div>
-      <img
-        className={styles.hero_image}
-        alt={'stock for home page'}
-        src={
-          'https://cdn.discordapp.com/attachments/1261068702171009085/1263557244205797407/Untitled114_20240718140508.png?ex=669f4822&is=669df6a2&hm=739bb58889c5bd0061af65ae1b7e0c62f8d482e7c970364a5725d8b21ccd31d3&'
-        }
-      />
-      <div className={styles.hero}>
-        <div>
-          <Title className={styles.hero_title}>
-            {'Empowering AANHPI leaders to '}
-            <span className={styles.title_teal}>{'make their mark.'}</span>
-          </Title>
+      <BackgroundImage
+        bgp={'right'}
+        bgsz={'750px'}
+        bgr={'no-repeat'}
+        miw={'600px'}
+        src={imageUrls.homeSplash.src}
+      >
+        <div className={styles.hero}>
+          <div>
+            <Title className={styles.hero_title} w={'75%'}>
+              {'Empowering AANHPI leaders to '}
+              <span className={styles.title_teal}>{'make their mark.'}</span>
+            </Title>
+          </div>
         </div>
-      </div>
+      </BackgroundImage>
       <div className={styles.skeletonStyle}>
         <Skeleton animate={false} height={50} width="40%" mb="xl" />
       </div>
