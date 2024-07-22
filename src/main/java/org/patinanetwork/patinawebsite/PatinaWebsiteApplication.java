ab2b845862d4e31da119db076c026734f7a82df6
Henry Chen <hjc77@cornell.edu>
2024-07-22 12:36:59 -0400

    Java: Autoformat
    Change-Id: Id13c357a3469b7f36e50a8a7f78d0905dd1602d3

--- src/main/java/org/patinanetwork/patinawebsite/PatinaWebsiteApplication.java
+++ src/main/java/org/patinanetwork/patinawebsite/PatinaWebsiteApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PatinaWebsiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatinaWebsiteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PatinaWebsiteApplication.class, args);
+    }
 }
