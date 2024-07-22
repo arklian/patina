ebc845016dafd0cee2584b9ec3c359b2ccb6d1a5
Riyuan Liu <riyusnliu@gmail.com>
2024-07-22 16:16:08 +0000

    PatNet: Create NavBar Component's Skeleton
    Create the basic NavBar Skeleton
    
    L=ENG-177
    TEST=manual
    -Page loads locally
    
    Change-Id: I402999a26b974b05b1a217d29b533cc2a901e815

--- js/src/patina/Patina.page.tsx
+++ js/src/patina/Patina.page.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from '@mantine/core'
 import { Outlet } from 'react-router-dom'
 import { theme, resolver } from './theme.ts'
+import { NavBar } from './navbar/NavBar.tsx'
 
 export function PatinaPage() {
   return (
@@ -9,6 +10,7 @@ export function PatinaPage() {
       cssVariablesResolver={resolver}
       forceColorScheme="dark"
     >
+      <NavBar />
       <Outlet />
     </MantineProvider>
   )
---
+++ js/src/patina/navbar/NavBar.tsx
@@ -0,0 +1,118 @@
+import { Menu, Group, Center, Burger, Container } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { IconChevronDown } from '@tabler/icons-react'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classes from './NavBar.module.css'
+
+const links = [
+  {
+    link: '/about',
+    label: 'About',
+    links: [
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+    ],
+  },
+  {
+    link: '/Program',
+    label: 'Program',
+    links: [
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+    ],
+  },
+  {
+    link: '/GetInvolved',
+    label: 'Get Involved',
+    links: [
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+    ],
+  },
+  {
+    link: '/StayInformed',
+    label: 'Stay Informed',
+    links: [
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+      { link: '/stuff', label: 'stuff' },
+    ],
+  },
+  { link: '/pricing', label: 'Blog' },
+  { link: '/pricing', label: 'Donate' },
+]
+
+//NavBar component renders the navigation bar with links and dropdown menus.
+export function NavBar() {
+  const [opened, { toggle }] = useDisclosure(false)
+
+  /**
+   * Generates menu items and links for the navigation bar.
+   * If a link has nested links, a dropdown menu is created.
+   */
+  const items = links.map((link) => {
+    const menuItems = link.links?.map((item) => (
+      <Menu.Item key={item.link}>{item.label}</Menu.Item>
+    ))
+
+    if (menuItems) {
+      return (
+        <Menu
+          key={link.label}
+          trigger="hover"
+          transitionProps={{ exitDuration: 0 }}
+          withinPortal
+        >
+          <Menu.Target>
+            <a
+              href={link.link}
+              className={classes.link}
+              onClick={(event) => event.preventDefault()}
+            >
+              <Center>
+                <span className={classes.linkLabel}>{link.label}</span>
+                <IconChevronDown size="0.9rem" stroke={1.5} />
+              </Center>
+            </a>
+          </Menu.Target>
+          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+        </Menu>
+      )
+    }
+
+    return (
+      <a
+        key={link.label}
+        href={link.link}
+        className={classes.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </a>
+    )
+  })
+
+  return (
+    <header className={classes.header}>
+      <Container size="75rem">
+        {' '}
+        {/* 75rem == 1200px */}
+        <div className={classes.inner}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <Group gap={5} visibleFrom="sm">
+            {items}
+          </Group>
+          <Burger opened={opened} onClick={toggle} size="sm" hiddenFrom="sm" />
+        </div>
+      </Container>
+    </header>
+  )
+}
