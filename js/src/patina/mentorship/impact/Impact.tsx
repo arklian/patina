e3c773ac544e691ddd96a43df17174bc39d92f91
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-22 16:48:43 -0400

    Mentor: Add Impact section
    -this adds a carousel that acts as the impact section
    
    Change-Id: I02ec3a2aaf415a9855e4df93b7bae28a3f9c3de7

--- js/src/patina/mentorship/impact/Impact.tsx
+++ js/src/patina/mentorship/impact/Impact.tsx
@@ -1,6 +1,26 @@
-import { Skeleton } from '@mantine/core'
+import { Carousel } from '@mantine/carousel'
+import styles from './impact.module.css'
 
-// Defining the Impact component
 export function Impact() {
-  return <Skeleton animate={false} height={70} width="100%" mb="xl" />
+  return (
+    <div className={styles.carouselContainer}>
+      <Carousel height={200} slideGap="md" controlsOffset="xs">
+        <Carousel.Slide>
+          <div className={styles.slide}>
+            <div className={styles.imagePlaceholder}></div>
+            <div className={styles.textContainer}>
+              <h3>{'Cinthia and Jasmine'}</h3>
+              <p>
+                {
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                }
+              </p>
+              <p className={styles.quote}>{'“Quote from student”'}</p>
+            </div>
+          </div>
+        </Carousel.Slide>
+        {/* Add more slides as needed */}
+      </Carousel>
+    </div>
+  )
 }
