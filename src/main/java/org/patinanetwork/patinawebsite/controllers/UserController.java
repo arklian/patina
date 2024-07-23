3888dd2f966894241e6bca9b2dc6811a98273264
Bryan Zhang <bz5989@princeton.edu>
2024-07-23 16:15:58 -0400

    PostgreSQL: Added controller
    Added user controller (still missing html files though)
    
    Change-Id: I33dcaab6bdfda44474ce2103512b082dfb3c3145

--- src/main/java/org/patinanetwork/patinawebsite/controllers/UserController.java
+++ src/main/java/org/patinanetwork/patinawebsite/controllers/UserController.java
@@ -1,13 +1,17 @@
 package org.patinanetwork.patinawebsite.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.patinanetwork.patinawebsite.models.User;
 import org.patinanetwork.patinawebsite.models.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UserController {
@@ -22,4 +26,16 @@ public class UserController {
         model.addAttribute("us", users);
         return "users/showAll";
     }
+
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam Map<String, String> newuser, HttpServletResponse response) {
+        System.out.println("ADD user");
+        String newName = newuser.get("name");
+        String newPassword = newuser.get("password");
+        int newDonation = Integer.parseInt(newuser.get("donation"));
+        userRepo.save(new User(newName, newPassword, newDonation));
+        response.setStatus(201);
+        return "users/addedUser";
+    }
+
 }
