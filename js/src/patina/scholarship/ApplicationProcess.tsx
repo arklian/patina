fb1ad9631f9e333054d906d66002dd6105899a1a
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-23 17:34:08 -0400

    Scholarship: Fixes Apply and About section
    Adjusted colors in theme.ts to match the figma.
    Adjusted stack component to be more responsive.
    Moved Gain files to inside scholarship.
    Added proper image to images.ts.
    
    L=ENG-291
    Test=Manual
    -page load locally
    
    Change-Id: If5b1b8f1780d09f0637da85ff3ad58108ed4f835

--- js/src/patina/scholarship/ApplicationProcess.tsx
+++ js/src/patina/scholarship/ApplicationProcess.tsx
@@ -1,17 +1,16 @@
-import { Text } from '@mantine/core'
 import { Stacks } from '../components/Stacks'
 import styles from './ApplicationProcess.module.css'
 
 const processStack = [
-  { title: 'OCT 10', description: 'Applications Open' },
-  { title: 'OCT 10', description: 'Application Deadline' },
-  { title: 'OCT 10', description: 'Results Announced' },
+  { title: 'SEP 1', description: 'Applications Open' },
+  { title: 'OCT 1', description: 'Applications Close' },
+  { title: 'NOV 1', description: 'First Round Selections' },
+  { title: 'DEC TBD', description: 'Finalist Announcement' },
 ]
 /** Displays information about the application deadlines */
 export function ApplicationProcess() {
   return (
     <div className={styles.container}>
-      <Text className={styles.title}>{'Application Process'}</Text>
       <Stacks items={processStack} />
     </div>
   )
