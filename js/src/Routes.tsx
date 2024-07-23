bac736b0219064d7c58f921873c1f24d88cfd61d
Riyuan Liu <riyusnliu@gmail.com>
2024-07-23 13:09:28 -0400

    Lilac: Common Folder structure
    Set up the base Folder structure, Theme file, and routes for Lilac
    
    L=ENG-285
    L=ENG-286
    Test=manual
    -Page loads locally
    
    Change-Id: Ifb983ca0fa9e2bd179e9202767fff513f4fe39ff

--- js/src/Routes.tsx
+++ js/src/Routes.tsx
@@ -1,6 +1,8 @@
 import { PatinaRoutes } from '@/patina/Patina.routes.tsx'
 import { PersonalRoutes } from './personal/Personal.routes.tsx'
 import { PatinaPage } from '@/patina/Patina.page.tsx'
+import { LilacPage } from '@/lilac/Lilac.page.tsx'
+import { LilacRoutes } from '@/lilac/Lilac.routes.tsx'
 
 export const Routes = [
   // Base path for hosting the patina website
@@ -9,6 +11,12 @@ export const Routes = [
     description: 'Home',
     element: <PatinaPage />,
     children: PatinaRoutes,
+  },
+  {
+    path: '/lilac',
+    description: 'lilac',
+    element: <LilacPage />,
+    children: LilacRoutes,
   },
   // Secondary path for hosting our personal websites
   {
