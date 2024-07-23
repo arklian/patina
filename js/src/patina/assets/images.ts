3b6b01608e7f5122bd28fe4dc43b7fa07ed8e631
Riyuan Liu <riyusnliu@gmail.com>
2024-07-23 13:25:41 -0400

    Issues: Fixed Image src in images.ts
    Changed all of the upper case I's in webImg to lower case.
    
    TEST=manual
    -Page loads locally
    
    Change-Id: I1e928317b1ca32a23babaa1e6bad8e972b34b6ca

--- js/src/patina/assets/images.ts
+++ js/src/patina/assets/images.ts
@@ -11,41 +11,41 @@ export const imageUrls = {
   mentorship: {
     drive:
       'https://drive.google.com/file/d/1jGA8KWh0NVbS1s-zIDzTVRY27A_Y8o86/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/mentorship.jpeg',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/mentorship.jpeg',
   },
   scholarship: {
     drive:
       'https://drive.google.com/file/d/1aDgXnl75WwSaoNFwzPiO5fVvZ78sBHPZ/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/scholarship.jpeg',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/scholarship.jpeg',
   },
   community: {
     drive:
       'https://drive.google.com/file/d/16fqE7xIAEA_4mH32dLcN5lm804aBCozd/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/community.jpeg',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/community.jpeg',
   },
   homeSplash: {
     drive:
       'https://drive.google.com/file/d/1wQR-vC-UXMqiJaBsskk6uA4Ehcpast87/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/patina%20home.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/patina%20home.png',
   },
   scholarSplash: {
     drive:
       'https://drive.google.com/file/d/1gVKHEX9NJExD83VqCsNo4IosDDF1h5r-/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/scholarship%20home.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/scholarship%20home.png',
   },
   learn: {
     drive:
       'https://drive.google.com/file/d/1m_rLYBHiNmBc1BQPIZcGRF7py6Gkr1t9/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/Learn.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/Learn.png',
   },
   hands: {
     drive:
       'https://drive.google.com/file/d/1oS2Un2VuVm_xwG6nG_fA2wg9eGCJw-jn/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/Hands.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/Hands.png',
   },
   heart: {
     drive:
       'https://drive.google.com/file/d/1htDYhOf4XefDcHicVvctZGGuIPx9gutB/view',
-    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webImg/Heart.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/Heart.png',
   },
 }
