508dfb0f28b53af1970574ce5bebbf709c678fc3
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-25 16:20:48 -0400

    Scholarship: Makes stack component more responsive
    Puts scholarship files into folders.
    
    L=ENG-316
    Test=Manual
    -page load locally
    
    Change-Id: Ib6189e3df02ab514cb67c0937c6049e260507f4e

--- js/src/patina/scholarship/applicationprocess/ApplicationProcess.tsx
+++ js/src/patina/scholarship/applicationprocess/ApplicationProcess.tsx
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:js/src/patina/scholarship/process/ApplicationProcess.tsx
 import { Stacks } from '@/patina/components/Stacks'
-========
-import { Stacks } from '../../components/Stacks'
->>>>>>>> 3ac63a9 (Scholarship: Makes stack component more responsive):js/src/patina/scholarship/applicationprocess/ApplicationProcess.tsx
 import styles from './ApplicationProcess.module.css'
 
 const processStack = [
