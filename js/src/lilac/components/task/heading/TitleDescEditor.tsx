6068f3c3ca955863f043a4b589149ca1f42126f6
Haoking Luo <hao5k6032@gmail.com>
2024-07-25 17:41:49 -0400

    Lilac: Add RichTextEditor and TextInput
    Added a Mantine RichTextEditor component to TitleDescEditor component
    for specifying the description. Added a regular Mantine TextInput
    component for specifying the title. Added a CSS file for the component.
    The RichTextEditor for the description of the issue allows  bold,
    italic, strikethrough, link, quote, code controls.
    Refactored Task directory name to lowercase task.
    
    L=ENG-294
    TEST=manual
    - Ran pnpm run dev locally
    
    Change-Id: I38dec7bd94016079f7c2b1dc0b1113f20c971778

---
+++ js/src/lilac/components/task/heading/TitleDescEditor.tsx
@@ -0,0 +1,64 @@
+import { TextInput } from '@mantine/core'
+import { RichTextEditor, Link } from '@mantine/tiptap'
+import { useEditor, BubbleMenu } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Placeholder from '@tiptap/extension-placeholder'
+import {
+  IconBold,
+  IconItalic,
+  IconStrikethrough,
+  IconLink,
+  IconCode,
+  IconQuote,
+} from '@tabler/icons-react'
+import '@mantine/tiptap/styles.css'
+import styles from './TitleDescEditor.module.css'
+
+const BoldIcon = () => <IconBold size="1rem" stroke={3} />
+const ItalicIcon = () => <IconItalic size="1rem" stroke={3} />
+const StrikeIcon = () => <IconStrikethrough size="1rem" stroke={3} />
+const LinkIcon = () => <IconLink size="1rem" stroke={3} />
+const CodeIcon = () => <IconCode size="1rem" stroke={3} />
+const QuoteIcon = () => <IconQuote size="1rem" stroke={3} />
+
+/**
+ * Component containing a TextInput for specifying issue title and RichTextEditor for description.
+ */
+export function TitleDescEditor() {
+  const descEditor = useEditor({
+    extensions: [
+      StarterKit,
+      Link,
+      Placeholder.configure({ placeholder: 'Add description...' }),
+    ],
+    content: '',
+  })
+
+  return (
+    <div className={styles.headingEditorDiv}>
+      <TextInput
+        classNames={{ input: styles.titleEditorField }}
+        variant={'unstyled'}
+        placeholder={'Issue title'}
+      />
+      <RichTextEditor
+        classNames={{ root: styles.contentEditorField }}
+        editor={descEditor}
+      >
+        {descEditor && (
+          <BubbleMenu editor={descEditor}>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Bold icon={BoldIcon} />
+              <RichTextEditor.Italic icon={ItalicIcon} />
+              <RichTextEditor.Strikethrough icon={StrikeIcon} />
+              <RichTextEditor.Link icon={LinkIcon} />
+              <RichTextEditor.Blockquote icon={QuoteIcon} />
+              <RichTextEditor.CodeBlock icon={CodeIcon} />
+            </RichTextEditor.ControlsGroup>
+          </BubbleMenu>
+        )}
+        <RichTextEditor.Content />
+      </RichTextEditor>
+    </div>
+  )
+}
