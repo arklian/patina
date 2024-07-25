28498d229cdf706ccea5e46ad55de7d1b6c62a2e
Haoking Luo <hao5k6032@gmail.com>
2024-07-25 16:37:28 -0400

    Lilac: Add Mantine RichTextEditors for title/desc
    Added two Mantine RichTextEditor components to TitleDescEditor component
    for Lilac. Added a CSS file for the component. The first text editor
    is for the title of the issue and is locked to a bold font with no
    controls like Linear. The second text editor is for the description of
    the issue and allows bold, italic, strikethrough, link, quote, code
    controls. Refactored Task directory name to lowercase task.
    
    L=ENG-294
    TEST=manual
    - Ran pnpm run dev locally
    
    Change-Id: I38dec7bd94016079f7c2b1dc0b1113f20c971778

---
+++ js/src/lilac/components/task/heading/TitleDescEditor.tsx
@@ -0,0 +1,74 @@
+import { RichTextEditor, Link } from '@mantine/tiptap'
+import { useEditor, BubbleMenu } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Placeholder from '@tiptap/extension-placeholder'
+import {
+  IconBold,
+  IconItalic,
+  IconStrikethrough,
+  IconLink,
+  IconCode,
+  IconQuote,
+} from '@tabler/icons-react'
+import '@mantine/tiptap/styles.css'
+import styles from './Heading.module.css'
+
+const BoldIcon = () => <IconBold size="1rem" stroke={3} />
+const ItalicIcon = () => <IconItalic size="1rem" stroke={3} />
+const StrikeIcon = () => <IconStrikethrough size="1rem" stroke={3} />
+const LinkIcon = () => <IconLink size="1rem" stroke={3} />
+const CodeIcon = () => <IconCode size="1rem" stroke={3} />
+const QuoteIcon = () => <IconQuote size="1rem" stroke={3} />
+
+/**
+ * Component containing two text editors for specifying the issue title and its description.
+ */
+export function TitleDescEditor() {
+  const titleEditor = useEditor({
+    extensions: [
+      StarterKit,
+      Placeholder.configure({ placeholder: 'Issue title' }),
+    ],
+    content: '',
+  })
+
+  const descEditor = useEditor({
+    extensions: [
+      StarterKit,
+      Link,
+      Placeholder.configure({ placeholder: 'Add description...' }),
+    ],
+    content: '',
+  })
+
+  return (
+    <div className={styles.headingEditorDiv}>
+      <RichTextEditor
+        classNames={{
+          root: styles.titleEditorField,
+        }}
+        editor={titleEditor}
+      >
+        <RichTextEditor.Content />
+      </RichTextEditor>
+      <RichTextEditor
+        classNames={{ root: styles.contentEditorField }}
+        editor={descEditor}
+      >
+        {descEditor && (
+          <BubbleMenu editor={descEditor}>
+            <RichTextEditor.ControlsGroup>
+              <RichTextEditor.Bold icon={BoldIcon} />
+              <RichTextEditor.Italic icon={ItalicIcon} />
+              <RichTextEditor.Strikethrough icon={StrikeIcon} />
+              <RichTextEditor.Link icon={LinkIcon} />
+              <RichTextEditor.Blockquote icon={QuoteIcon} />
+              <RichTextEditor.CodeBlock icon={CodeIcon} />
+            </RichTextEditor.ControlsGroup>
+          </BubbleMenu>
+        )}
+        <RichTextEditor.Content />
+      </RichTextEditor>
+    </div>
+  )
+}
