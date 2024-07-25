12ccbc760339be984052ef177c414200685d83e3
Tony Wang <wangtony4005@gmail.com>
2024-07-25 19:32:20 +0000

    Secrets: Added dummy secrets.sh
    Dummy secrets.sh for the public patina repository.
    
    TEST=manual
    L=ENG-274
    
    Change-Id: I73805466daf5e956f8fc8019980fd6266b30d431

---
+++ secrets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Access Digital Ocean Spatial Storage Secret Key - Manny
+export DIGITAL_OCEAN_SPACES_SECRET=redacted
+# Access Digital Ocean Spatial Storage Access Key - Manny
+export DIGITAL_OCEAN_SPACES_ACCESS=redacted
+# Host Code for Database - Bryan
+export DATABASE_URL=redacted
+# Host Username for Database - Bryan
+export DATABASE_USER=redacted
+# Host Password for Database - Bryan (Most important one!)
+export DATABASE_PASSWORD=redacted
+
