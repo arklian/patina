b744ec09ac3ed5da04f7474f4805cba13090c151
Riyuan Liu <riyusnliu@gmail.com>
2024-07-25 13:31:50 -0400

    PatNet: Mentorship Gain section
    Finished Mentorship Gain Section
    
    L=ENG-232
    Test=Manual
    -Page load locally
    
    Change-Id: If530c192d53fa1e401e7c733ece9295b5ecc8ace

--- js/src/patina/mentorship/gain/Gain.tsx
+++ js/src/patina/mentorship/gain/Gain.tsx
@@ -1,6 +1,23 @@
-import { Skeleton } from '@mantine/core'
+import { SimpleGrid, Title } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
+import { MentorshipCards } from '@/patina/mentorship/gain/MentorshipCards.tsx'
+import styles from './Gain.module.css'
 
-// Defining the Gain component
+/**
+ * Gain component for the Patina Network Mentorship program.
+ * Displays a title, and 3 Cards Component displaying what you will gain after
+ * this program
+ */
 export function Gain() {
-  return <Skeleton animate={false} height={70} width="100%" mb="xl" />
+  const largeScreen = useMediaQuery('(min-width: 40em)')
+  return (
+    <div>
+      <Title className={styles.title}>{'What Will I Gain?'}</Title>
+      <SimpleGrid cols={largeScreen ? 3 : 1}>
+        <MentorshipCards />
+        <MentorshipCards />
+        <MentorshipCards />
+      </SimpleGrid>
+    </div>
+  )
 }
