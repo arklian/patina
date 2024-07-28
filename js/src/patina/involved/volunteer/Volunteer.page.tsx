0b1c8e9f5d1491b0bdf39e84332698a3f21c76fa
KaziMansha <mmansha93@gmail.com>
2024-07-28 13:12:31 -0400

    Get Involved Pages: Complete Email Section
    Completed Email section for Get Involved Page
    
    Change-Id: Ic650f6540396ff2aea555c6cc762f7b8befeec89

--- js/src/patina/involved/volunteer/Volunteer.page.tsx
+++ js/src/patina/involved/volunteer/Volunteer.page.tsx
@@ -1,11 +1,11 @@
-import { Title, Text, Image } from '@mantine/core'
+import { Title, Text, Image, Button } from '@mantine/core'
 import { imageUrls } from '../../assets/images.ts'
 import { VolunteerCard } from './VolunteerCard'
 import styles from './Volunteer.module.css'
 
 export function VolunteerPage() {
   return (
-    <div>
+    <div className={styles.container}>
       <section className={styles.wrapper}>
         <div className={styles.text}>
           <Title order={2} className={styles.title}>
@@ -43,6 +43,26 @@ export function VolunteerPage() {
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
           type="Remote/In-person"
         />
+      </section>
+      <section className={styles.emailSection}>
+        <div className={styles.backgroundBlur}> </div>
+        <div className={styles.content}>
+          <Text size="md" className={styles.emailText}>
+            {
+              "Want to get involved but don't necessarily align with these opportunities? Sign up to be on the volunteer list for future events! Please fill out this form, connect with us on Instagram and LinkedIn or email us at "
+            }
+            <a
+              href={'mailto:hi@patinanetwork.org'}
+              className={styles.emailEmail}
+            >
+              {'hi@patinanetwork.org'}
+            </a>
+            {' to join our volunteer email list'}
+          </Text>
+          <Button size="sm" className={styles.emailButton}>
+            {'Sign Up'}
+          </Button>
+        </div>
       </section>
     </div>
   )
