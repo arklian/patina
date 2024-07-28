ce4ca3a5d8293e9ac9d9619d17c551ef51bafc21
Henry Chen <hjc77@cornell.edu>
2024-07-28 18:38:39 -0400

    Protos: Add proto support in Java
    IntelliJ properly finds protobufs and they can be used to hold data
    instead of using plain Java objects.
    
    L=ENG-319
    TEST=manual
    Get endpoint for events still works
    
    Change-Id: I48973e41bb3c8fbc8d34cb6315b4800070165c73

--- db/migrations/patina/V0000__edit_event_table.sql
+++ db/migrations/patina/V0000__edit_event_table.sql
@@ -1,9 +1,10 @@
+-- TODO(Henry): Figure out better column names.
 CREATE TABLE event
 (
-    id           SERIAL PRIMARY KEY,
-    name         VARCHAR(255) NOT NULL,
-    message      TEXT NOT NULL,
-    location     VARCHAR(255) NOT NULL,
-    date         DATE NOT NULL,
-    deleted      BOOLEAN NOT NULL DEFAULT FALSE
+    id       SERIAL PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    message  TEXT         NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    date     DATE         NOT NULL,
+    deleted  BOOLEAN      NOT NULL DEFAULT FALSE
 );
