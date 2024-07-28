834fa6db13e8c74d86696937697b408f359fc15c
KaziMansha <mmansha93@gmail.com>
2024-07-28 13:16:18 -0400

    Kazi: Created layout for hero section
    Created layout for hero section of volunteering opportunities
    
    L=ENG-219
    TEST=manual
    
    Change-Id: I048baf8c2a8740dbae14b4b2276d34eebfb949c3

---
+++ js/src/patina/involved/volunteer/Volunteer.page.tsx
@@ -0,0 +1,23 @@
+import { Title, Text, Image } from '@mantine/core'
+import { imageUrls } from '../../assets/images.ts'
+import styles from './Volunteer.module.css'
+
+export function Volunteer() {
+  return (
+    <div>
+      <section className={styles.wrapper}>
+        <div className={styles.text}>
+          <Title order={2} className={styles.title}>
+            {'Patina Network'} <br /> {'Apply to be a Volunteer'}
+          </Title>
+          <Text size="md" className={styles.description}>
+            {
+              'The Patina Network volunteer program is... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+            }
+          </Text>
+        </div>
+        <Image className={styles.volunteerImg} src={imageUrls.mentorship.src} />
+      </section>
+    </div>
+  )
+}
