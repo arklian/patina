088c7f4d5e35a1c0ab756ae1a8ee9650fcff2a33
Martin M <m2msquared47@gmail.com>
2024-07-28 12:24:55 -0400

    Martin: Add Contact Page Content
    (Last commit before switching devices.)
    Added contact info (fake phone #) and a non-functional message area for
    looks.
    
    Change-Id: I324157c869c3d6aaa4bf8ec7db87fc5d6bd07cae

--- js/src/personal/martin/contact/Contact.page.tsx
+++ js/src/personal/martin/contact/Contact.page.tsx
@@ -1,15 +1,71 @@
-import MartinFront from '@/personal/martin/assets/MartinFront.jpg'
-import { Jumbotron } from '@/personal/martin/components/Jumbotron.tsx'
+import {
+  Button,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+  Textarea,
+  TextInput,
+  Title,
+} from '@mantine/core'
+import classes from '@/personal/martin/Martin.module.css'
+
+interface contactProps {
+  title: string
+  desc: string
+}
+const contactData = [
+  { title: 'Email', desc: 'm2msquared47@gmail.com' },
+  { title: 'Phone', desc: '(123)-456-6789' },
+  { title: 'Location', desc: 'Seattle, WA' },
+  { title: 'Common Hours', desc: 'Weekdays 10am-8pm PST' },
+]
+
+function ContactLine({ title, desc }: contactProps) {
+  return (
+    <div>
+      <Title order={3}>{title}</Title>
+      <Text>{desc}</Text>
+    </div>
+  )
+}
 
 export function MartinContact() {
+  const details = contactData.map((item) => (
+    <ContactLine title={item.title} desc={item.desc} />
+  ))
   return (
     <>
-      <Jumbotron
-        title="Contact"
-        text={'Short Paragraph into to page. '}
-        imgSrc={MartinFront}
-        imgSize="35%"
-      />
+      <div className={classes.contactWrapper}>
+        <SimpleGrid cols={{ base: 1, sm: 2 }} spacing={50}>
+          <div>
+            <Title>{'Contact'}</Title>
+            <Text>
+              {'You can get a hold of me using my contact info here.'}
+            </Text>
+            <Stack mt="lg">{details}</Stack>
+          </div>
+          <div>
+            <TextInput
+              required
+              label="Email"
+              placeholder="jsmith@gmail.com"
+              mt="sm"
+            />
+            <TextInput required label="Name" placeholder="John Smith" mt="sm" />
+            <Textarea
+              required
+              label="Message"
+              placeholder="Type your message here..."
+              minRows={4}
+              mt="sm"
+            />
+            <Group justify="flex-end" mt="sm">
+              <Button data-disabled>{'Send'}</Button>
+            </Group>
+          </div>
+        </SimpleGrid>
+      </div>
     </>
   )
 }
