4eca20bcb73a04fd454b558f10bb403f9965e819
Riyuan Liu <riyusnliu@gmail.com>
2024-07-29 16:15:02 -0400

    Lilac: Sub-issues under Task
    Created the basic layout for task editor
    L=ENG-295
    Test=Manual
    -page loaded locally
    
    Change-Id: I300ce0a502e3af8faaec9de65e31d11c6243336f

--- js/src/lilac/components/task/activity/Activity.tsx
+++ js/src/lilac/components/task/activity/Activity.tsx
@@ -1,5 +1,31 @@
-import { Skeleton } from '@mantine/core'
+import { Badge, Group, Text } from '@mantine/core'
+
+const items = [
+  { author: 'Author 1', filler: 'Filler 1', time: 'Time 1' },
+  { author: 'Author 2', filler: 'Filler 2', time: 'Time 2' },
+  { author: 'Author 3', filler: 'Filler 3', time: 'Time 3' },
+]
 
 export function Activity() {
-  return <Skeleton animate={false} height={70} width="100%" mb="xl" />
+  return (
+    <div>
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>{'Activity'}</Text>
+        <Group>
+          <Text>{'Subscribe'}</Text>
+          <Badge color="pink">{''}</Badge>
+        </Group>
+      </Group>
+      {items.map((item, index) => (
+        <Group key={index}>
+          <Badge color="pink">{''}</Badge>
+          <Group>
+            <Text>{item.author}</Text>
+            <Text c={'dimmed'}>{item.filler}</Text>
+            <Text c={'dimmed'}>{`· ${item.time}`}</Text>
+          </Group>
+        </Group>
+      ))}
+    </div>
+  )
 }
--- js/src/lilac/components/task/subissues/SubIssues.tsx
+++ js/src/lilac/components/task/subissues/SubIssues.tsx
@@ -1,32 +1,9 @@
-import { Text, Group, Badge } from '@mantine/core'
-
-const items = [
-  { author: 'Author 1', filler: 'Filler 1', time: 'Time 1' },
-  { author: 'Author 2', filler: 'Filler 2', time: 'Time 2' },
-  { author: 'Author 3', filler: 'Filler 3', time: 'Time 3' },
-  // Add more items as needed
-]
+import { NavbarLinksGroup } from '@/lilac/components/task/subissues/subgroup/LinksGroup.tsx'
 
 export function SubIssues() {
   return (
     <div>
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>{'Activity'}</Text>
-        <Group>
-          <Text>{'Subscribe'}</Text>
-          <Badge color="pink">{''}</Badge>
-        </Group>
-      </Group>
-      {items.map((item, index) => (
-        <Group key={index}>
-          <Badge color="pink">{''}</Badge>
-          <Group>
-            <Text>{item.author}</Text>
-            <Text c={'dimmed'}>{item.filler}</Text>
-            <Text c={'dimmed'}>{`· ${item.time}`}</Text>
-          </Group>
-        </Group>
-      ))}
+      <NavbarLinksGroup />
     </div>
   )
 }
