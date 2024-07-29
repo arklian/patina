86c7910d161339ac273192c002092fe23b11b03a
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-29 14:47:33 -0400

    DigitalOcean: Figure out standard format for images
    L: ENG-259
    Change-Id: Ib7e9053a9c3c2c763bc6d33d38d2a4c2df21b1d1

--- js/src/patina/assets/images.ts
+++ js/src/patina/assets/images.ts
@@ -1,61 +1,39 @@
 /**
  * Images for patina website, hosted using figma
- * https://webapps.stackexchange.com/a/98573
+ * https://drive.google.com/drive/u/0/folders/1Udaumu-JfYuddAy1_kBXfndFggeUhIwz
  */
 export const imageUrls = {
   patinaLogo: {
-    drive:
-      'https://drive.google.com/file/d/1DwXQtbDrDBiRiS96KEbJj84FIj_3ARuI/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Logo2.png',
   },
   mentorship: {
-    drive:
-      'https://drive.google.com/file/d/1jGA8KWh0NVbS1s-zIDzTVRY27A_Y8o86/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Mentorship.jpeg',
   },
   scholarship: {
-    drive:
-      'https://drive.google.com/file/d/1aDgXnl75WwSaoNFwzPiO5fVvZ78sBHPZ/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Scholarship.jpeg',
   },
   scholarshipHome: {
-    drive:
-      'https://drive.google.com/file/d/1aDgXnl75WwSaoNFwzPiO5fVvZ78sBHPZ/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/ScholarshipHome.png',
   },
   mentorHome: {
-    drive:
-      'https://drive.google.com/file/d/1aDgXnl75WwSaoNFwzPiO5fVvZ78sBHPZ/view',
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/mentorshiphero.png',
+    src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Mentorship.jpeg',
   },
   community: {
-    drive:
-      'https://drive.google.com/file/d/16fqE7xIAEA_4mH32dLcN5lm804aBCozd/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Community.jpeg',
   },
   homeSplash: {
-    drive:
-      'https://drive.google.com/file/d/1wQR-vC-UXMqiJaBsskk6uA4Ehcpast87/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/HeaderImages.png',
   },
   scholarSplash: {
-    drive:
-      'https://drive.google.com/file/d/1gVKHEX9NJExD83VqCsNo4IosDDF1h5r-/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/ScholarshipHome.png',
   },
   learn: {
-    drive:
-      'https://drive.google.com/file/d/1m_rLYBHiNmBc1BQPIZcGRF7py6Gkr1t9/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Learn.png',
   },
   hands: {
-    drive:
-      'https://drive.google.com/file/d/1oS2Un2VuVm_xwG6nG_fA2wg9eGCJw-jn/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Hands.png',
   },
   heart: {
-    drive:
-      'https://drive.google.com/file/d/1htDYhOf4XefDcHicVvctZGGuIPx9gutB/view',
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/Heart.png',
   },
   volunteerOpportunities: {
