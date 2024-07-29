b7c29041edf39f646bd5b9c0da7798f137895907
Henry Chen <hjc77@cornell.edu>
2024-07-28 20:18:59 -0400

    Community: Fetch Events with React Query
    First pass at fetching Events using React Query. Just prints out the
    events list to console for now.
    
    CORS:
    - There was issues with hitting the Spring endpoint from the Vite local
      page, as it was hitting CORS issues.
    
    - Originally used the following annotation on the Spring controller:
      @CrossOrigin(origins = {"http://localhost:5173"})
    
    - That worked, but found out that Vite has an option to proxy requests
      in the server config, so decided to use that instead.
    
    TEST=manual
    Can see the Events data in the console
    Can hit localhost:8080 from localhost:5173
    
    Change-Id: Ifa499874dd68fd3a0b014cb717454a0f3131bae9

--- js/src/patina/Patina.page.tsx
+++ js/src/patina/Patina.page.tsx
@@ -1,19 +1,25 @@
 import { MantineProvider } from '@mantine/core'
 import { Outlet } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { theme, resolver } from './theme.ts'
 import { NavBar } from './navbar/NavBar.tsx'
 import { Footer } from '@/patina/components/Footer.tsx'
 
+// Singleton query client for react-query
+const queryClient = new QueryClient()
+
 export function PatinaPage() {
   return (
-    <MantineProvider
-      theme={theme}
-      cssVariablesResolver={resolver}
-      forceColorScheme="dark"
-    >
-      <NavBar />
-      <Outlet />
-      <Footer />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider
+        theme={theme}
+        cssVariablesResolver={resolver}
+        forceColorScheme="dark"
+      >
+        <NavBar />
+        <Outlet />
+        <Footer />
+      </MantineProvider>
+    </QueryClientProvider>
   )
 }
