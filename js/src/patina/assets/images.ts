f27a10fa474231fc1544652b1025d9b02b99cc6a
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-29 16:51:58 -0400

    Images: Removes cdn from images
    TEST=manual
    passed
    
    Change-Id: I2d4ed6f73f028f4984f4b69bd31ea6df12a852c3

--- js/src/patina/assets/images.ts
+++ js/src/patina/assets/images.ts
@@ -4,42 +4,42 @@
  */
 export const imageUrls = {
   patinaLogo: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/patinaLogo.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/patinaLogo.png',
   },
   mentorshipHome: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/mentorshipHome.jpeg',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/mentorshipHome.jpeg',
   },
   scholarshipHome: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/scholarshipHome.jpeg',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/scholarshipHome.jpeg',
   },
   scholarshipHero: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/scholarshipHero.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/scholarshipHero.png',
   },
   mentorApplyHero: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/mentorApplyHero.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/mentorApplyHero.png',
   },
   communityHome: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/communityHome.jpeg',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/communityHome.jpeg',
   },
   homeHero: {
-    src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/homeHero.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/homeHero.png',
   },
   volunteerHero: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/volunteerHero.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/volunteerHero.png',
   },
   mentorshipHero: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/mentorshipHero.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/mentorshipHero.png',
   },
   communityHero: {
-    src: 'https://patina-dev.nyc3.cdn.digitaloceanspaces.com/webimg/communityHero.png ',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/communityHero.png',
   },
   learn: {
     src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/learn.png',
   },
   hands: {
-    src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/hands.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/hands.png',
   },
   heart: {
-    src: 'https://patina-prod.nyc3.digitaloceanspaces.com/webimg/heart.png',
+    src: 'https://patina-dev.nyc3.digitaloceanspaces.com/webimg/heart.png',
   },
 }
