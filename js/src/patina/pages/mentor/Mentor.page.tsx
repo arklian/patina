9b90537703fdea5239269c8a018f469e549b464c
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-07-30 15:53:25 -0400

    Mentor: Application Section
    - Added the banner to the page [unfinished]
    
    L: ENG-305
    Change-Id: Ia9ba20e324c6829f2f4bb3530cf4f35e91b08988

--- js/src/patina/pages/mentor/Mentor.page.tsx
+++ js/src/patina/pages/mentor/Mentor.page.tsx
@@ -2,6 +2,7 @@ import { Hero } from '@/patina/pages/mentor/hero/Hero'
 import { Impact } from '@/patina/pages/mentor/impact/Impact'
 import { About } from '@/patina/pages/mentor/about/About'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import { Banner } from '@/patina/pages/mentor/banner/banner.tsx'
 
 /**
  * Mentor page allows people to apply to be mentors.
@@ -11,6 +12,7 @@ export function MentorPage() {
     <ContentPage>
       <Hero />
       <About />
+      <Banner />
       <Impact />
     </ContentPage>
   )
