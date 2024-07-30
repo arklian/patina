ae340a1fb024ee5010cf7a8d70c8c2762dd61a06
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-30 16:42:17 -0400

    Mentorship: Finishes impact section
    Also adjust card component to work with more cases
    
    L=ENG-301
    TEST=manual
    runs
    
    Image URL: https://patina-dev.nyc3.cdn.digitaloceanspaces.com/screenshot/Screenshot-2024-07-30T17:34:46-MalihaTasnim-impact.png
    
    Change-Id: Ib9967dab8f2927ce2418e717bdb2ea6cecaf89af

--- js/src/patina/home/Initiatives.tsx
+++ js/src/patina/home/Initiatives.tsx
@@ -1,9 +1,9 @@
 import { Text } from '@mantine/core'
-import { Cards } from '../components/Cards.tsx'
+import { Cards, CardsProps } from '@/patina/components/Cards'
 import { imageUrls } from '@/patina/assets/images'
 import styles from './Initiatives.module.css'
 
-const detailsMap = [
+const detailsMap: CardsProps[] = [
   {
     title: 'Mentorship',
     details:
@@ -11,6 +11,8 @@ const detailsMap = [
     img: imageUrls.mentorshipHome.src,
     link: '/mentorship',
     alt: 'Group picture of all interns.',
+    bottom: 'Learn more\u2192',
+    underline: true,
   },
   {
     title: 'Scholarship',
@@ -18,6 +20,8 @@ const detailsMap = [
     img: imageUrls.scholarshipHome.src,
     link: '/scholarship',
     alt: 'Woman posing in graduation gown and holding a degree.',
+    bottom: 'Learn more\u2192',
+    underline: true,
   },
   {
     title: 'Community',
@@ -26,6 +30,8 @@ const detailsMap = [
     img: imageUrls.communityHome.src,
     link: '/community',
     alt: 'Group of women sitting on chairs. They are talking to each other.',
+    bottom: 'Learn more\u2192',
+    underline: true,
   },
 ]
 
@@ -36,16 +42,20 @@ export function Initiatives() {
       <div className={styles.initiatives_container}>
         <div className={styles.container}>
           <Text className={styles.title}>{'Our initiatives'}</Text>
-          {detailsMap.map(({ title, details, img, link, alt }, index) => (
-            <Cards
-              key={index}
-              title={title}
-              details={details}
-              img={img}
-              link={link}
-              alt={alt}
-            />
-          ))}
+          {detailsMap.map(
+            ({ title, details, img, link, bottom, alt, underline }, index) => (
+              <Cards
+                key={index}
+                title={title}
+                details={details}
+                img={img}
+                link={link}
+                alt={alt}
+                bottom={bottom}
+                underline={underline}
+              />
+            ),
+          )}
         </div>
       </div>
     </>
---
+++ js/src/patina/mentorship/impact/Impact.tsx
@@ -0,0 +1,49 @@
+import { Text } from '@mantine/core'
+import { Cards } from '@/patina/components/Cards'
+import { imageUrls } from '@/patina/assets/images'
+import styles from './Impact.module.css'
+
+const detailsMap = [
+  {
+    title: 'Name',
+    details: ' “Quote Lorem ipsum dolor sit amet, consectetur adipiscing” ',
+    img: imageUrls.placeholder.src,
+    bottom: 'Job Placement Stat',
+    alt: 'Placeholder',
+    underline: false,
+  },
+  {
+    title: 'Name',
+    details: ' “Quote Lorem ipsum dolor sit amet, consectetur adipiscing” ',
+    img: imageUrls.placeholder.src,
+    bottom: 'Job Placement Stat',
+    alt: 'Placeholder',
+    underline: false,
+  },
+]
+
+/** Displays custom cards and images of the various initiatives of Patina */
+export function Impact() {
+  return (
+    <>
+      <div className={styles.impact_container}>
+        <Text className={styles.title}>{'Impact'}</Text>
+        <div className={styles.container}>
+          {detailsMap.map(
+            ({ title, details, img, bottom, alt, underline }, index) => (
+              <Cards
+                key={index}
+                title={title}
+                details={details}
+                img={img}
+                bottom={bottom}
+                alt={alt}
+                underline={underline}
+              />
+            ),
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
