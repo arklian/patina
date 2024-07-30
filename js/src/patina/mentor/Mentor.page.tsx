1eafee42e911d6077a9fb61f249ec7221d2f8c29
Maliha Tasnim <ayesiddika@gmail.com>
2024-07-30 17:01:25 -0400

    Mentor: Application Section
    - Added the banner to the page
    
    L: ENG-305
    Image URL: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-07-30T20:30:26-MalihaTasnim-banner.png
    
    Change-Id: Ia9ba20e324c6829f2f4bb3530cf4f35e91b08988

--- js/src/patina/mentor/Mentor.page.tsx
+++ js/src/patina/mentor/Mentor.page.tsx
@@ -2,6 +2,7 @@ import { Stack, Container } from '@mantine/core'
 import { Hero } from '@/patina/mentor/hero/Hero'
 import { Impact } from '@/patina/mentor/impact/Impact'
 import { About } from '@/patina/mentor/about/About'
+import { Banner } from '@/patina/mentor/banner/Banner.tsx'
 
 /**
  * Mentor page allows people to apply to be mentors.
@@ -12,6 +13,11 @@ export function MentorPage() {
       <Stack>
         <Hero />
         <About />
+<<<<<<< Updated upstream
+=======
+        {/* 75rem == 1200px */}
+        <Banner />
+>>>>>>> Stashed changes
         <Impact />
       </Stack>
     </Container>
