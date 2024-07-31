67bb962f556107d086fbf5c23a5a655bb993fb50
Henry Chen <hjc77@cornell.edu>
2024-07-31 16:50:26 -0400

    Patina: Use common ContentPage for pages
    TEST=manual
    Checked every page is fine.
    
    Change-Id: Iff857319d5fee20be4aacec47a81a12d90ac5612

--- js/src/patina/pages/community/Community.page.tsx
+++ js/src/patina/pages/community/Community.page.tsx
@@ -1,14 +1,12 @@
-import { Container, Stack } from '@mantine/core'
 import { Hero } from '@/patina/pages/community/hero/Hero.tsx'
 import { MainSection } from '@/patina/pages/community/events/MainSection.tsx'
+import { ContentPage } from '@/patina/components/ContentPage.tsx'
 
 export function CommunityPage() {
   return (
-    <Container size={'75rem'}>
-      <Stack>
-        <Hero />
-        <MainSection />
-      </Stack>
-    </Container>
+    <ContentPage>
+      <Hero />
+      <MainSection />
+    </ContentPage>
   )
 }
