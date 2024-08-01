318b7eca975c6c14e9948133ff5469ee0bbac297
Haoking Luo <hao5k6032@gmail.com>
2024-08-01 14:42:04 -0400

    Blog: Create authentication functionality
    Created a LoginController with an endpoint /api/login that takes in a
    raw string and compares it to an admin password. If comparison passes,
    creates a new "auth" cookie in the browser.
    Inside the LoginPage component, the login button now fetches to the
    login endpoint and inputs the user inputted password string as the body
    of the request.
    
    L=ENG-350
    TEST=manual
    - gw bootRun on terminal, go to localhost:8080/login
    - Inputted wrong password, nothing added to cookies
    - Inputted correct password, "auth" cookie added to browser
    
    Change-Id: I2c7f7d0d404700787f4905fdd20b9783521ef104

---
+++ src/main/java/org/patinanetwork/patinawebsite/login/LoginController.java
@@ -0,0 +1,27 @@
+package org.patinanetwork.patinawebsite.login;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for handling login
+ */
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class LoginController {
+
+    @PostMapping(value = "/api/login", consumes = "text/plain")
+    public String login(@RequestBody String input, HttpServletResponse response) {
+        if (input.equals("asoifj4y04h02h0hwas0fha0shxzASFHsaflhsaflk")) {
+            Cookie cookie = new Cookie("auth", "o3hto2dsgidshg");
+            response.addCookie(cookie);
+            return "Login success";
+        }
+        return "Invalid login";
+    }
+}
