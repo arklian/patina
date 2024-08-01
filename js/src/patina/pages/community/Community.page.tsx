806e3cbd53627dc137aab0b2a6c271017af42f55
Maliha Tasnim <ayesiddika@gmail.com>
2024-08-01 16:27:39 -0400

    All Pages: Makes hero component for consistency
    Also fixed scholarship content
    Also added special text to volunteer
    
    L=ENG-387
    L=ENG-378
    TEST=manual
    Ran this test and it passed.
    Image URL: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-01T20:07:43-MalihaTasnim-volunteer.png
    Image URL: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-01T20:09:37-MalihaTasnim-scholar.png
    
    Change-Id: I1bd9a34e56cc73394e15236a57a93f1431c95914

--- js/src/patina/pages/community/Community.page.tsx
+++ js/src/patina/pages/community/Community.page.tsx
@@ -1,11 +1,24 @@
-import { Hero } from '@/patina/pages/community/hero/Hero.tsx'
+import { Text } from '@mantine/core'
+import { Hero } from '@/patina/components/Hero'
+import { imageUrls } from '@/patina/assets/images'
 import { MainSection } from '@/patina/pages/community/events/MainSection.tsx'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import styles from './Community.module.css'
 
 export function CommunityPage() {
+  const message = (
+    <Text className={styles.description}>
+      {"Patina Network's community events include ..."}
+    </Text>
+  )
   return (
     <ContentPage>
-      <Hero />
+      <Hero
+        title="Community Events"
+        details={message}
+        alt="idk"
+        img={imageUrls.communityHero.src}
+      />
       <MainSection />
     </ContentPage>
   )
