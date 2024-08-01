9a4c20ece9d50ffa6df4f2b4c07cc3feb396bfbf
Maliha Tasnim <ayesiddika@gmail.com>
2024-08-01 16:44:28 -0400

    All Pages: Makes hero component for consistency
    Also fixed scholarship content
    Also added special text to volunteer
    
    L=ENG-387
    L=ENG-378
    TEST=manual
    Ran this test and it passed.
    Image URL: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-01T20:07:43-MalihaTasnim-volunteer.png
    Image URL: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-01T20:09:37-MalihaTasnim-scholar.png
    
    Change-Id: I1bd9a34e56cc73394e15236a57a93f1431c95914

--- js/src/patina/pages/mentorship/Mentorship.page.tsx
+++ js/src/patina/pages/mentorship/Mentorship.page.tsx
@@ -1,19 +1,36 @@
+import { Text } from '@mantine/core'
 import { About } from '@/patina/pages/mentorship/about/About.tsx'
 import { Apply } from '@/patina/pages/mentorship/apply/Apply.tsx'
 import { Gain } from '@/patina/pages/mentorship/gain/Gain.tsx'
-import { Hero } from '@/patina/pages/mentorship/hero/Hero.tsx'
+import { Hero } from '@/patina/components/Hero'
 import { Details } from '@/patina/pages/mentorship/details/Details.tsx'
 import { Impact } from '@/patina/pages/mentorship/impact/Impact.tsx'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import { imageUrls } from '@/patina/assets/images'
+import styles from './Mentorship.module.css'
 
 /**
  * MentorshipPage component renders a mentorship page layout with multiple sections.
  * Each section is represented by a Skeleton component, which can be replaced with actual content.
  * */
 export function MentorshipPage() {
+  const message = (
+    <Text className={styles.description}>
+      {'The Patina Network mentorship program is open to students currently ' +
+        'enrolled in any US college program or recently graduated who are ' +
+        'interested in guidance with their workplace readiness.'}
+    </Text>
+  )
   return (
     <ContentPage>
-      <Hero />
+      {/*TODO: replace with proper button link later */}
+      <Hero
+        title="Mentorship"
+        details={message}
+        img={imageUrls.mentorshipHero.src}
+        button="https://docs.google.com/forms/d/e/1FAIpQLSfpYDIYEIDlvC922E7tUXXlBlOhpEt2-DPkCK0nMzcAPmgX-w/viewform"
+        alt="A man and a woman sitting at a desk"
+      />
       <About />
       <Gain />
       <Details />
--- js/src/patina/pages/scholarship/Scholarship.page.tsx
+++ js/src/patina/pages/scholarship/Scholarship.page.tsx
@@ -1,53 +1,35 @@
-import { Button, Group, Image, SimpleGrid, Text, Title } from '@mantine/core'
-import { useMediaQuery } from '@mantine/hooks'
+import { SimpleGrid, Text } from '@mantine/core'
 import { Forms } from '@/patina/components/Forms.tsx'
 import { Criteria } from '@/patina/pages/scholarship/criteria/Criteria.tsx'
 import { ApplicationProcess } from '@/patina/pages/scholarship/applicationprocess/ApplicationProcess.tsx'
 import { AboutScholarship } from '@/patina/pages/scholarship/about/AboutScholarship.tsx'
 import { Application } from '@/patina/pages/scholarship/application/Application.tsx'
+import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import { Hero } from '@/patina/components/Hero'
 // import { DonorSection } from '@/patina/pages/scholarship/donors/Donors.page.tsx'
 import { imageUrls } from '@/patina/assets/images.ts'
 import styles from './Scholarship.module.css'
-import { ContentPage } from '@/patina/components/ContentPage.tsx'
 
 export function ScholarshipPage() {
-  const largeScreen = useMediaQuery('(min-width: 60em)')
+  const message = (
+    <Text className={styles.description}>
+      {'The Patina Network Scholarship Fund intends to manifest our '}
+      <span className={styles.colored}>{'vision'}</span>
+      {
+        ' by awarding scholarships annually to students who are of ethnicity, heritage, or ancestry in relations to the AANHPI communities and would benefit the most from the financial stipend.'
+      }
+    </Text>
+  )
   return (
     <ContentPage>
-      <SimpleGrid
-        className={styles.heroContainer}
-        cols={largeScreen ? 2 : 1}
-        spacing="xl"
-        mb={'7rem'}
-        mt={'4rem'}
-      >
-        <div>
-          <Title className={styles.title} ta={'left'}>
-            {'Patina Network Scholarship Fund'}
-          </Title>
-          <Text className={styles.description}>
-            {
-              'The Patina Network Scholarship Fund intends to manifest our vision by awarding scholarships annually to students who are of ethnicity, heritage, or ancestry in relations to the AANHPI communities and would benefit the most from the financial stipend.'
-            }
-          </Text>
-          <Group justify={'left'} pt={'md'}>
-            <Button
-              variant={'filled'}
-              color={'rgba(255, 255, 255, 1)'}
-              size={'md'}
-              autoContrast
-            >
-              {'Apply Now'}
-            </Button>
-          </Group>
-        </div>
-        <Group justify={'center'} pt={'lg'}>
-          <Image
-            className={styles.scholarshipHero}
-            src={imageUrls.scholarshipHero.src}
-            alt="Scholarship Image"
-          />
-        </Group>
+      <SimpleGrid>
+        <Hero
+          title="Scholarship Fund"
+          details={message}
+          img={imageUrls.scholarshipHero.src}
+          button="https://docs.google.com/forms/d/e/1FAIpQLSfE93_PaX8Eynv_6zdMQnYHiljFq7Ci_E0_9hzRs2z3aSE3-g/viewform?fbzx=-2135089361293927732"
+          alt="Woman holding books"
+        />
       </SimpleGrid>
       <div className={styles.about}>
         <ApplicationProcess />
