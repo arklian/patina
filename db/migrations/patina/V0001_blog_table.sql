d7ced523f43abd5f7320070bcc628a44f973c47f
Riyuan Liu <riyusnliu@gmail.com>
2024-08-02 02:01:51 -0400

    Patnet: Created table for Blog
    created blog sql table
    
    L=ENG-346
    Test=NONE
    
    Change-Id: Ie31cf073f571504a11ff1a6fcfe479f26f20ab18

---
+++ db/migrations/patina/V0001_blog_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE blogpost
+(
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
