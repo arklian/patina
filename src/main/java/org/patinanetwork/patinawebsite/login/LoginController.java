8136446a5f733ea945a11c622d3f82bc766d78e6
Haoking Luo <hao5k6032@gmail.com>
2024-08-02 13:19:57 -0400

    Blog: Add password and cookie auth to secrets
    Added Value annotations inside LoginController to use secrets for both
    the Patina admin password and the cookie as defined in patina-secrets.
    
    L=ENG-393
    TEST=manual
    - gw bootRun and logged in with admin password and correct auth token
    shows up as cookies.
    
    Change-Id: I14a4770b60cdbd5520cd9bdc00032e8c5de118a3

--- src/main/java/org/patinanetwork/patinawebsite/login/LoginController.java
+++ src/main/java/org/patinanetwork/patinawebsite/login/LoginController.java
@@ -2,6 +2,7 @@ package org.patinanetwork.patinawebsite.login;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,10 +18,16 @@ import java.util.Map;
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoginController {
 
+    @Value("${PATINA_ADMIN_PASSWORD}")
+    private String ADMIN_PASSWORD;
+
+    @Value("${PATINA_ADMIN_COOKIE}")
+    private String ADMIN_COOKIE;
+
     @PostMapping(value = "/api/login", consumes = "application/json")
     public String login(@RequestBody Map<String, Object> input, HttpServletResponse response) {
-        if (input.get("password").equals("asoifj4y04h02h0hwas0fha0shxzASFHsaflhsaflk")) {
-            Cookie cookie = new Cookie("auth", "o3hto2dsgidshgid30285y038hs0fisasflszf");
+        if (input.get("password").equals(ADMIN_PASSWORD)) {
+            Cookie cookie = new Cookie("auth", ADMIN_COOKIE);
             response.addCookie(cookie);
             return "Login success";
         }
