7b19d7edbe1dcafea5074e0e5acb44d4b9477e47
Bryan Zhang <bz5989@princeton.edu>
2024-08-05 13:08:42 -0400

    Patina: Fixed forms padding/updated Hero
    Now forms padding is good and hero has optional button capability
    (when not given link, won't show button)
    
    Include text capability
    
    L=ENG-371
    TEST=manual
    
    Change-Id: I4274f243f7490b798e53a45ffddebd3b49a761b4

--- js/src/patina/pages/mentorship/Mentorship.page.tsx
+++ js/src/patina/pages/mentorship/Mentorship.page.tsx
@@ -27,8 +27,9 @@ export function MentorshipPage() {
         title="Patina Network Mentorship"
         details={message}
         img={imageUrls.mentorshipHero.src}
-        button="https://docs.google.com/forms/d/e/1FAIpQLSfpYDIYEIDlvC922E7tUXXlBlOhpEt2-DPkCK0nMzcAPmgX-w/viewform"
+        buttonLink="https://docs.google.com/forms/d/e/1FAIpQLSfpYDIYEIDlvC922E7tUXXlBlOhpEt2-DPkCK0nMzcAPmgX-w/viewform"
         alt={imageUrls.mentorshipHero.alt}
+        buttonText="Apply Here"
       />
       <About />
       <Gain />
