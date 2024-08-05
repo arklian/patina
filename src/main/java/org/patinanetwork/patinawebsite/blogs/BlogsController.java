7242c2e2a62902ecc352014539107edb28deb5de
Riyuan Liu <riyusnliu@gmail.com>
2024-08-05 19:39:28 +0000

    PatNet: Set up backend for submitting blog posts
    Implemented the backend logic to handle blog submissions. The endpoint
    `/api/blog/submit` now processes incoming blog data in JSON format, parses
    it into Protobuf objects, and performs validation for required fields. If
    valid, it stores the blog post in the database with the current timestamp.
    Added necessary error handling for invalid requests and server issues.
    
    L=ENG-349
    TEST=manual
    - Verified the endpoint by sending various requests and confirmed
      correct handling of valid and invalid data.
    
    Change-Id: Ifc7e415b4d24b8c3bda16895b03d1a367fb09226

--- src/main/java/org/patinanetwork/patinawebsite/blogs/BlogsController.java
+++ src/main/java/org/patinanetwork/patinawebsite/blogs/BlogsController.java
@@ -1,20 +1,55 @@
 package org.patinanetwork.patinawebsite.blogs;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.patinanetwork.common.protos.JsonParser;
+import org.patinanetwork.common.protos.JsonPrinter;
+import org.patinanetwork.patinawebsite.blogs.protos.Blog;
+import org.patinanetwork.patinawebsite.blogs.protos.CreateBlogReq;
+import org.patinanetwork.patinawebsite.blogs.protos.CreateBlogResp;
+import org.patinanetwork.patinawebsite.blogs.repo.BlogsRepo;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class BlogsController {
 
-    @PostMapping(value = "/api/blog/submit")
-    public BlogResponse submitBlog(@RequestBody BlogRequest blogRequest) {
-        // Log the received data (optional)
-        System.out.println("Received blog submission: " + blogRequest.getName() + ", " + blogRequest.getTitle() + ", " + blogRequest.getContent());
+    BlogsRepo blogsRepo;
+    JsonPrinter jsonPrinter;
+    JsonParser jsonParser;
 
-        // Create a response based on the received data
-        BlogResponse response = new BlogResponse(blogRequest.getName(), blogRequest.getTitle(), blogRequest.getContent());
+    public BlogsController(BlogsRepo blogsRepo, JsonPrinter jsonPrinter, JsonParser jsonParser) {
+        this.blogsRepo = blogsRepo;
+        this.jsonPrinter = jsonPrinter;
+        this.jsonParser = jsonParser;
+    }
 
-        return response;
+    @PostMapping(value = "/api/blog/submit", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String CreateBlog(@RequestBody String jsonRequest) throws InvalidProtocolBufferException {
+        // Parse the incoming JSON into a Protobuf CreateBlogReq object
+        CreateBlogReq blogReq = jsonParser.parse(jsonRequest, CreateBlogReq.newBuilder());
+
+        // Get current timestamp in the required format
+        String currentTimestamp = OffsetDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
+
+        // Create a new Blog instance with the current timestamp
+        Blog blog = Blog.newBuilder()
+                .setAuthor(blogReq.getAuthor())
+                .setTitle(blogReq.getTitle())
+                .setContent(blogReq.getContent())
+                .setCreateTime(currentTimestamp)
+                .build();
+
+        // Add the blog to the repository
+        blogsRepo.addBlog(blog);
+        // Return the JSON representation of the Blog object
+        CreateBlogResp resp = CreateBlogResp.newBuilder().setBlog(blog).build();
+        return jsonPrinter.print(resp);
     }
 }
