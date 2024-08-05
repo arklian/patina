5903e215c13b28e769d67ad71b1f08184954a8da
Tony Wang <wangtony4005@gmail.com>
2024-08-05 14:31:15 -0400

    Events: Add addEvent POST request endpoint
    made an addEvent function in repo using prepareStatement for sql.
    created addEvent endpoint in controller @ api/events/add that takes in json,
    and using the jsonParser to make new event object.
    
    Test=manual
    L=ENG-344
    
    Change-Id: I39a0a0f480cc8b1b376cd4b09df45044bd69f3d7

--- src/main/java/org/patinanetwork/patinawebsite/events/EventsController.java
+++ src/main/java/org/patinanetwork/patinawebsite/events/EventsController.java
@@ -3,14 +3,18 @@ package org.patinanetwork.patinawebsite.events;
 import org.patinanetwork.common.protos.JsonParser;
 import org.patinanetwork.common.protos.JsonPrinter;
 import org.patinanetwork.patinawebsite.events.infra.EventsRepo;
+import org.patinanetwork.patinawebsite.events.protos.CreateEventReq;
+import org.patinanetwork.patinawebsite.events.protos.CreateEventResp;
+import org.patinanetwork.patinawebsite.events.protos.DeleteEventResp;
 import org.patinanetwork.patinawebsite.events.protos.Event;
 import org.patinanetwork.patinawebsite.events.protos.GetEventResp;
 import org.patinanetwork.patinawebsite.events.protos.ListEventResp;
-import org.patinanetwork.patinawebsite.events.protos.DeleteEventResp;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -51,5 +55,14 @@ public class EventsController {
         Event event = eventsRepo.deleteEvent(eventId);
         DeleteEventResp resp = DeleteEventResp.newBuilder().setEvent(event).build();
         return jsonPrinter.print(event);
+    }
+
+    @PostMapping(value = "/api/events/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String createEvent(@RequestBody String jsonEvent) {
+        CreateEventReq req = jsonParser.parse(jsonEvent, CreateEventReq.newBuilder());
+        Event event = req.getEvent();
+        eventsRepo.createEvent(event);
+        CreateEventResp resp = CreateEventResp.newBuilder().setEvent(event).build();
+        return jsonPrinter.print(resp);
     }
 }
