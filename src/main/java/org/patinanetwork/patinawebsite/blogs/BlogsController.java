b4c94012f3aa853e8b08ee1d10f542aa4eb3ec96
Haoking Luo <hao5k6032@gmail.com>
2024-08-06 16:50:44 -0400

    Blog: List latest blogs and all blogs on frontend
    Modified ImageCard component to take in a blog's title, content, tags
    to be displayed. Added a GET endpoint to BlogsController to list all
    entries in the blogs database table. Modified BlogPage component to
    fetch to /api/blogs endpoint and store all blogs in an array which is
    sorted based on each blog's createTime from latest to earliest.
    
    L=ENG-398
    TEST=manual
    - Ran gw bootRun and entries from blog table in pgAdmin show up and are
    displayed properly with ImageCards.
    
    Change-Id: Ic3a7183c7b5f3c875dd0352320838c0b2a8435ec

--- src/main/java/org/patinanetwork/patinawebsite/blogs/BlogsController.java
+++ src/main/java/org/patinanetwork/patinawebsite/blogs/BlogsController.java
@@ -1,20 +1,22 @@
 package org.patinanetwork.patinawebsite.blogs;
 
-import com.google.protobuf.InvalidProtocolBufferException;
 import org.patinanetwork.common.protos.JsonParser;
 import org.patinanetwork.common.protos.JsonPrinter;
 import org.patinanetwork.patinawebsite.blogs.protos.Blog;
 import org.patinanetwork.patinawebsite.blogs.protos.CreateBlogReq;
 import org.patinanetwork.patinawebsite.blogs.protos.CreateBlogResp;
+import org.patinanetwork.patinawebsite.blogs.protos.ListBlogResp;
 import org.patinanetwork.patinawebsite.blogs.repo.BlogsRepo;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -31,7 +33,7 @@ public class BlogsController {
     }
 
     @PostMapping(value = "/api/blog/submit", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String CreateBlog(@RequestBody String jsonRequest){
+    public String CreateBlog(@RequestBody String jsonRequest) {
         // Parse the incoming JSON into a Protobuf CreateBlogReq object
         CreateBlogReq blogReq = jsonParser.parse(jsonRequest, CreateBlogReq.newBuilder());
 
@@ -50,6 +52,13 @@ public class BlogsController {
         blogsRepo.addBlog(blog);
         // Return the JSON representation of the Blog object
         CreateBlogResp resp = CreateBlogResp.newBuilder().setBlog(blog).build();
+        return jsonPrinter.print(resp);
+    }
+
+    @GetMapping(value = "/api/blogs")
+    public String listBlogs() {
+        List<Blog> blogs = blogsRepo.listAllBlogs();
+        ListBlogResp resp = ListBlogResp.newBuilder().addAllBlogs(blogs).build();
         return jsonPrinter.print(resp);
     }
 }
