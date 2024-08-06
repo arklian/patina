d00c95c1e83b131696b80b88ea2e88ca7bb57548
Riyuan Liu <riyusnliu@gmail.com>
2024-08-06 16:13:03 -0400

    PatNet: Set up basic oauth for Testing
    - only set up for github
    - Configured OAuth2 login flow in the SecurityConfig class
    - Updated dependencies in build.gradle to include spring-boot-starter-security and other required libraries
    - Now when try to go to admin page, u will be force to login
    
    This setup is for initial testing and can be further customized as needed.
    
    TEST=manual
    -try locally to go to admin page, redirect me to login
    -github login work
    
    Change-Id: I6deae6d37467cc3aa1f4cb91bad310d559854e4f

---
+++ src/main/java/org/patinanetwork/patinawebsite/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package org.patinanetwork.patinawebsite.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/api/secured").authenticated();
+                    // TODO: no database, as long they login, they can access the page
+                    auth.anyRequest().permitAll();
+                })
+                .oauth2Login(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+    }
+
+}
