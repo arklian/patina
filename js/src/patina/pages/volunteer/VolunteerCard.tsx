fd3689877ad8aadf3a22202fff1da231a5d97783
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-08-07 07:57:09 +0000

    PatNet: Make volunteer card a modal
    - make the volunteer cards clickable, to show more
    info
    
    L=ENG-423
    
    Image URL: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-07T07:21:16-MReyes1241-volunteerModalClosed.png
    
    Image URL: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-07T07:22:00-MReyes1241-volunteerModalOpen.png
    
    Change-Id: I8fd44b74683dc27629c05ec703591774332ac104

--- js/src/patina/pages/volunteer/VolunteerCard.tsx
+++ js/src/patina/pages/volunteer/VolunteerCard.tsx
@@ -1,24 +1,61 @@
-import { Card, Title, Text } from '@mantine/core'
+import { Modal, Card, Title, Text } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 import styles from './VolunteerCard.module.css'
 
 interface CardProps {
   title: string
   description: string
   type: string
+  modalContent: string
 }
 
-export function VolunteerCard({ title, description, type }: CardProps) {
+export function VolunteerCard({
+  title,
+  description,
+  type,
+  modalContent,
+}: CardProps) {
+  const [opened, { open, close }] = useDisclosure(false)
+
   return (
-    <Card className={styles.card}>
-      <Title order={4} className={styles.cardTitle}>
-        {title}
-      </Title>
-      <Text size="sm" className={styles.cardRoleType}>
-        {type}
-      </Text>
-      <Text size="sm" className={styles.cardDescription}>
-        {description}
-      </Text>
-    </Card>
+    <>
+      <Card
+        onClick={open}
+        className={styles.card}
+        style={{ cursor: 'pointer' }}
+      >
+        <div className={styles.cardContent}>
+          <Title order={4} className={styles.cardTitle}>
+            {title}
+          </Title>
+          <Text size="sm" className={styles.cardRoleType}>
+            {type}
+          </Text>
+          <Text size="sm" className={styles.cardDescription}>
+            {description}
+          </Text>
+        </div>
+        <Text size="sm" className={styles.moreInfo}>
+          {'Click to see more info'}
+        </Text>
+      </Card>
+
+      <Modal
+        opened={opened}
+        onClose={close}
+        title={null}
+        centered
+        className={styles.modal}
+      >
+        <div className={styles.modalContentContainer}>
+          <Title order={4} className={styles.cardTitle}>
+            {title}
+          </Title>
+          <Text size="sm" className={styles.modalContent}>
+            {modalContent}
+          </Text>
+        </div>
+      </Modal>
+    </>
   )
 }
