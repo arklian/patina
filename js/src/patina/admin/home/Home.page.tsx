2ec2427f032094d576dc6c2319c35acc34a2caf3
Riyuan Liu <riyusnliu@gmail.com>
2024-08-07 18:31:52 +0000

    Admin: Update Home Page
    - Enhanced the Home Page for the Admin section with new content and layout.
      Added sections for blog posts and events hosted.
    - Backend setup is still pending for these new sections.
    
    TEST=manual
    - Verified that the new sections are displayed correctly on the Home Page.
    - Checked responsiveness and layout on various devices.
    
    Change-Id: I3f2c8ce03acd447531f0e4d15f519416d27c2690

--- js/src/patina/admin/home/Home.page.tsx
+++ js/src/patina/admin/home/Home.page.tsx
@@ -1,16 +1,33 @@
-import { Container, Skeleton, Stack } from '@mantine/core'
+import { Container, Stack, Title, Card, Text, Grid } from '@mantine/core'
 
 export function HomePage() {
   return (
     <Container>
       <Stack>
-        {'Home Page'}
-        {/* Section 1 */}
-        <Skeleton animate={false} height={70} width="100%" mb="xl" />
-        {/* Section 2 */}
-        <Skeleton animate={false} height={70} width="100%" mb="xl" />
-        {/* Section 3 */}
-        <Skeleton animate={false} height={70} width="100%" mb="xl" />
+        {/* Title */}
+        <Title order={1}>{'Admin Dashboard'}</Title>
+
+        {/* Dashboard Metrics */}
+        <Grid gutter="md">
+          <Grid.Col span={12}>
+            <Card shadow="sm" padding="lg" radius="md" withBorder>
+              <Title order={3}>{'Blog Posted'}</Title>
+              <Text size="xl">{'5'}</Text>
+            </Card>
+          </Grid.Col>
+          <Grid.Col span={12}>
+            <Card shadow="sm" padding="lg" radius="md" withBorder>
+              <Title order={3}>{'Event Hosted'}</Title>
+              <Text size="xl">{'5'}</Text>
+            </Card>
+          </Grid.Col>
+          <Grid.Col span={12}>
+            <Card shadow="sm" padding="lg" radius="md" withBorder>
+              <Title order={3}>{'Total User'}</Title>
+              <Text size="xl">{'10'}</Text>
+            </Card>
+          </Grid.Col>
+        </Grid>
       </Stack>
     </Container>
   )
