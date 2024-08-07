21ea4dc8cb53562ef44a5397d32ed0e1955cb314
KaziMansha <mmansha93@gmail.com>
2024-08-06 20:51:10 -0400

    Internship: Added text elements
    Added a section for logistics and details for the internship. Also,
    added CSS file and changed styling of text for Hero description and
    logistics and details for the internship.
    
    TEST=manual
    
    Change-Id: I1cfc3bf5a749296a441f90b56695feb97b5e1537

--- js/src/patina/pages/internship/Internship.page.tsx
+++ js/src/patina/pages/internship/Internship.page.tsx
@@ -2,10 +2,11 @@ import { Text } from '@mantine/core'
 import { imageUrls } from '@/patina/assets/images.ts'
 import { Hero } from '@/patina/components/Hero'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import styles from './Internship.module.css'
 
 export function InternshipPage() {
   const message = (
-    <Text>
+    <Text className={styles.details}>
       {
         'This summer, we partnered with local tech organizations to offer a tech-focused bootcamp-style internship.'
       }
@@ -19,6 +20,19 @@ export function InternshipPage() {
         img={imageUrls.internshipHero.src}
         alt={imageUrls.internshipHero.alt}
       />
+      <section className={styles.internshipDetails}>
+        <Text className={styles.internshipText}>
+          {'Blah blah logistics and details of the internship'}
+        </Text>
+        <Text className={styles.internshipText}>
+          {
+            'Join our mailing list to get notified about future opportunities like this!'
+          }
+        </Text>
+        <Text className={styles.internshipTextHeader}>
+          {'Check out their work:'}
+        </Text>
+      </section>
     </ContentPage>
   )
 }
