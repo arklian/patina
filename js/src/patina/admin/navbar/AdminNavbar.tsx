579d9f120aba672484e2c3d1d8e423164a225a0e
Riyuan Liu <riyusnliu@gmail.com>
2024-08-07 18:31:37 +0000

    Admin: Implement Navigation Bar for Admin
    Created a navigation bar for the Admin page to improve user navigation.
    The navigation bar has been tested and functions correctly on both
    mobile and desktop devices.
    
    Users can now access the frontend interfaces for submitting events and
    blogs, which communicate with the respective endpoints.
    
    TEST=manual
    - Verified the navigation bar’s responsiveness on various devices.
    - Confirmed that the frontend interfaces for event and blog submission
      are accessible and functioning correctly.
    
    Change-Id: I5f4642761e6ef7c377c83d96609e26f1facb5c65

---
+++ js/src/patina/admin/navbar/AdminNavbar.tsx
@@ -0,0 +1,16 @@
+import { NavLink, Space, Text } from '@mantine/core'
+import { navItems } from './navData'
+
+export function AdminNavbar() {
+  return (
+    <div>
+      {navItems.map((item) => (
+        <NavLink
+          href={item.src}
+          label={<Text size={'xl'}>{item.label}</Text>}
+          leftSection={<Space w={10} />}
+        />
+      ))}
+    </div>
+  )
+}
---
+++ js/src/patina/admin/navbar/navData.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+  { label: 'Patina Network', src: '/' },
+  { label: 'Blog', src: '/admin/blog' },
+  { label: 'Event', src: '/admin/event' },
+  { label: 'Dashboard', src: '/admin' },
+]
