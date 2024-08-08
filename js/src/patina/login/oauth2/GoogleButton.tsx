e6d9410aedca5a51519ce7fdd5ab1193329c25d4
Riyuan Liu <riyusnliu@gmail.com>
2024-08-08 14:40:23 -0400

    Oauth: Setup Google Cloud oauth
    - Created Google Cloud project and configured OAuth consent screen
    - Generated OAuth 2.0 credentials and implemented OAuth flow
    - Integrated Google sign-in functionality with required scopes
    
    TEST=manual
    - User can now access /api/secured after logging in with google
    
    Change-Id: I7b925923d855a5269206cd013929d346be37c59b

--- js/src/patina/login/oauth2/GoogleButton.tsx
+++ js/src/patina/login/oauth2/GoogleButton.tsx
@@ -4,7 +4,16 @@ import { IconBrandGoogle } from '@tabler/icons-react'
 export function GoogleButton(
   props: ButtonProps & React.ComponentPropsWithoutRef<'button'>,
 ) {
+  const handleGoogleLogin = () => {
+    // Redirect to the GitHub OAuth2 authorization endpoint
+    window.location.href = '/oauth2/authorization/google'
+  }
   return (
-    <Button leftSection={<IconBrandGoogle />} variant="default" {...props} />
+    <Button
+      leftSection={<IconBrandGoogle />}
+      variant="default"
+      onClick={handleGoogleLogin}
+      {...props}
+    />
   )
 }
