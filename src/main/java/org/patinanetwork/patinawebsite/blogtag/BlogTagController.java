f728e0327cbd457eac06684b35eae9cc6dcffeab
Kelly Lin <linkelly150@gmail.com>
2024-08-08 16:52:36 -0400

    PatNet: Added more Functions to Blog Tags
    Added create and delete functions, updated protos.
    
    Change-Id: I5b47a97b8252037a0ce72221a789f4a740c4a0c6

--- src/main/java/org/patinanetwork/patinawebsite/blogtag/BlogTagController.java
+++ src/main/java/org/patinanetwork/patinawebsite/blogtag/BlogTagController.java
@@ -2,15 +2,12 @@ package org.patinanetwork.patinawebsite.blogtag;
 
 import org.patinanetwork.common.protos.JsonParser;
 import org.patinanetwork.common.protos.JsonPrinter;
-import org.patinanetwork.patinawebsite.blogtag.protos.Blogtag;
-import org.patinanetwork.patinawebsite.blogtag.protos.CreateBlogtagReq;
-import org.patinanetwork.patinawebsite.blogtag.protos.CreateBlogtagResp;
+import org.patinanetwork.patinawebsite.blogtag.protos.*;
 import org.patinanetwork.patinawebsite.blogtag.repo.BlogTagRepo;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -29,7 +26,35 @@ public class BlogTagController {
     @GetMapping(value = "/api/blogtag/{blogtagId}")
     public String getBlogtag(@PathVariable("blogtagId") int blogtagId) {
         Blogtag blogtag = blogTagRepo.getBlogtagById(blogtagId);
-        GetBl
+        GetBlogTagResp resp = GetBlogTagResp.newBuilder().setBlogtag(blogtag).build();
         return jsonPrinter.print(resp);
     }
+
+    @GetMapping(value = "/api/blogtag/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String createBlogtag(@RequestBody String jsonRequest) {
+        CreateBlogtagReq blogtagReq = jsonParser.parse(jsonRequest, CreateBlogtagReq.newBuilder());
+        Blogtag blogtag = Blogtag.newBuilder()
+                .setId(blogtagReq.getId())
+                .setName(blogtagReq.getName())
+                .build();
+
+        blogTagRepo.addBlogtag(blogtag);
+        CreateBlogtagResp resp = CreateBlogtagResp.newBuilder().setBlogtag(blogtag).build();
+        return jsonPrinter.print(resp);
+    }
+
+    @DeleteMapping(value = "/api/blogtag/delete/ {blogtagId}")
+    public String deleteBlogtag(@PathVariable("blogtagId") int blogtagId) {
+        Blogtag blogtag = blogTagRepo.deleteBlogtag(blogtagId);
+        DeleteBlogtagResp resp = DeleteBlogtagResp.newBuilder().setBlogtag(blogtag).build();
+        return jsonPrinter.print(resp);
+    }
+
+    @GetMapping(value = "/api/blogtag")
+    public String listBlogtags() {
+        List<Blogtag> blogtags = blogTagRepo.listAllBlogtag();
+        ListBlogtagResp resp = ListBlogtagResp.newBuilder().addAllBlogtags(blogtags).build();
+        return jsonPrinter.print(resp);
+    }
+
 }
