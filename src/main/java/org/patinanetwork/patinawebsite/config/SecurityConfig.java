515d1c7a543f4fa8e88f082e8b52afde4afc4033
Riyuan Liu <riyusnliu@gmail.com>
2024-08-08 15:44:37 -0400

    Oauth: Disable CSRF protection for API endpoints
    - Disabled CSRF protection to resolve 403 Forbidden errors for API endpoints.
    - Ensured endpoints such as /api/blog/submit are accessible without CSRF token validation.
    
    Change-Id: I6e034adbad197176681080736d8ce56e5081be42

--- src/main/java/org/patinanetwork/patinawebsite/config/SecurityConfig.java
+++ src/main/java/org/patinanetwork/patinawebsite/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,6 +16,8 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/api/blog/submit","/api/event/delete/**","/api/events/add"))
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/secured").authenticated();
                     // TODO: no database, as long they login, they can access the page
