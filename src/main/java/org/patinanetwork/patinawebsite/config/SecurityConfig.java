4d29d5b43f837d6bfa87aa0bde1cb121d29afc14
Riyuan Liu <riyusnliu@gmail.com>
2024-08-08 16:13:46 -0400

    OAUTH: Setup simple frontend for OAuth with GitHub
    - Added GithubIcon component to render GitHub logo
    - Implemented GithubButton component with OAuth2 authorization redirection
    - Integrated GithubButton in the Login_oauth2Page component
    
    TEST=manual
    - User can access /api/secured only after logging in with GitHub
    
    Change-Id: I3afef64eae9071bbe4ca6d939694bf0de799a389

--- src/main/java/org/patinanetwork/patinawebsite/config/SecurityConfig.java
+++ src/main/java/org/patinanetwork/patinawebsite/config/SecurityConfig.java
@@ -7,23 +7,23 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable()) //TODO: Add back csrf protection
+                http.csrf(csrf -> csrf.disable()) //TODO: Add back csrf protection
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/secured").authenticated();
                     // TODO: no database, as long they login, they can access the page
                     auth.anyRequest().permitAll();
                 })
-                .oauth2Login(withDefaults())
-                .formLogin(withDefaults())
-                .build();
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login_oauth2")
+                        .defaultSuccessUrl("/api/secured", true)  // Redirect to secured page on success
+                        .failureUrl("/login")  // Redirect to login page on failure
+                );
+        return http.build();
     }
-
 }
