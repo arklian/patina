c2ff66579172e03f66634c26cab30c5bf10ed173
Henry Chen <hjc77@cornell.edu>
2024-08-09 16:48:40 +0000

    Events: Update text
    TEST=manual
    
    Change-Id: Ieb75a1bad66d6ad94a3243e6181553159d2ddc5c

--- js/src/patina/pages/community/Community.page.tsx
+++ js/src/patina/pages/community/Community.page.tsx
@@ -8,7 +8,9 @@ import styles from './Community.module.css'
 export function CommunityPage() {
   const message = (
     <Text className={styles.description}>
-      {"Patina Network's community events include ..."}
+      {
+        '“Patina Network’s community consists of the wider Asian American, Native Hawaiian, and Pasifika people and their Allies. All the people who want to join to celebrate, empower and educate are welcome to come. Our events are designed to bring people together, foster meaningful connections, and create positive impact. Whether you’re passionate about our cause, looking to get involved, or simply seeking a way to meet people and have fun, we invite you to explore our upcoming events and join us.'
+      }
     </Text>
   )
   return (
