b7a95ab45ad763788f91601fcf0f0910db7d36cd
Kelly Lin <linkelly150@gmail.com>
2024-08-09 12:54:20 -0400

    PatNet: Created BlogTag controller, repo, PSQL repo
    Functions are a work in progress.
    
    Change-Id: I397e9c27494df4b7a59754d74acd04606594e738

---
+++ src/main/java/org/patinanetwork/patinawebsite/blogtag/repo/BlogTagRepo.java
@@ -0,0 +1,16 @@
+package org.patinanetwork.patinawebsite.blogtag.repo;
+
+import org.patinanetwork.patinawebsite.blogtag.protos.Blogtag;
+
+import java.util.List;
+
+public interface BlogTagRepo {
+    // Add a new blog
+    Blogtag addBlogtag(Blogtag blogtag);
+
+    Blogtag getBlogtag(int blogtagId);
+
+    // Get a blog by its ID
+    Blogtag getBlogtagById(int blogtagId);
+
+}
