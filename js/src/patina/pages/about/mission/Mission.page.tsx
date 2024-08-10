79b56b8a29a05802bffeb666340edec6ad5d859f
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-08-10 14:49:12 -0400

    PatNet: Fix layout for statement section of mission page
    L=ENG-440
    
    missionStatementdesktop.png: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-10T05:26:41-MReyes1241-missionStatementdesktop.png
    
    missionStatementphone.png: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-10T05:28:38-MReyes1241-missionStatementphone.png
    
    Change-Id: I8fb51e7cafe914478d9d7546c4240baf901008ab

--- js/src/patina/pages/about/mission/Mission.page.tsx
+++ js/src/patina/pages/about/mission/Mission.page.tsx
@@ -10,61 +10,80 @@ export function MissionPage() {
         <Image
           src={imageUrls.missionBannerSmile.src}
           alt={imageUrls.missionBannerSmile.alt}
-        />
-      </div>
-      <Space h={'md'} />
-
-      <Title className={styles.title}>{'Our Vision'}</Title>
-      <div>
-        <Text size={'xl'}>
-          {'Our vision is to see AANHPI women leading in every field, from politics and business to arts and sciences,' +
-            ' contributing to a more equitable, inclusive, and vibrant world.'}
-        </Text>
-      </div>
-      <Space h="md" />
-      <Title className={styles.title}>{'Our Mission'}</Title>
-      <div>
-        <Text size={'xl'}>
-          {'At Patina Network, our mission is to celebrate, empower, educate on the race, culture, and ethnicity of' +
-            ' AANHPI women, and champion allyship within the community.'}
-        </Text>
-      </div>
-
-      <Space h="md" />
-      <Title className={styles.title}>{'Why “Patina Network”?'}</Title>
-      <div>
-        <Text size={'xl'}>
-          {'A patina is the result of impressions over time. It represents how one can be shaped and enriched by' +
-            ' their unique circumstances, collectively creating a pattern that is distinctive, rich, and celebrated. '}
-        </Text>
-      </div>
-
-      <div>
-        <Image
-          src={imageUrls.missionKnife.src}
-          alt={imageUrls.missionKnife.alt}
+          height={'auto'}
+          width={'100%'}
         />
       </div>
 
-      <Space h="md" />
-      <Title className={styles.title}>{'How it started'}</Title>
-      <div>
-        <Text size={'xl'}>
-          {'After meeting serendipitously at a networking event, our founders bonded over their shared experience' +
-            ' of struggling with lack of mentorship and resources during critical years of their education.'}
-          <br />
+      <Space h={'lg'} />
 
-          {'They decided to do something about it! We are now unified under the name Patina Network, and we strive' +
-            ' to empower future generations of students, foster a safe safe with peers, and engage elders who' +
-            ' overcame similar challenges. '}
-        </Text>
+      <div className={styles.statementContainer}>
+        <div>
+          <Title order={4} className={styles.title}>
+            {'Our Vision'}
+          </Title>
+          <div>
+            <Text>
+              {'Our vision is to see AANHPI women leading in every field, from politics and business to arts and sciences,' +
+                ' contributing to a more equitable, inclusive, and vibrant world.'}
+            </Text>
+          </div>
+        </div>
+        <Space w="xl" />
+        <div>
+          <Title order={4} className={styles.title}>
+            {'Our Mission'}
+          </Title>
+          <div>
+            <Text>
+              {'At Patina Network, our mission is to celebrate, empower, educate on the race, culture, and ethnicity of' +
+                ' AANHPI women, and champion allyship within the community.'}
+            </Text>
+          </div>
+        </div>
       </div>
+
       <Space h="md" />
+
       <div>
-        <Image
-          src={imageUrls.missionInterns24.src}
-          alt={imageUrls.missionInterns24.alt}
-        />
+        <Title className={styles.title}>{'Why “Patina Network”?'}</Title>
+        <div>
+          <Text size={'xl'}>
+            {'A patina is the result of impressions over time. It represents how one can be shaped and enriched by' +
+              ' their unique circumstances, collectively creating a pattern that is distinctive, rich, and celebrated. '}
+          </Text>
+        </div>
+
+        <div>
+          <Image
+            src={imageUrls.missionKnife.src}
+            alt={imageUrls.missionKnife.alt}
+          />
+        </div>
+      </div>
+
+      <Space h="md" />
+
+      <div>
+        <Title className={styles.title}>{'Our story'}</Title>
+        <div>
+          <Text size={'xl'}>
+            {'After meeting serendipitously at a networking event, our founders bonded over their shared experience' +
+              ' of struggling with lack of mentorship and resources during critical years of their education.'}
+            <br />
+
+            {'They decided to do something about it! We are now unified under the name Patina Network, and we strive' +
+              ' to empower future generations of students, foster a safe safe with peers, and engage elders who' +
+              ' overcame similar challenges. '}
+          </Text>
+        </div>
+        <Space h="md" />
+        <div>
+          <Image
+            src={imageUrls.missionInterns24.src}
+            alt={imageUrls.missionInterns24.alt}
+          />
+        </div>
       </div>
     </ContentPage>
   )
