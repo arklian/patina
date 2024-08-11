e0dbf3ea83e4fe02b8f45b4662beb4d04969d1e4
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-08-11 16:55:23 +0000

    PatNet: Add pillars section to the mission page
    L=ENG-440
    
    pillarsMobile.png: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-11T00:27:35-MReyes1241-pillarsMobile.png
    
    pillarsWeb.png: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-11T00:43:13-MReyes1241-pillarsWeb.png
    
    Change-Id: Id7a49302283afff07a9e837e35e9dd08e3fb0d51

--- js/src/patina/pages/about/mission/Mission.page.tsx
+++ js/src/patina/pages/about/mission/Mission.page.tsx
@@ -2,6 +2,7 @@ import { Title, Space, Text, Image } from '@mantine/core'
 import { imageUrls } from '@/patina/assets/images.ts'
 import styles from './Mission.module.css'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import { PillarSection } from '@/patina/pages/about/mission/PillarSection.tsx'
 
 export function MissionPage() {
   return (
@@ -42,6 +43,9 @@ export function MissionPage() {
           </div>
         </div>
       </div>
+
+      <Space h={'xl'} />
+      <PillarSection />
 
       <Space h="xl" />
 
