a264c1c02ecae5aeb0da4c1f726c0f8de216c68d
Haoking Luo <hao5k6032@gmail.com>
2024-08-12 18:01:58 -0400

    Blog: Add onClick functionality, More Posts sxn
    Refactored Blog type into its own .ts file. Refactored ImageCard to take
    in a Blog object rather than all a blog's fields. Refactored BlogRoutes
    into PatinaRoutes rather than the top level Routes. ImageCard is now
    wrapped inside an UnstyledButton and onClick will navigate to the
    blog page showing that specific blog. Made FocusedBlog component run
    useEffect on id route param change, add another useQuery to get allBlogs
    to show in More Posts section, make useEffect refetch to blog GET
    endpoint with the id of the blog you clicked on.
    
    https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-12T20:59:05-HaokingLuo-helpme.png
    
    L=ENG-438
    TEST=manual
    - Ran gw bootRun and navigate to Blog page
    - Clicked on a blog, displays all its info
    - Clicked on another blog in More Posts section, new blog's info shown
    
    Change-Id: I256ad857040c055ca25da9719a3467edc7a6f134

--- js/src/patina/pages/blog/ImageCard.tsx
+++ js/src/patina/pages/blog/ImageCard.tsx
@@ -1,4 +1,6 @@
-import { Image, Pill } from '@mantine/core'
+import { useNavigate } from 'react-router-dom'
+import { Image, Pill, UnstyledButton } from '@mantine/core'
+import { Blog } from './blog.ts'
 import styles from './ImageCard.module.css'
 
 /**
@@ -6,25 +8,18 @@ import styles from './ImageCard.module.css'
  */
 type ImageCardProps = {
   horizontal: boolean
-  title: string
-  content: string
+  blog: Blog
   tags: string[]
-  image: string
 }
 
-export function ImageCard({
-  horizontal,
-  title,
-  content,
-  tags,
-  image,
-}: ImageCardProps) {
+export function ImageCard({ horizontal, blog, tags }: ImageCardProps) {
+  const navigate = useNavigate()
   let contentSubstring = ''
   let JSONParsable = false
   // Content should be a JSON.stringify'd string
   // Convert content back to JSON object for accessing innermost text to display in brief
   try {
-    const contentJSON = JSON.parse(content)
+    const contentJSON = JSON.parse(blog.content)
     const innermostTextFields = contentJSON.content[0].content[0]
     contentSubstring = innermostTextFields.text.substring(0, 120)
     JSONParsable = true
@@ -32,27 +27,42 @@ export function ImageCard({
     console.log(e)
   }
   // Remove this once all existing blogs in SQL blog table have valid JSON parsable content column
-  if (!JSONParsable) contentSubstring = content.substring(0, 120)
+  if (!JSONParsable) contentSubstring = blog.content.substring(0, 120)
 
   return (
-    <div className={styles.border}>
-      <div className={horizontal ? styles.cardHorizontal : styles.cardVertical}>
-        <Image
-          className={horizontal ? styles.imageHorizontal : styles.imageVertical}
-          src={image === '' ? 'https://placehold.co/600x400' : image}
-        />
-        <div className={styles.textSection}>
-          <div className={styles.textTitle}>{title}</div>
-          <div className={styles.textDesc}>{contentSubstring}</div>
-          <div className={styles.pillsSection}>
-            {tags.map((tag, index: number) => (
-              <Pill key={index} className={styles.pill}>
-                {tag}
-              </Pill>
-            ))}
+    <UnstyledButton
+      onClick={() => {
+        console.log(`Navigating to blog ${blog.id}`)
+        navigate(`/blog/${blog.id}`)
+      }}
+    >
+      <div className={styles.border}>
+        <div
+          className={horizontal ? styles.cardHorizontal : styles.cardVertical}
+        >
+          <Image
+            className={
+              horizontal ? styles.imageHorizontal : styles.imageVertical
+            }
+            src={
+              blog.image.substring(0, 5) !== 'https' ?
+                'https://placehold.co/600x400'
+              : blog.image
+            }
+          />
+          <div className={styles.textSection}>
+            <div className={styles.textTitle}>{blog.title}</div>
+            <div className={styles.textDesc}>{contentSubstring}</div>
+            <div className={styles.pillsSection}>
+              {tags.map((tag, index: number) => (
+                <Pill key={index} className={styles.pill}>
+                  {tag}
+                </Pill>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </UnstyledButton>
   )
 }
---
+++ js/src/patina/pages/blog/blog.ts
@@ -0,0 +1,8 @@
+export type Blog = {
+  id: number
+  author: string
+  title: string
+  createTime: string
+  content: string
+  image: string
+}
