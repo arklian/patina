22ad65ba6669422a3c8f347a8628681f1dfaa58c
Haoking Luo <hao5k6032@gmail.com>
2024-08-12 16:59:15 -0400

    Blog: Add onClick functionality, More Posts sxn
    Refactored Blog type into its own .ts file. Refactored ImageCard to take
    in a Blog object rather than all a blog's fields. Refactored BlogRoutes
    into PatinaRoutes rather than the top level Routes. ImageCard is now
    wrapped inside an UnstyledButton and onClick will navigate to the
    blog page showing that specific blog. Made FocusedBlog component run
    useEffect on id route param change, add another useQuery to get allBlogs
    to show in More Posts section, make useEffect refetch to blog GET
    endpoint with the id of the blog you clicked on.
    
    https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-12T20:59:05-HaokingLuo-helpme.png
    
    L=ENG-438
    TEST=manual
    - Ran gw bootRun and navigate to Blog page
    - Clicked on a blog, displays all its info
    - Clicked on another blog in More Posts section, new blog's info shown
    
    Change-Id: I256ad857040c055ca25da9719a3467edc7a6f134

--- js/src/patina/pages/blog/FocusedBlog.tsx
+++ js/src/patina/pages/blog/FocusedBlog.tsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
-import { Image, Divider } from '@mantine/core'
+import { Image, Divider, Pill } from '@mantine/core'
 import { RichTextEditor } from '@mantine/tiptap'
 import { useEditor } from '@tiptap/react'
 import { useEffect } from 'react'
 import StarterKit from '@tiptap/starter-kit'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import { ImageCard } from './ImageCard.tsx'
 import styles from './FocusedBlog.module.css'
 
 /**
@@ -13,7 +14,7 @@ import styles from './FocusedBlog.module.css'
  */
 export function FocusedBlog() {
   const { id } = useParams()
-  const { data, status } = useQuery({
+  const { data, status, refetch } = useQuery({
     queryKey: ['blog'],
     queryFn: async () => {
       const response = await fetch(`/api/blog/${id}`)
@@ -25,6 +26,18 @@ export function FocusedBlog() {
   })
   const blog = data?.blog
 
+  const { data: dataListAll, status: statusListAll } = useQuery({
+    queryKey: ['blogs'],
+    queryFn: async () => {
+      const response = await fetch(`/api/blogs`)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      return response.json()
+    },
+  })
+
+  const allBlogs = dataListAll?.blogs
   const editor = useEditor({
     extensions: [StarterKit],
     content: 'Loading this blog',
@@ -32,27 +45,43 @@ export function FocusedBlog() {
   })
 
   useEffect(() => {
+    const newResponse = refetch()
+    newResponse.then((value) => {
+      editor?.commands.setContent(JSON.parse(value.data.blog.content))
+    })
     if (status === 'success') {
       editor?.commands.setContent(JSON.parse(blog.content))
     }
-  }, [status])
+  }, [status, id])
 
   return status === 'success' ?
       <ContentPage>
         <Image src={blog.image} />
-        <div className={styles.pillsSection} />
         <div className={styles.blogSection}>
+          <div className={styles.pillsSection}>
+            <Pill className={styles.pill}>{'Student Spotlight'}</Pill>
+            <Pill className={styles.pill}>{'Summer 2024'}</Pill>
+          </div>
           <div className={styles.titleText}>{blog.title}</div>
           <Divider color={'white'} />
           <RichTextEditor
             classNames={{
               root: styles.editorRoot,
-              content: styles.editorContent,
             }}
             editor={editor}
           >
             <RichTextEditor.Content />
           </RichTextEditor>
+          <Divider color={'white'} />
+        </div>
+        <div className={styles.morePostsSection}>
+          <div className={styles.morePostsTitle}>{'More posts'}</div>
+          {statusListAll === 'success' ?
+            <div className={styles.morePostsPosts}>
+              <ImageCard horizontal={false} blog={allBlogs[5]} tags={['']} />
+              <ImageCard horizontal={false} blog={allBlogs[6]} tags={['']} />
+            </div>
+          : <div>{'Loading more blogs...'}</div>}
         </div>
       </ContentPage>
     : <div>{'Loading this blog'}</div>
