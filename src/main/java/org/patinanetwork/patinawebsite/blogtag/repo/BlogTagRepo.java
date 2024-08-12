4c66297ee87006f79f5383dca3162c76ef523652
Kelly Lin <linkelly150@gmail.com>
2024-08-12 15:55:07 -0400

    PatNet: Created BlogTag controller, repo, PSQL repo
    Implemented functions for add, get, delete, and list.
    
    Change-Id: I397e9c27494df4b7a59754d74acd04606594e738

---
+++ src/main/java/org/patinanetwork/patinawebsite/blogtag/repo/BlogTagRepo.java
@@ -0,0 +1,19 @@
+package org.patinanetwork.patinawebsite.blogtag.repo;
+
+import org.patinanetwork.patinawebsite.blogtag.protos.Blogtag;
+
+import java.util.List;
+
+public interface BlogtagRepo {
+    // Add a new blogtag
+    Blogtag addBlogtag(Blogtag blogtag);
+
+    // Get a blogtag by its ID
+    Blogtag getBlogtagById(int blogtagId);
+
+    // Delete a blogtag by its ID
+    Blogtag deleteBlogtag(int blogtagId);
+
+    // List all blogs
+    List<Blogtag> listAllBlogtag();
+}
