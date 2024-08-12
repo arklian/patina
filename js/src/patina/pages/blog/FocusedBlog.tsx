c0f24bfc715940c5eb61eb6a80c36a11f064ef4b
Haoking Luo <hao5k6032@gmail.com>
2024-08-11 22:16:27 -0400

    Blog: Add RichTextEditor to FocusedBlog component
    Modified FocusedBlog to now include a read-only Rich Text Editor that
    can properly display the styles of the stored blog content.
    
    https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-12T02:16:14-HaokingLuo-focusedblog2.png
    
    L=ENG-438
    TEST=manual
    - Ran gw bootRun and blog content displays properly on test blog.
    
    Change-Id: I0101ee68324d8a33d04baf3e931d135b28879e6f

--- js/src/patina/pages/blog/FocusedBlog.tsx
+++ js/src/patina/pages/blog/FocusedBlog.tsx
@@ -1,6 +1,10 @@
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
-import { Image } from '@mantine/core'
+import { Image, Divider } from '@mantine/core'
+import { RichTextEditor } from '@mantine/tiptap'
+import { useEditor } from '@tiptap/react'
+import { useEffect } from 'react'
+import StarterKit from '@tiptap/starter-kit'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
 import styles from './FocusedBlog.module.css'
 
@@ -19,16 +23,35 @@ export function FocusedBlog() {
       return response.json()
     },
   })
-
   const blog = data?.blog
+
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: 'Loading this blog',
+    editable: false,
+  })
+
+  useEffect(() => {
+    if (status === 'success')
+      editor?.commands.setContent(JSON.parse(blog.content))
+  }, [status])
 
   return status === 'success' ?
       <ContentPage>
         <Image src={blog.image} />
+        <div className={styles.pillsSection} />
         <div className={styles.blogSection}>
-          <div>{`Title: ${blog.title}`}</div>
-          <div>{`Author: ${blog.author}`}</div>
-          <div>{`Create_Time: ${blog.createTime}`}</div>
+          <div className={styles.titleText}>{blog.title}</div>
+          <Divider color={'white'} />
+          <RichTextEditor
+            classNames={{
+              root: styles.editorRoot,
+              content: styles.editorContent,
+            }}
+            editor={editor}
+          >
+            <RichTextEditor.Content />
+          </RichTextEditor>
         </div>
       </ContentPage>
     : <div>{'Loading this blog'}</div>
