ca5be3a7ae07f92e36a80b4e696429a4ee672c9a
Tony Wang <wangtony4005@gmail.com>
2024-08-12 13:09:53 -0400

    Secrets: updated dummy secrets.sh
    Change-Id: Ic47ad76f1c8e334d40b2a102182f2789f3b6f1f5

--- secrets.sh
+++ secrets.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-# Access Digital Ocean Spatial Storage Secret Key - Manny
-export DIGITAL_OCEAN_SPACES_SECRET=redacted
-# Access Digital Ocean Spatial Storage Access Key - Manny
-export DIGITAL_OCEAN_SPACES_ACCESS=redacted
-# Host Code for Database - Bryan
-export DATABASE_URL=redacted
-# Host Username for Database - Bryan
-export DATABASE_USER=redacted
-# Host Password for Database - Bryan (Most important one!)
-export DATABASE_PASSWORD=redacted
+# Access Digital Ocean Spacial Storage Secret Key - Manny
+export DIGITAL_OCEAN_SPACES_SECRET=REDACTED
+# Access Digital Ocean Spacial Storage Access Key - Manny
+export DIGITAL_OCEAN_SPACES_ACCESS=REDACTED
+if [ "$1" = "prod" ]; then
+  # Host key for prod Database - Tony
+  export DATABASE_HOST=REDACTED
+  # Port number for prod Database Connection Pool - Tony
+  export DATABASE_PORT=REDACTED
+  # Database Name for prod Database - Tony
+  export DATABASE_DB=REDACTED
+  # Username for prod Database - Tony
+  export DATABASE_USER=REDACTED
+  # Password for prod Database - Tony
+  export DATABASE_PASSWORD=REDACTED
+else
+  # Host Key for Dev Database - Bryan
+  export DATABASE_HOST=REDACTED
+  # Port Number for Dev Database Connection Pool - Bryan
+  export DATABASE_PORT=REDACTED
+  # Database Name for Dev Database - Bryan
+  export DATABASE_DB=REDACTED
+  # Username for Dev Database - Bryan
+  export DATABASE_USER=REDACTED
+  # Password for Dev Database - Bryan (Most important one!)
+  export DATABASE_PASSWORD=REDACTED
+fi
 # Password for Admin Page - Haoking
-export PATINA_ADMIN_PASSWORD=redacted
+export PATINA_ADMIN_PASSWORD=REDACTED
 # Cookie for successful admin login - Haoking
-export PATINA_ADMIN_COOKIE=redacted
+export PATINA_ADMIN_COOKIE=REDACTED
 # Credentials for pushing Docker image to Digital Ocean
-export DO_DOCKER_IMAGE_UPLOAD_PAT=redacted
+export DO_DOCKER_IMAGE_UPLOAD_PAT=REDACTED
+# Credentials for Google API to manipulate drive folder - Manny
+export GOOGLE_CLIENT_ID=REDACTED
+export GOOGLE_CLIENT_SECRET=REDACTED
+export GOOGLE_REFRESH_TOKEN=REDACTED
+# Bryan's OpenAI API secret
+export CHATGPT_SECRET=REDACTED
