88445e6c1aaadc8ca30775c784d9b3098771c9d4
Riyuan Liu <riyusnliu@gmail.com>
2024-08-13 20:40:31 -0400

    FormLog: Setup the Backend for form login for spring boot
    Configured Spring Security to handle form login. The login form now posts to /login instead of /api/login. Used environment variables for admin password configuration.
    
    Remove the Login Controller, since it not needed no more
    
    Need to send back the cookie, however, I don't think we need the cookies no more if we setup the securityConfig correctly
    
    TEST=manual
    -login with form work
    
    Change-Id: Ia8b39aa65857d5f186c60dc65759e438114e6770

--- src/main/java/org/patinanetwork/patinawebsite/auth/config/SecurityConfig.java
+++ src/main/java/org/patinanetwork/patinawebsite/auth/config/SecurityConfig.java
@@ -1,16 +1,23 @@
 package org.patinanetwork.patinawebsite.auth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${PATINA_ADMIN_PASSWORD}")
+    private String ADMIN_PASSWORD;
 
     private final AuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
@@ -19,17 +26,34 @@ public class SecurityConfig {
     }
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // TODO: Add back CSRF protection
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/secured").hasRole("ADMIN");
-                    auth.anyRequest().permitAll();
-                })
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin", "/admin/**").hasRole("ADMIN")
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                        .defaultSuccessUrl("/admin", true)
+                        .failureUrl("/login?error")
+                )
                 .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/oauth2")
+                        .loginPage("/login")
                         .successHandler(customAuthenticationSuccessHandler)
-                        .failureUrl("/oauth2")
+                        .failureUrl("/login")
                 );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User
+                .withUsername("admin")
+                .password("{noop}"+ ADMIN_PASSWORD)
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
