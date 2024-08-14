819049cec3111c33b89470209aabd38f368e288a
Jionghao Wu <jhwu03@gmail.com>
2024-08-13 22:15:38 -0400

    Jionghao: Add mobile menu for header
    TEST=manual
    ran locally and worked
    
    Change-Id: I842f7535f136757718e916490a1dec4499f8a49e

--- js/src/personal/jionghao/components/HoverButton.tsx
+++ js/src/personal/jionghao/components/HoverButton.tsx
@@ -8,6 +8,20 @@ export function HoverButton(props: any) {
     hovered,
     ref,
   }: { hovered: boolean; ref: RefObject<HTMLAnchorElement> } = useHover()
+
+  const handleClick = (
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    event.preventDefault()
+    const section = document.querySelector(props.buttonLink)
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' })
+    }
+    if (props.onClick) {
+      props.onClick()
+    }
+  }
+
   return (
     <Button
       ref={ref}
@@ -17,6 +31,8 @@ export function HoverButton(props: any) {
       component="a"
       href={props.buttonLink}
       size={'md'}
+      rightSection={props.right}
+      onClick={handleClick}
     >
       {props.buttonName}
     </Button>
