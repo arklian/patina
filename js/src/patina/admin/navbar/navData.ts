7a00486a0283763d376c4e546e85ce1691468ae1
Riyuan Liu <riyusnliu@gmail.com>
2024-08-15 00:07:49 -0400

    Admin: Modify the admin page to look better
    - Added some padding-bottom to login
    - Make login Container smaller
    - Make Blog and Event use Container Instead of ConentPage
    
    TEST=manual
    - Page load locally
    
    Change-Id: Ia3dcc52b6f905a6ec23fbb7a5188276911822665

--- js/src/patina/admin/navbar/navData.ts
+++ js/src/patina/admin/navbar/navData.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { label: 'Patina Network', src: '/' },
+  { label: 'Dashboard', src: '/admin' },
   { label: 'Blog', src: '/admin/blog' },
   { label: 'Event', src: '/admin/event' },
-  { label: 'Dashboard', src: '/admin' },
 ]
