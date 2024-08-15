a493f3d99203702b32e87b7e71ac01f236bfdad1
Riyuan Liu <riyusnliu@gmail.com>
2024-08-15 02:39:45 -0400

    Service: Notifications Edit Events and Blogs
    Implemented user notifications for the following actions:
    - Submitting a blog
    - Submitting an event
    - Deleting an event
    
    - These notifications provide immediate feedback to users
    - Enhancing the user experience by confirming successful Modification.
    
    TEST=manual
    - Manually tested each action, confirming notifications are displayed.
    
    Change-Id: I296b05605ee788d64ec3095e850a034be35e674d

--- js/src/patina/admin/AdminMain.page.tsx
+++ js/src/patina/admin/AdminMain.page.tsx
@@ -1,10 +1,12 @@
 import { MantineProvider } from '@mantine/core'
-import { Admin } from '@/patina/admin/Admin.tsx' // Adjust the import path as needed
+import { Notifications } from '@mantine/notifications'
+import { Admin } from '@/patina/admin/Admin.tsx'
 import { resolver } from '@/patina/theme.ts'
 
 export function AdminMainPage() {
   return (
     <MantineProvider cssVariablesResolver={resolver} forceColorScheme="dark">
+      <Notifications />
       <Admin />
     </MantineProvider>
   )
