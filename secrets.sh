e12aab2fc5bdfa1a3ba04c77bbe4aa4b283f1a02
Riyuan Liu <riyusnliu@gmail.com>
2024-08-15 00:05:44 -0400

    OAuth: Finalize OAuth Backend Logic and User Handling
    Completed the backend logic for OAuth integration, including user and
    admin table management. Implemented OAuth sign-in functionality which
    invokes `CustomAuthenticationSuccessHandler` upon successful login.
    
    The `CustomAuthenticationSuccessHandler` now:
    - Retrieves email and username from OAuth2 user attributes.
    - Handles cases where email is missing by redirecting to an error page.
    - Checks if the user exists in the database and creates a new user if not.
    - Determines user role (ADMIN or USER) and updates authorities.
    - Redirects users based on their role: admins are redirected to `/api/secured`,
      while regular users are redirected to `/`.
    
    TEST=manual
    - Verified OAuth login flow and redirection based on user roles.
    - Tested login with both admin and non-admin users to confirm appropriate security measures.
    - Ensured error handling for missing email and correct role assignment.
    
    Change-Id: Ic76b08a9b514def477b1f299a6641d481b7de061

--- secrets.sh
+++ secrets.sh
@@ -15,6 +15,14 @@ if [ "$1" = "prod" ]; then
   export DATABASE_USER=REDACTED
   # Password for prod Database - Tony
   export DATABASE_PASSWORD=REDACTED
+  # Client ID for Github - Riyuan
+  export GITHUB_OAUTH_CLIENT_ID=redacted
+  # Client Secret for Github - Riyuan
+  export GITHUB_OAUTH_CLIENT_SECRET=redacted
+  # Client ID for Google - Riyuan
+  export GOOGLE_OAUTH_CLIENT_ID=redacted
+  # Client Secret for Google - Riyuan
+  export GOOGLE_OAUTH_CLIENT_SECRET=redacted
 else
   # Host Key for Dev Database - Bryan
   export DATABASE_HOST=REDACTED
@@ -26,16 +34,17 @@ else
   export DATABASE_USER=REDACTED
   # Password for Dev Database - Bryan (Most important one!)
   export DATABASE_PASSWORD=REDACTED
+  # Client ID for Github - Riyuan
+  export GITHUB_OAUTH_CLIENT_ID=redacted
+  # Client Secret for Github - Riyuan
+  export GITHUB_OAUTH_CLIENT_SECRET=redacted
+  # Client ID for Google - Riyuan
+  export GOOGLE_OAUTH_CLIENT_ID=redacted
+  # Client Secret for Google - Riyuan
+  export GOOGLE_OAUTH_CLIENT_SECRET=redacted
 fi
 # Password for Admin Page - Haoking
 export PATINA_ADMIN_PASSWORD=REDACTED
 # Cookie for successful admin login - Haoking
 export PATINA_ADMIN_COOKIE=REDACTED
 # Credentials for pushing Docker image to Digital Ocean
-export DO_DOCKER_IMAGE_UPLOAD_PAT=REDACTED
-# Credentials for Google API to manipulate drive folder - Manny
-export GOOGLE_CLIENT_ID=REDACTED
-export GOOGLE_CLIENT_SECRET=REDACTED
-export GOOGLE_REFRESH_TOKEN=REDACTED
-# Bryan's OpenAI API secret
-export CHATGPT_SECRET=REDACTED
