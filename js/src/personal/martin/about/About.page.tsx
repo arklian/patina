d9bf682197495959f9eda53b9a31c4955fc5ddf7
Martin M <m2msquared47@gmail.com>
2024-08-15 18:48:38 -0700

    Martin: Update data.json
    Exported data from main and about pages to the data.json file. All
    large blocks of text have been removed from main and about for code
    readability.
    
    TEST=manual
    Ran prt, no errors
    
    Change-Id: I54a4d45ba031bd0d6da4ca338178212525acd6c7

--- js/src/personal/martin/about/About.page.tsx
+++ js/src/personal/martin/about/About.page.tsx
@@ -2,49 +2,15 @@ import { Accordion, Container, Group, Text } from '@mantine/core'
 import { SoundCloudPlayer } from '@/personal/martin/components/SoundCloudPlayer.tsx'
 import { Jumbotron } from '@/personal/martin/components/Jumbotron.tsx'
 import { theme } from '@/personal/martin/theme.ts'
+import { aboutData } from '@/personal/martin/data.json'
 import classes from '@/personal/martin/Martin.module.css'
 
-const accordionList = [
-  {
-    id: 'life',
-    label: 'Life Stuff',
-    desc: 'My background',
-    content:
-      'I am a Filipino/Japanese American born in Fresno, CA. I lived 8 years in Pewaukee, WI, then moved to Seattle,' +
-      ' WA in late 2016. I began my interest in computers and programming in middle school when I joined a LEGO/VEX' +
-      ' robotics team where I became the head programmer in my third year of joining.',
-  },
-  {
-    id: 'edu',
-    label: 'Education',
-    desc: 'My path as a growing software engineer',
-    content:
-      "I graduated from the University of Washington-Bothell in the Fall of '24 with a BS of Science - CSSE." +
-      ' During my tenure at UW-Bothell, I had many opportunities to learn various interesting topics such as' +
-      ' Game Engine and API development, CyberSecurity, User Centered Design, and more. I have also been an active' +
-      ' member in both the Filipino American Student Association (FASA) and Brilliant Billiards Club (BBC) where I' +
-      " served as club president from 2021-23. I have also graduated from O'Dea High School in Seattle, WA.",
-  },
-  {
-    id: 'exp',
-    label: 'Experience',
-    desc: 'My work background',
-    content:
-      'I have worked as a Team Manager at Grocery Outlet for three years where I learned multiple skills such' +
-      ' as communication, customer service, leadership, and team coordination. I am currently an SWE intern at ' +
-      'Patina Network learning professional project and full-stack development while also expanding my network,' +
-      ' and preparing myself for job searching and technical interviews.',
-  },
-  {
-    id: 'hobby',
-    label: 'Hobbies',
-    desc: 'How I spend my free time',
-    content:
-      'In my free time, I enjoy listening and making music. I listen to all genres of music and typically' +
-      ' create cinematic future bass or house beats. I also participate in both casual and competitive sim racing' +
-      ' in the  MX-5 cup or GT2/GT3 divisions.',
-  },
-]
+type aboutList = {
+  id: string
+  label: string
+  desc: string
+  content: string
+}
 
 interface LabelProps {
   label: string
@@ -65,7 +31,7 @@ function AccordionLabel({ label, desc }: LabelProps) {
 }
 
 export function MartinAbout() {
-  const items = accordionList.map((item) => (
+  const items = aboutData.map((item: aboutList) => (
     <Accordion.Item value={item.id} key={item.label}>
       <Accordion.Control className={classes.accordionLabel}>
         <AccordionLabel {...item} />
