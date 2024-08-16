6d829ae43c90d9184135c7b32279d4fafe8ab8b4
Bryan Zhang <bz5989@princeton.edu>
2024-08-16 15:18:47 -0400

    Blogs: Separate out CreateBlog
    Change-Id: Ifd0d23e4c97b2160b9c447a87a2d3d1a2d76a60d

---
+++ src/main/java/org/patinanetwork/patinawebsite/blogs/ops/CreateOp.java
@@ -0,0 +1,55 @@
+package org.patinanetwork.patinawebsite.blogs.ops;
+
+import org.patinanetwork.patinawebsite.blogs.protos.Blog;
+import org.patinanetwork.patinawebsite.blogs.protos.CreateBlogReq;
+import org.patinanetwork.patinawebsite.blogs.protos.CreateBlogResp;
+import org.patinanetwork.patinawebsite.blogs.repo.BlogsRepo;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Class to test listAllBlogs() method of BlogsRepo.
+ */
+public class CreateOp {
+    private final String author;
+    private final String title;
+    private final String content;
+    private final String image;
+    private final BlogsRepo repo;
+
+    public CreateOp(CreateBlogReq blogReq, BlogsRepo repo) {
+        this.author = blogReq.getAuthor();
+        this.title = blogReq.getTitle();
+        this.content = blogReq.getContent();
+        this.image = blogReq.getImage();
+        this.repo = repo;
+    }
+
+
+    public CreateBlogResp run() {
+        // Get current timestamp in the required format
+        String currentTimestamp = OffsetDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"));
+
+        // Create a new Blog instance with the current timestamp
+        Blog blog;
+        if (image.isEmpty())
+            blog = Blog.newBuilder()
+                    .setAuthor(author)
+                    .setTitle(title)
+                    .setContent(content)
+                    .setCreateTime(currentTimestamp)
+                    .setImage(image)
+                    .build();
+        else
+            blog = Blog.newBuilder()
+                    .setAuthor(author)
+                    .setTitle(title)
+                    .setCreateTime(currentTimestamp)
+                    .build();
+
+        // Add the blog to the repository
+        repo.addBlog(blog);
+        return CreateBlogResp.newBuilder().setBlog(blog).build();
+    }
+}
