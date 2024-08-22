49d5c931f388f800880b9ab58f28ca8b7d33702c
Kelly Lin <linkelly150@gmail.com>
2024-08-22 16:57:04 -0400

    Blog Tags: Create get blog tag function
    Can get blog tag item from id.
    
    TEST=manual
    
    gw bootRun and postman works.
    
    Change-Id: If5b81205f772c93716f3a1ef887d7331b79cc583

--- src/main/java/org/patinanetwork/patinawebsite/blogtag/repo/BlogtagRepo.java
+++ src/main/java/org/patinanetwork/patinawebsite/blogtag/repo/BlogtagRepo.java
@@ -6,5 +6,8 @@ public interface BlogtagRepo {
 
     // Add a new blogtag
     Blogtag addBlogtag(Blogtag blogtag);
+    
+    //Fetches blogtag item from Id
+    Blogtag getBlogtag(int blogtagId);
 
 }
