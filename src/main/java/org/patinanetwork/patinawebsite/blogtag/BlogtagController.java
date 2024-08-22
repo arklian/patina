0d0973fbcb28c1e6ee23793e152f2179f0864153
Kelly Lin <linkelly150@gmail.com>
2024-08-22 17:23:03 -0400

    Blog Tags: Created blog tag files
    Created and tested add function by name only.
    
    TEST=manual
    
    gw bootRun runs with no errors. Tested on postman
    and adds rows in PgAdmin
    
    Change-Id: I8ab13d7eba7feeedb2bb317c7d38cd53ba672aa7

---
+++ src/main/java/org/patinanetwork/patinawebsite/blogtag/BlogtagController.java
@@ -0,0 +1,41 @@
+package org.patinanetwork.patinawebsite.blogtag;
+
+import org.patinanetwork.common.protos.JsonParser;
+import org.patinanetwork.common.protos.JsonPrinter;
+import org.patinanetwork.patinawebsite.blogtag.protos.Blogtag;
+import org.patinanetwork.patinawebsite.blogtag.protos.CreateBlogtagReq;
+import org.patinanetwork.patinawebsite.blogtag.protos.CreateBlogtagResp;
+import org.patinanetwork.patinawebsite.blogtag.repo.BlogtagRepo;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class BlogtagController {
+
+    BlogtagRepo blogtagRepo;
+    JsonPrinter jsonPrinter;
+    JsonParser jsonParser;
+
+    public BlogtagController(BlogtagRepo blogtagRepo, JsonPrinter jsonPrinter, JsonParser jsonParser) {
+        this.blogtagRepo = blogtagRepo;
+        this.jsonPrinter = jsonPrinter;
+        this.jsonParser = jsonParser;
+    }
+
+
+    @PostMapping(value = "/api/blogtag/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String createBlogtag(@RequestBody String jsonRequest) {
+        CreateBlogtagReq blogtagReq = jsonParser.parse(jsonRequest, CreateBlogtagReq.newBuilder());
+        Blogtag blogtag = Blogtag.newBuilder()
+                .setName(blogtagReq.getBlogtag().getName())
+                .build();
+        Blogtag newBlogTag = blogtagRepo.createBlogtag(blogtag);
+        CreateBlogtagResp resp = CreateBlogtagResp.newBuilder().setBlogtag(newBlogTag).build();
+        return jsonPrinter.print(resp);
+    }
+}
+
