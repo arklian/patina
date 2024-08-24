d1b2ffc99fbfa5afee9dff43b0de957058914b3a
Martin M <m2msquared47@gmail.com>
2024-08-24 13:47:44 -0700

    AdminBlog: Add Pagination to DataTable
    Allow the 'Edit Blog' section to view all blogs by page. Lets the user
    select a page and set the number of blogs per page.
    
    TEST=manual
    Ran page locally to verify feature works
    
    Change-Id: I46980bc46582bc730e09145ac4e64428d53e0dc3

--- js/src/patina/admin/blog/EditBlog.tsx
+++ js/src/patina/admin/blog/EditBlog.tsx
@@ -1,17 +1,21 @@
-import { Stack, Title } from '@mantine/core'
+import { Loader, Stack, Title } from '@mantine/core'
 import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
 import { BlogDataTable } from './BlogDataTable.tsx'
 
 export function EditBlog() {
+  const [activePage, setActivePage] = useState(1)
+  const [limit, setLimit] = useState(10)
+
   const blogsResp = useQuery({
-    queryKey: ['blogs'],
+    queryKey: ['blogs', limit, activePage],
     queryFn: async () => {
       const response = await fetch('/api/blogs', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ limit: 10, page: 1 }),
+        body: JSON.stringify({ limit: Number(limit), page: activePage }),
       })
 
       if (!response.ok) {
@@ -24,7 +28,16 @@ export function EditBlog() {
   return (
     <Stack>
       <Title order={3}>{'Edit Blog'}</Title>
-      <BlogDataTable records={blogsResp.data?.blogs} />
+      {blogsResp.status === 'success' ?
+        <BlogDataTable
+          records={blogsResp.data?.blogs}
+          total={blogsResp.data?.total}
+          page={activePage}
+          setPage={setActivePage}
+          limit={limit}
+          setLimit={setLimit}
+        />
+      : <Loader />}
     </Stack>
   )
 }
