deb91f390588f6295c45b57f8f4c460fbd27c2e6
Martin M <m2msquared47@gmail.com>
2024-08-24 13:47:44 -0700

    AdminBlog: Extract EditBlog component
    Extracted components of EditBlog from AdminBlog.page to EditBlog
    component. Fetched blogs from the database to show the first 10 recent
    blogs in the data table.
    
    TEST=manual
    Ran page locally to verify feature works
    
    Change-Id: I399262c3b37c2f6c5ec6d08cc07dc2239b72b032

--- js/src/patina/admin/blog/BlogDataTable.tsx
+++ js/src/patina/admin/blog/BlogDataTable.tsx
@@ -1,15 +1,33 @@
 import { DataTable } from 'mantine-datatable'
 
+const PAGE_SIZES = [10, 15, 20]
+
 type BlogRowType = {
-  id: Number
+  id: number
   author: string
   title: string
   createTime: string
 }
-
-export function BlogDataTable({ records }: { records: BlogRowType[] }) {
+type BlogDataTableProps = {
+  records: BlogRowType[]
+  total: number
+  page: number
+  setPage: any
+  limit: number
+  setLimit: any
+}
+export function BlogDataTable({
+  records,
+  total,
+  page,
+  setPage,
+  limit,
+  setLimit,
+}: BlogDataTableProps) {
   return (
     <DataTable
+      withTableBorder
+      withColumnBorders
       columns={[
         { accessor: 'id' },
         { accessor: 'author' },
@@ -17,6 +35,12 @@ export function BlogDataTable({ records }: { records: BlogRowType[] }) {
         { accessor: 'createTime' },
       ]}
       records={records}
+      totalRecords={total}
+      recordsPerPage={limit}
+      page={page}
+      onPageChange={(p) => setPage(p)}
+      recordsPerPageOptions={PAGE_SIZES}
+      onRecordsPerPageChange={setLimit}
     />
   )
 }
---
+++ js/src/patina/admin/blog/EditBlog.tsx
@@ -0,0 +1,43 @@
+import { Loader, Stack, Title } from '@mantine/core'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+import { BlogDataTable } from './BlogDataTable.tsx'
+
+export function EditBlog() {
+  const [activePage, setActivePage] = useState(1)
+  const [limit, setLimit] = useState(10)
+
+  const blogsResp = useQuery({
+    queryKey: ['blogs', limit, activePage],
+    queryFn: async () => {
+      const response = await fetch('/api/blogs', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ limit: Number(limit), page: activePage }),
+      })
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`)
+      }
+      return response.json()
+    },
+  })
+
+  return (
+    <Stack>
+      <Title order={3}>{'Edit Blog'}</Title>
+      {blogsResp.status === 'success' ?
+        <BlogDataTable
+          records={blogsResp.data?.blogs}
+          total={blogsResp.data?.total}
+          page={activePage}
+          setPage={setActivePage}
+          limit={limit}
+          setLimit={setLimit}
+        />
+      : <Loader />}
+    </Stack>
+  )
+}
