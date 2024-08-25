17c02351dbf2a7834d72c064b13a8317b8360e41
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-08-25 01:23:07 -0400

    Manny: Add Header
    - Using the mantine simple header in my home screen
    
    Change-Id: Ib2d6f4172a106f8fd4ce1ce62ee748aacc2e7184

--- js/src/personal/manny/Manny.page.tsx
+++ js/src/personal/manny/Manny.page.tsx
@@ -2,11 +2,13 @@ import { MantineProvider } from '@mantine/core'
 // import styles from './Manny.module.css'
 import { theme } from './theme.ts'
 import { Hero } from '@/personal/manny/pages/Hero.tsx'
+import { HeaderSimple } from '@/personal/manny/components/HeaderSimple.tsx'
 
 export function MannyPage() {
   return (
     <MantineProvider theme={theme}>
       <Hero />
+      <HeaderSimple />
     </MantineProvider>
   )
 }
