25474ceaee3a91211d75de7f9b44671f4c2e6404
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-08-25 00:38:36 -0400

    Manny: redesign landing page
    - made the home into a hero section
    
    mannyHero.png: https://patina-dev.nyc3.digitaloceanspaces.com/screenshot/Screenshot-2024-08-25T04:38:20-MReyes1241-mannyHero.png
    
    Change-Id: If9a0f33173db3ef3daeb0bb9811ef89aca43369f

--- js/src/personal/manny/Manny.page.tsx
+++ js/src/personal/manny/Manny.page.tsx
@@ -1,25 +1,12 @@
-import { Title, Text, MantineProvider } from '@mantine/core'
-import { Outlet } from 'react-router-dom'
-import styles from './Manny.module.css'
+import { MantineProvider } from '@mantine/core'
+// import styles from './Manny.module.css'
 import { theme } from './theme.ts'
-import { FooterSocial } from '@/personal/manny/components/FooterSocial.tsx'
+import { Hero } from '@/personal/manny/pages/Hero.tsx'
 
 export function MannyPage() {
   return (
     <MantineProvider theme={theme}>
-      <Title className={styles.title} ta="center" mt={100}>
-        {'Welcome to '}
-        <Text
-          inherit
-          variant="gradient"
-          component="span"
-          gradient={{ from: 'white', to: 'blue' }}
-        >
-          {"Manny's Page"}
-        </Text>
-      </Title>
-      <FooterSocial />
-      <Outlet />
+      <Hero />
     </MantineProvider>
   )
 }
