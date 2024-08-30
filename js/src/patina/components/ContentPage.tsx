3d2b3bb5d5f922f7f19a6f5dc3b153eb866f34cd
Henry Chen <hjc77@cornell.edu>
2024-08-30 17:01:48 -0400

    ContentPage: Allow transition to consistent gap
    Letting a user pass in Gap to the content page, so that we can
    progressively transition the pages to have a consistent gap, and move
    all of the layout stuff outside of the individual components.
    
    Reducing the Hero module gap here, but we should do a pass on all of the
    pages and remove it.
    
    Having the gap be set inside ContentPage will allow us to handle mobile
    more consistently, without having to add it everywhere ourselves.
    
    TEST=manual
    
    Change-Id: Iaee898f401dc610be7a2c942576eea1cc2cca139

--- js/src/patina/components/ContentPage.tsx
+++ js/src/patina/components/ContentPage.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styles from './ContentPage.module.css'
 
 type ContentPageProps = {
+  gap?: number
   children: React.ReactNode
 }
 
@@ -10,10 +11,10 @@ type ContentPageProps = {
  * Page wrapper for content pages across the Patina Website to keep a consistent
  * style.
  */
-export function ContentPage({ children }: ContentPageProps) {
+export function ContentPage({ gap, children }: ContentPageProps) {
   return (
     <div className={styles.page}>
-      <Stack align={'center'} gap={0}>
+      <Stack align={'center'} gap={gap || 0}>
         {children}
       </Stack>
     </div>
