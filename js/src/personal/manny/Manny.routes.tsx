74bc223b9814f15bff3c676e9771e641a0938f0a
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-09-03 19:09:16 -0400

    Manny: Fix the routing system
    - With this patch the nav bar will now function as it is
    intended, by taking the user to the designated page
    
    Test= manual
    
    Change-Id: I2c5c0149d614777af4dbec9023daafafe1bb73d8

--- js/src/personal/manny/Manny.routes.tsx
+++ js/src/personal/manny/Manny.routes.tsx
@@ -1,7 +1,6 @@
 import { MannyPage } from '@/personal/manny/Manny.page.tsx'
-// import { ResumePage as MannyResume } from '@/personal/manny/pages/Resume.tsx'
-// import { About as MannyAboutMePage } from '@/personal/manny/pages/About.tsx'
-// import { Contact as MannyContactMePage } from '@/personal/manny/pages/Contact.tsx'
+import { ResumePage } from '@/personal/manny/sections/Resume.page.tsx'
+import { MainPage } from '@/personal/manny/sections/MannyMain.page.tsx'
 
 export const MannyRoutes = {
   path: 'manny',
@@ -11,28 +10,12 @@ export const MannyRoutes = {
     {
       index: true,
       description: "Manny's Home Page",
-      element: <MannyPage />,
+      element: <MainPage />,
+    },
+    {
+      path: 'resume',
+      description: "Manny's Resume Page",
+      element: <ResumePage />,
     },
   ],
 }
-
-// {
-//   path: 'manny',
-//     description: "Manny's Page",
-//   element: <MannyPage />,
-// },
-// {
-//   path: 'manny/resume',
-//     description: "Manny's Resume Page",
-//   element: <MannyResume />,
-// },
-// {
-//   path: 'manny/aboutme',
-//     description: "Manny's About Me Page",
-//   element: <MannyAboutMePage />,
-// },
-// {
-//   path: 'manny/contact',
-//     description: "Manny's Contact Me Page",
-//   element: <MannyContactMePage />,
-// },
