d253738e032af7af29adfa11a29bf05e42b2e230
MReyes1241 <Manuelreyes1241@Outlook.com>
2024-09-03 09:05:43 -0400

    Manny: Fix the routing system
    - With this patch the nav bar will now function as it is
    intended, by taking the user to the designated page
    
    Test= manual
    
    Change-Id: I2c5c0149d614777af4dbec9023daafafe1bb73d8

---
+++ js/src/personal/manny/sections/MannyMain.page.tsx
@@ -0,0 +1,16 @@
+import { MantineProvider } from '@mantine/core'
+// import styles from './Manny.module.css'
+import { theme } from '../theme.ts'
+import { Hero } from '@/personal/manny/sections/Hero.tsx'
+import { HeaderSimple } from '@/personal/manny/components/HeaderSimple.tsx'
+import { About } from './About.tsx'
+
+export function MannymainPage() {
+  return (
+    <MantineProvider theme={theme}>
+      <Hero />
+      <HeaderSimple />
+      <About />
+    </MantineProvider>
+  )
+}
