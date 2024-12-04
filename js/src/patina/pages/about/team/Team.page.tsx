656bc37ffeac962840bec27e4897e6f204c0f1b6
Henry Chen <hjc77@cornell.edu>
2024-12-03 21:33:58 -0500

    Team: Update CSS
    Made cards wider so that names and emails no longer cut out.
    Four cards per row now instead of six.
    Centered text and cards on mobile
    
    TEST=manual
    
    Change-Id: I0f4b40158b85c92bbe0f9e6e280de0cf981cf527

--- js/src/patina/pages/about/team/Team.page.tsx
+++ js/src/patina/pages/about/team/Team.page.tsx
@@ -1,36 +1,45 @@
-import { Title, Space, Group, Stack } from '@mantine/core'
+import { Group, Space, Stack, Title } from '@mantine/core'
 import { ContentPage } from '@/patina/components/ContentPage.tsx'
 import {
-  boardMembers,
   advisors,
+  boardMembers,
   interns,
 } from '@/patina/pages/about/team/teamData.ts'
 import { PersonCard } from '@/patina/pages/about/team/PersonCard.tsx'
+import styles from './Team.module.css'
 
 export function TeamPage() {
   return (
     <ContentPage>
       <Stack w={'100%'} align={'flex-start'}>
-        <Title order={1}>{'Our Team'}</Title>
+        <Title order={1} className={styles.title}>
+          {'Our Team'}
+        </Title>
         <Space h="md" />
-        <Title order={2}>{'Board of Directors'}</Title>
-        <Group component={'ul'} p={0}>
+        <Title order={2} className={styles.title}>
+          {'Board of Directors'}
+        </Title>
+        <Group component={'ul'} p={0} w={'100%'}>
           {boardMembers.map((boardMember) => (
             <PersonCard {...boardMember} />
           ))}
         </Group>
         <Space h="md" />
 
-        <Title order={2}>{'Advisors'}</Title>
-        <Group component={'ul'} p={0}>
+        <Title order={2} className={styles.title}>
+          {'Advisors'}
+        </Title>
+        <Group component={'ul'} p={0} w={'100%'}>
           {advisors.map((boardMember) => (
             <PersonCard {...boardMember} />
           ))}
         </Group>
         <Space h="md" />
 
-        <Title order={2}>{'Interns'}</Title>
-        <Group component={'ul'} p={0}>
+        <Title order={2} className={styles.title}>
+          {'Interns'}
+        </Title>
+        <Group component={'ul'} p={0} w={'100%'}>
           {interns.map((boardMember) => (
             <PersonCard {...boardMember} />
           ))}
