47238d974dad6e93afbe819aeaf86b225728f99c
Henry Chen <hjc77@cornell.edu>
2024-12-20 13:17:22 -0500

    Mentor: Made new page for Spring semester
    Copied over the page and routed it to mentor-sp25
    
    TEST=manual
    Tested the page locally.
    
    Change-Id: I6d078a622f05271467a82420c037558435230239

---
+++ js/src/patina/pages/mentor-sp25/Mentor.page.tsx
@@ -0,0 +1,65 @@
+import { Button, Center, Space, Text, useMantineTheme } from '@mantine/core'
+import { Hero } from '@/patina/components/Hero'
+import { About } from '@/patina/pages/mentor/about/About'
+import { ContentPage } from '@/patina/components/ContentPage.tsx'
+import { imageUrls } from '@/patina/assets/images'
+import styles from './Mentor.module.css'
+import { ProgramDetails } from '@/patina/pages/mentor/program/ProgramDetails.tsx'
+
+const mentorGoogleFormUrl =
+  'https://docs.google.com/forms/d/1yWPT7gYtpiKbBO6IKgcTygTzuHdAZaSomgu0BnDW23s/viewform'
+
+/**
+ * Mentor page allows people to apply to be mentors.
+ * */
+export function MentorPage() {
+  const message = (
+    <Text className={styles.description}>
+      {
+        'Share your expertise and make a meaningful impact by mentoring! Join us to help cultivate diverse talent, foster cultural awareness, and shape a more inclusive future in your industry.'
+      }
+      <p />
+      {'Applications for Spring 2025 are now open!'}
+    </Text>
+  )
+  return (
+    <ContentPage>
+      <Hero
+        title="Be a Patina Network Mentor"
+        details={message}
+        img={imageUrls.mentorApplyHero.src}
+        alt={imageUrls.mentorApplyHero.alt}
+        buttonLink={mentorGoogleFormUrl}
+        buttonText="Apply to be a mentor today!"
+        buttonColor={'var(--mantine-color-patina-blue-light)'}
+      />
+      <About />
+      <Space h={64} />
+      <ProgramDetails />
+      <Space h={64} />
+      <ApplyButton />
+      <Space h={64} />
+    </ContentPage>
+  )
+}
+
+function ApplyButton() {
+  const theme = useMantineTheme()
+
+  return (
+    <Center>
+      <Button
+        component="a"
+        variant="filled"
+        color={theme.other.patinaBlueLight}
+        autoContrast
+        target="_blank"
+        href={mentorGoogleFormUrl}
+      >
+        <Text c={'black'} size="20">
+          {'Apply to be a mentor here!'}
+        </Text>
+      </Button>
+    </Center>
+  )
+}
