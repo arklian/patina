dff7b46d4b253253cc84d0713005b7431a3b6be1
Henry Chen <hjc77@cornell.edu>
2025-01-18 15:20:09 -0500

    WIP: Connect SQL
    TODO: Make patchat_member table in the patina-dev db
    
    Change-Id: Ib99bf1de602508ca45ec4eccf11891e8f3b2b0a6

--- src/main/java/org/patinanetwork/patchats/repo/PsqlPatChatRepo.java
+++ src/main/java/org/patinanetwork/patchats/repo/PsqlPatChatRepo.java
@@ -1,12 +1,47 @@
 package org.patinanetwork.patchats.repo;
 
+import org.patinanetwork.common.db.DBConnection;
 import org.patinanetwork.patinawebsite.blogs.protos.PatChatMember;
 import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Component(value = "PsqlPatChatRepo")
 public class PsqlPatChatRepo implements PatChatRepo {
-    @Override
-    public PatChatMember getPatChatMember(int i) {
-        return PatChatMember.newBuilder().setId(1).setName("Test Dummy").setActive(true).build();
+
+
+    DBConnection dbConnection;
+    Connection conn;
+
+    public PsqlPatChatRepo(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+        this.conn = dbConnection.getConn();
     }
+
+    @Override
+    public PatChatMember getPatChatMember(int id) {
+
+            String sql = "SELECT id, name, active FROM patchat_member WHERE id = ?";
+            PatChatMember member = null;
+
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setInt(1, id);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    if (rs.next()) {
+                        int db_id = rs.getInt("id");
+                        String name = rs.getString("name");
+                        boolean active = rs.getBoolean("active");
+                        member = PatChatMember.newBuilder().setId(db_id).setName(name).setActive(active).build();
+                    }
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException("Error while retrieving patchat member by ID", e);
+            }
+
+        return member;
+        }
+
 }
