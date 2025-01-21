d1c2ec740f9f21ba459035037929bd65982700b4
Maliha Tasnim <ayesiddika@gmail.com>
2025-01-20 19:19:41 -0500

    PatChats: Add DELETE endpoint for deleting members
    TEST=manual
    localhost:8080/api/patchats/delete/1 returns JSON
    
    Change-Id: If7245a6370180bc7129c5c749d4345c495dd33cc

--- src/main/java/org/patinanetwork/patchats/PatChatServer.java
+++ src/main/java/org/patinanetwork/patchats/PatChatServer.java
@@ -2,13 +2,16 @@ package org.patinanetwork.patchats;
 
 import org.patinanetwork.common.protos.JsonParser;
 import org.patinanetwork.common.protos.JsonPrinter;
+import org.patinanetwork.patchats.ops.DeletePatChatMemberOp;
 import org.patinanetwork.patchats.ops.GetPatChatMemberOp;
 import org.patinanetwork.patchats.ops.ListPatChatMembersOp;
-import org.patinanetwork.patchats.repo.PatChatRepo;
+import org.patinanetwork.patchats.protos.DeletePatChatMemberReq;
 import org.patinanetwork.patchats.protos.GetPatChatMemberReq;
 import org.patinanetwork.patchats.protos.ListPatChatMembersReq;
+import org.patinanetwork.patchats.repo.PatChatRepo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +44,13 @@ public class PatChatServer {
         var req = ListPatChatMembersReq.newBuilder().build();
         ListPatChatMembersOp op = new ListPatChatMembersOp(patChatRepo);
         return jsonPrinter.print(op.run());
+    }
+
+    @DeleteMapping(value = "/api/patchats/delete/{memberId}")
+    public String deletePatChatMember(@PathVariable("memberId") int memberId) {
+        System.out.println("Received DELETE request for memberId: " + memberId);
+        var req = DeletePatChatMemberReq.newBuilder().setId(memberId).build();
+        DeletePatChatMemberOp op = new DeletePatChatMemberOp(patChatRepo);
+        return jsonPrinter.print(op.run(req));
     }
 }
--- src/main/java/org/patinanetwork/patchats/repo/PsqlPatChatRepo.java
+++ src/main/java/org/patinanetwork/patchats/repo/PsqlPatChatRepo.java
@@ -3,7 +3,6 @@ package org.patinanetwork.patchats.repo;
 import org.patinanetwork.common.db.DBConnection;
 import org.patinanetwork.patchats.protos.PatChatMember;
 import org.springframework.stereotype.Component;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,7 +12,6 @@ import java.util.List;
 
 @Component(value = "PsqlPatChatRepo")
 public class PsqlPatChatRepo implements PatChatRepo {
-
     DBConnection dbConnection;
     Connection conn;
 
@@ -49,6 +47,7 @@ public class PsqlPatChatRepo implements PatChatRepo {
         return member;
     }
 
+    @Override
     public List<PatChatMember> listPatChatMembers() {
         String sql = "SELECT id, name, active FROM patchat_member";
         List<PatChatMember> members = new ArrayList<>();
@@ -71,7 +70,26 @@ public class PsqlPatChatRepo implements PatChatRepo {
         } catch (SQLException e) {
             throw new RuntimeException("Error while retrieving all members", e);
         }
-
         return members;
+    }
+
+    @Override
+    public PatChatMember deletePatChatMember(int id) {
+        PatChatMember member = getPatChatMember(id);
+        if (member == null) {
+            return PatChatMember.getDefaultInstance();
+        }
+
+        String sql = "DELETE FROM patchat_member WHERE id = ?";
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            if (stmt.executeUpdate() > 0) {
+                return member;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while deleting PatChatMember with ID " + id, e);
+        }
+        return member;
     }
 }
