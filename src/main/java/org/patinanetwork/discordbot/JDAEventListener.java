8f3fb65fb18ed25d4cf52b444d0831a3316c26c0
Arshadul Monir <arshadulmonir@gmail.com>
2025-01-22 19:42:01 +0000

    PatChats: Join Patchats function
    Created a join_patchats discord bot command and a joinPatchats function
    in the event listener
    
    TEST=manual
    Tested discord command tested successfully locally
    
    Change-Id: I7fff8a48663ecc93b196dd5ac8823da9e45bdb1e

--- src/main/java/org/patinanetwork/discordbot/JDAEventListener.java
+++ src/main/java/org/patinanetwork/discordbot/JDAEventListener.java
@@ -2,34 +2,49 @@ package org.patinanetwork.discordbot;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.patinanetwork.patchats.PatChatClient;
+import org.patinanetwork.patchats.protos.AddPatChatMemberReq;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JDAEventListener extends ListenerAdapter {
+    PatChatClient patChatClient;
 
-
-    public JDAEventListener() {
+    public JDAEventListener(PatChatClient patChatClient) {
+        this.patChatClient = patChatClient;
     }
 
     @Override
-    public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
-    {
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
         // Only accept commands from guilds
-        if (event.getGuild() == null)
-            return;
-        switch (event.getName())
-        {
+        if (event.getGuild() == null) return;
+        switch (event.getName()) {
             case "say":
                 say(event, event.getOption("content").getAsString()); // content is required so no null-check here
                 break;
+            case "join_patchats":
+                joinPatChats(event);
+                break;
             default:
-                event.reply("I can't handle that command right now :(").setEphemeral(true).queue();
+                event.reply("I can't handle that command right now :(")
+                        .setEphemeral(true)
+                        .queue();
         }
     }
 
-    public void say(SlashCommandInteractionEvent event, String content)
-    {
+    public void say(SlashCommandInteractionEvent event, String content) {
         event.reply(content).queue(); // This requires no permissions!
     }
 
+    public void joinPatChats(SlashCommandInteractionEvent event) {
+        String memberName = event.getUser().getEffectiveName();
+
+        try {
+            patChatClient.addPatChatMember(
+                    AddPatChatMemberReq.newBuilder().setName(memberName).build());
+            event.reply("Successfully added.").setEphemeral(true).queue();
+        } catch (Exception e) {
+            event.reply("Something went wrong.").setEphemeral(true).queue();
+        }
+    }
 }
