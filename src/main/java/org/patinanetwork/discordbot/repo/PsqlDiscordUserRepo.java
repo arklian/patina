0d4b6fb7beb642232a7f525bc71f0163f12acc97
Arshadul Monir <arshadulmonir@gmail.com>
2025-01-23 15:55:00 -0500

    PatChats: Discord User files + proto
    Re-added discord user repo and template for psql repo files into
    discordbot folder and created a proto file for it.
    
    TEST=manual
    - Proto file generated correctly
    
    Edit: Resolved comments for previous push
    Change-Id: I8a07ebf22a1a02914d8792e138c67489849f1097

---
+++ src/main/java/org/patinanetwork/discordbot/repo/PsqlDiscordUserRepo.java
@@ -0,0 +1,25 @@
+package org.patinanetwork.discordbot.repo;
+
+import org.patinanetwork.discordbot.protos.DiscordUser;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component(value = "PsqlDiscordUserRepo")
+public class PsqlDiscordUserRepo implements DiscordUserRepo {
+    public DiscordUser getDiscordUser(int patchat_member_id) {
+        return null;
+    }
+
+    public List<DiscordUser> listDiscordUsers() {
+        return List.of();
+    }
+
+    public DiscordUser addDiscordUser(DiscordUser member) {
+        return null;
+    }
+
+    public DiscordUser deleteDiscordUser(int patchat_member_id) {
+        return null;
+    }
+}
