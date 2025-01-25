5a7b399444d3e7b6eff69bd5c56e64cb8f412387
MReyes1241 <Manuelreyes1241@Outlook.com>
2025-01-25 17:55:04 -0500

    Patchats: create a client side op for broadcast
    For sake of separating frontend and backend functionality
    created a operation
    for broadcast in the discord client
    
    TEST=manual
    
    Change-Id: I8d1066a826b573720263bd9296a0afab0c412fe1

--- src/main/java/org/patinanetwork/discordbot/DiscordUserClient.java
+++ src/main/java/org/patinanetwork/discordbot/DiscordUserClient.java
@@ -1,6 +1,8 @@
 package org.patinanetwork.discordbot;
 
+import net.dv8tion.jda.api.JDA;
 import org.patinanetwork.discordbot.ops.AddDiscordUserOp;
+import org.patinanetwork.discordbot.ops.BroadcastPatChatMatchingOp;
 import org.patinanetwork.discordbot.ops.GetDiscordUserOp;
 import org.patinanetwork.discordbot.ops.ListDiscordUserOp;
 import org.patinanetwork.discordbot.protos.AddDiscordUserReq;
@@ -9,6 +11,7 @@ import org.patinanetwork.discordbot.protos.GetDiscordUserReq;
 import org.patinanetwork.discordbot.protos.GetDiscordUserResp;
 import org.patinanetwork.discordbot.protos.ListDiscordUsersResp;
 import org.patinanetwork.discordbot.repo.DiscordUserRepo;
+import org.patinanetwork.patchats.PatChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -18,9 +21,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class DiscordUserClient {
     DiscordUserRepo discordUserRepo;
+    PatChatClient patChatClient;
+    JDA jda;
 
-    public DiscordUserClient(@Qualifier("PsqlDiscordUserRepo") DiscordUserRepo discordUserRepo) {
+    public DiscordUserClient(
+            @Qualifier("PsqlDiscordUserRepo") DiscordUserRepo discordUserRepo, PatChatClient patChatClient, JDA jda) {
         this.discordUserRepo = discordUserRepo;
+        this.patChatClient = patChatClient;
+        this.jda = jda;
     }
 
     public GetDiscordUserResp getDiscordUser(GetDiscordUserReq req) {
@@ -33,5 +41,9 @@ public class DiscordUserClient {
 
     public AddDiscordUserResp addDiscordUser(AddDiscordUserReq req) {
         return new AddDiscordUserOp(discordUserRepo).run(req);
+    }
+
+    public void broadcastPatChatMatching() {
+        new BroadcastPatChatMatchingOp(discordUserRepo, patChatClient, jda).run();
     }
 }
